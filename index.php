bc88822db2e9a1bc4c42dd4f337b4292facffd38
rgronback <rgronback>
2006-05-11 19:16:54 +0000

    Changed formatting.

--- index.php
+++ index.php
@@ -39,29 +39,28 @@ development technologies within the Eclipse community by providing a unified set
 		<div class="homeitem3col">
 			<h3>Framework Projects</h3>
 			<ul>
-				<li><a href="http://www.eclipse.org/emf">EMF</a>. Eclipse Modeling Framework: a modeling framework and code generation facility for building tools and other applications based on a structured data model.</li>
-				<li><a href="http://www.eclipse.org/gmf">GMF</a>. Graphical Modeling Framework: provides a generative component and runtime infrastructure for developing graphical editors based on <a href="http://www.eclipse.org/emf" target="_top"><b>EMF</b></a> and <a href="http://www.eclipse.org/gef" target="_top"><b>GEF</b></a>.</li>
-				<li><a href="http://www.eclipse.org/emft/projects/validation">VF</a>. Validation Framework: provides model constraint definition, traversal, and evaluation for EMF model validation.</li>
-				<li><a href="http://www.eclipse.org/mddi">MDDi</a>. Model Driven Development integration: produces an extensible framework and exemplary tools dedicated to integration of modeling tools in Eclipse.</li>
+				<li><a href="http://www.eclipse.org/emf">Eclipse Modeling Framework</a> (EMF) : a modeling framework and code generation facility for building tools and other applications based on a structured data model.</li>
+				<li><a href="http://www.eclipse.org/gmf">Graphical Modeling Framework</a> (GMF) : provides a generative component and runtime infrastructure for developing graphical editors based on <a href="http://www.eclipse.org/emf" target="_top"><b>EMF</b></a> and <a href="http://www.eclipse.org/gef" target="_top"><b>GEF</b></a>.</li>
+				<li><a href="http://www.eclipse.org/emft/projects/validation">Validation Framework</a> (VF) : provides model constraint definition, traversal, and evaluation for EMF model validation.</li>
+				<li><a href="http://www.eclipse.org/mddi">Model Driven Development integration</a> (MDDi) : produces an extensible framework and exemplary tools dedicated to integration of modeling tools in Eclipse.</li>
 			</ul>
 		</div>
 		<div class="homeitem3col">
 			<h3>Standards Implementations</h3>
 			<ul>
-				<li><a href="http://www.eclipse.org/uml2">UML2</a>. Unified Modeling Language 2: an EMF-based implementation of the UMLª 2.x metamodel for the Eclipse platform.</li>
-				<li><a href="http://www.eclipse.org/emft/projects/ocl">OCL</a>. Object Constraint Language: defines APIs for OCL expression syntax for implementing queries and contraints.</li>
-				<li><a href="http://www.eclipse.org/xsd">XSD</a>. XML Schema Infoset Model: a reference library that provides an <a href="http://download.eclipse.org/tools/emf/xsd/javadoc?org/eclipse/xsd/package-summary.html#details">API</a> for use with any code that examines, creates or modifies <a href="http://www.w3.org/TR/XMLSchema-0">W3C XML Schema</a>.</li>
-				<li><a href="http://www.eclipse.org/emft/projects/eodm">EODM</a>. EMF Ontology Definition Metamodel is an implementation of RDF(S)/OWL metamodels of the <a href="http://www.omg.org/ontology">Ontology Definition Metamodel (ODM)</a> using EMF with additional parsing, inference, model transformation and editing functions.</li>
+				<li><a href="http://www.eclipse.org/uml2">Unified Modeling Language 2</a> (UML2) : an EMF-based implementation of the UMLª 2.x metamodel for the Eclipse platform.</li>
+				<li><a href="http://www.eclipse.org/emft/projects/ocl">Object Constraint Language</a> (OCL) : defines APIs for OCL expression syntax for implementing queries and contraints.</li>
+				<li><a href="http://www.eclipse.org/xsd">XML Schema Infoset Model</a> (XSD) : a reference library that provides an <a href="http://download.eclipse.org/tools/emf/xsd/javadoc?org/eclipse/xsd/package-summary.html#details">API</a> for use with any code that examines, creates or modifies <a href="http://www.w3.org/TR/XMLSchema-0">W3C XML Schema</a>.</li>
+				<li><a href="http://www.eclipse.org/emft/projects/eodm">EMF Ontology Definition Metamodel</a> (EODM) : is an implementation of RDF(S)/OWL metamodels of the <a href="http://www.omg.org/ontology">Ontology Definition Metamodel (ODM)</a> using EMF with additional parsing, inference, model transformation and editing functions.</li>
 			</ul>
 		</div>
 		<div class="homeitem3col">
 			<h3>Model Technology and Research Projects</h3>
 			<ul>
-				<li><a href="http://www.eclipse.org/emft/projects/jet">JET</a>. Java Emitter Templates: provides code generation framework & facilities that are used by EMF.</li>
-				<li><a href="http://www.eclipse.org/emft/projects/query">MQ</a>. Model Query: facilitates the process of search and retrieval of model elements of interest in a flexible yet controlled and structured manner.</li>
-				<li><a href="http://www.eclipse.org/emft/projects/transaction">MT</a>. Model Transaction: provides a model management layer built on top of EMF for managing EMF resources.</li>
-				<li><a href="http://www.eclipse.org/gmt">GMT</a>. Generative Modeling Tools: research-oriented project focused on producing prototypes in the area of Model Driven Engineering (MDE).</li>
-				<li><a href="http://www.eclipse.org/mddi">MDDi</a>. Model Driven Development integration: produces an extensible framework and exemplary tools dedicated to integration of modeling tools in Eclipse.</li>
+				<li><a href="http://www.eclipse.org/emft/projects/jet">Java Emitter Templates</a> (JET) : provides code generation framework & facilities that are used by EMF.</li>
+				<li><a href="http://www.eclipse.org/emft/projects/query">Model Query</a> (MQ) : facilitates the process of search and retrieval of model elements of interest in a flexible yet controlled and structured manner.</li>
+				<li><a href="http://www.eclipse.org/emft/projects/transaction">Model Transaction</a> (MT) : provides a model management layer built on top of EMF for managing EMF resources.</li>
+				<li><a href="http://www.eclipse.org/gmt">Generative Modeling Tools</a> (GMT) : research-oriented project focused on producing prototypes in the area of Model Driven Engineering (MDE).</li>
 			</ul>
 		</div>
 		<hr class="clearer" />
