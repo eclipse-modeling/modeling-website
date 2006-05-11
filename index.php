9f9e148f3b9244e8e6a1c7f49bcf65f9277c05f7
rgronback <rgronback>
2006-05-11 20:20:47 +0000

    Adjusted category names.

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
 		<p>The Eclipse Modeling Project focuses on the evolution and promotion of model-based 
 development technologies within the Eclipse community by providing a unified set of modeling frameworks, tooling, and standards implementations.</p>
 		<div class="homeitem3col">
-			<h3>Framework Projects</h3>
+			<h3>Frameworks</h3>
 			<ul>
 				<li><a href="http://www.eclipse.org/emf">Eclipse Modeling Framework</a> (EMF) : a modeling framework and code generation facility for building tools and other applications based on a structured data model.</li>
 				<li><a href="http://www.eclipse.org/gmf">Graphical Modeling Framework</a> (GMF) : provides a generative component and runtime infrastructure for developing graphical editors based on <a href="http://www.eclipse.org/emf" target="_top"><b>EMF</b></a> and <a href="http://www.eclipse.org/gef" target="_top"><b>GEF</b></a>.</li>
@@ -46,7 +46,7 @@ development technologies within the Eclipse community by providing a unified set
 			</ul>
 		</div>
 		<div class="homeitem3col">
-			<h3>Standards Implementations</h3>
+			<h3>Industry Standards</h3>
 			<ul>
 				<li><a href="http://www.eclipse.org/uml2">Unified Modeling Language 2</a> (UML2) : an EMF-based implementation of the UMLª 2.x metamodel for the Eclipse platform.</li>
 				<li><a href="http://www.eclipse.org/emft/projects/ocl">Object Constraint Language</a> (OCL) : defines APIs for OCL expression syntax for implementing queries and contraints.</li>
@@ -55,7 +55,7 @@ development technologies within the Eclipse community by providing a unified set
 			</ul>
 		</div>
 		<div class="homeitem3col">
-			<h3>Model Technology and Research Projects</h3>
+			<h3>Technology and Research Projects</h3>
 			<ul>
 				<li><a href="http://www.eclipse.org/emft/projects/jet">Java Emitter Templates</a> (JET) : provides code generation framework & facilities that are used by EMF.</li>
 				<li><a href="http://www.eclipse.org/emft/projects/query">Model Query</a> (MQ) : facilitates the process of search and retrieval of model elements of interest in a flexible yet controlled and structured manner.</li>
