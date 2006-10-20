75972555a081185dd25722073ad9a73a7c640173
nickb <nickb>
2006-10-20 21:16:34 +0000

    replace old index.php with new ones; fix main one

--- emf/docs/presentations/index.php
+++ emf/docs/presentations/index.php
@@ -12,15 +12,16 @@ ob_start();
 
 $dirName = str_replace("/index.php","",$_SERVER["SCRIPT_NAME"]);
 $dirName = explode("/",$dirName); $dirName = $dirName[sizeof($dirName)-1]; 
-$files = loadDirSimple("$dirName",".*");
+$files = loadDirSimple($_SERVER['DOCUMENT_ROOT'] . "/emf/docs/presentations/" . ($dirName!='presentations'?$dirName:""), ".*", $dirName!='presentations'?"f":"d");
 print '<div id="midcolumn">
 <div class="homeitem3col">
 <h3>Presentation Materials '.($dirName!='presentations'?'From '.$dirName:'').'</h3>
 <ul>
 ';
-if (sizeof($files)>0) { 
+if (sizeof($files)>2) { 
 	rsort($files);
 	foreach ($files as $file) { 
+		if ($file != "CVS" && $file !="index.php")
 		print '<li><a href="'.$file.'">'.$file.'</a></li>'."\n";
 	}
 } else {
@@ -30,23 +31,13 @@ print '</ul>
 </div>
 </div>';
 
-/**********************/
+$html = ob_get_contents();
+ob_end_clean();
 
-function loadDirSimple($dir,$ext) { // 1D array
-	$stuff = array();
-	if (is_dir($dir) && is_readable($dir)) { 
-		ini_set("display_errors","0"); // suppress file not found errors
-		$handle=opendir($dir);
-		while (($file = readdir($handle))!==false) {
-		  if ( ($ext=="" || preg_match("/".$ext."$/",$file)) && $file!=".." && $file!="." && $file!="CVS" && $file!="index.php") { 
-			  $stuff[] = "$file"; 
-		  }
-		}
-		closedir($handle); 
-		ini_set("display_errors","1"); // and turn 'em back on.
-	} else {
-		//exit;
-	}
-	return $stuff;
-}
+$pageTitle = "Presentation Materials";
+$pageKeywords = "";
+$pageAuthor = "Nick Boldt";
+
+$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+
 ?>
