dba33a268868510250dbb1b8e4cdd59822c77044
nickb <nickb>
2006-11-29 01:34:59 +0000

    only show extras if project matches

--- emf/news/relnotes.php
+++ emf/news/relnotes.php
@@ -1,10 +1,13 @@
 <?php
-$cvsprojs = array(
+$cvsprojs = array (
 	"emf" => "org.eclipse.emf",
 	"xsd" => "org.eclipse.xsd"
 );
 
-include_once $_SERVER["DOCUMENT_ROOT"] . "/modeling/emf/emf/news/relnotes-common-emf.php";
+if (isset ($_GET["project"]) && ($_GET["project"] == "emf" || $_GET["project"] == "xsd"))
+{
+	include_once $_SERVER["DOCUMENT_ROOT"] . "/modeling/emf/emf/news/relnotes-common-emf.php";
+}
 
-require($_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/relnotes-common.php");
+require ($_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/relnotes-common.php");
 ?>
