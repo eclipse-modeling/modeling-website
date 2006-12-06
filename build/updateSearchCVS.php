41cf17e5f28a91715284dfa18a448ed8aa1f42cd
nickb <nickb>
2006-12-06 21:39:56 +0000

    add emf and emft servers into $isBuildServer definition

--- build/updateSearchCVS.php
+++ build/updateSearchCVS.php
@@ -2,8 +2,8 @@
 require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php"); require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); $App = new App(); $Nav = new Nav(); $Menu = new Menu(); include($App->getProjectCommon());
 
 require_once($_SERVER['DOCUMENT_ROOT'] . "/modeling/includes/scripts.php");
-$isBuildServer = $_SERVER["SERVER_NAME"] == "build.eclipse.org";
 $isEMFserver = (preg_match("/^emf(?:\.torolab\.ibm\.com)$/", $_SERVER["SERVER_NAME"]));
+$isBuildServer = (preg_match("/^(emft|build)\.eclipse\.org$/", $_SERVER["SERVER_NAME"])) || $isEMFserver;
 internalUseOnly(); 
 
 include($_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/db.php");
