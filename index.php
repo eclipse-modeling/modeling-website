5925073a6bdfaa0fe6cfa3a90c74cc6bf1ce1a48
nickb <nickb>
2007-01-16 23:50:24 +0000

    [170688]

--- index.php
+++ index.php
@@ -91,9 +91,9 @@ development technologies within the Eclipse community by providing a unified set
 					an implementation of the UML 2 umbrella of standards defined by the OMG and tools 
 					to manipulate models based on those standards.</li>
 				<ul>
-					<li><a href="http://www.eclipse.org/modeling/mdt/?project=uml2-ocl#uml2-ocl">Object Constraint Language</a> (OCL) : 
+					<li><a href="http://www.eclipse.org/modeling/mdt/?project=ocl#ocl">Object Constraint Language</a> (OCL) : 
 						defines APIs for OCL expression syntax for implementing queries and contraints.</li>
-					<li><a href="http://www.eclipse.org/modeling/mdt/?project=uml2-uml#uml2-uml">Unified Modeling Language 2</a> (UML2) : 
+					<li><a href="http://www.eclipse.org/modeling/mdt/?project=uml2#uml2">Unified Modeling Language 2</a> (UML2) : 
 						an EMF-based implementation of the UML 2.x metamodel for the Eclipse platform.</li>
 					<li><a href="http://www.eclipse.org/modeling/mdt/?project=uml2tools#uml2tools">UML2 Tools</a> : 
 						a set of GMF-based editors for viewing and editing UML models.</li>
