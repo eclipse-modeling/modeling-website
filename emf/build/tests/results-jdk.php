e4a4bbe77ae2497756194cefb74aee7cec32457c
nickb <nickb>
2007-02-08 20:44:07 +0000

    *** empty log message ***

--- emf/build/tests/results-jdk.php
+++ emf/build/tests/results-jdk.php
@@ -1,9 +1,7 @@
 <?php
 $pre = "../../"; 
-require_once($_SERVER['DOCUMENT_ROOT'] . "/emf/includes/header.php"); 
-internalUseOnly(); 
-
 require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php"); require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php");  require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); $App = new App(); $Nav = new Nav(); $Menu = new Menu(); include($App->getProjectCommon());
+internalUseOnly(); 
 ob_start();
 
 $VER = $_GET["version"] && preg_match("/(14|50)/",$_GET["version"])? $_GET["version"] : "50"; 
