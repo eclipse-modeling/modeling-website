efa8dbd1c6f0ecf2d98278056c51087e8d7b762a
nickb <nickb>
2007-02-12 20:09:28 +0000

    improve mysql degradation when database server dead

--- includes/db.php
+++ includes/db.php
@@ -1,18 +1,30 @@
 <?php
 
-
-
 if (is_file($writableRoot . "parsecvs-dbaccess.php") && preg_match("@^/$PR/build/@", $_SERVER["PHP_SELF"]))
 {
 	require_once($writableRoot . "parsecvs-dbaccess.php");
 	$connect = mysql_connect($dbhost, $dbuser, $dbpass);
-	mysql_select_db((isset($db) && $db ? $db : "modeling"), $connect) or die(mysql_error());
+	if ($connect)
+	{
+		mysql_select_db((isset($db) && $db ? $db : "modeling"), $connect) or die(mysql_error());
+	}
+	else
+	{
+		$connect = null;
+	}
 }
 else if (is_file($writableRoot . "searchcvs-dbaccess.php"))
 {
 	require_once $writableRoot . "searchcvs-dbaccess.php";
 	$connect = mysql_connect($dbhost, $dbuser, $dbpass);
-	mysql_select_db((isset($db) && $db ? $db : "modeling"), $connect) or die(mysql_error());
+	if ($connect)
+	{
+		mysql_select_db((isset($db) && $db ? $db : "modeling"), $connect) or die(mysql_error());
+	}
+	else
+	{
+		$connect = null;
+	}
 }
 else
 {
