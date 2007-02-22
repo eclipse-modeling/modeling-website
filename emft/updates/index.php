0beca6dbbd7d68d088f54b8ec99a87c29dc06236
nickb <nickb>
2007-02-22 18:28:37 +0000

    switch to shared page and add mention of MDT

--- emft/updates/index.php
+++ emft/updates/index.php
@@ -1,55 +1,15 @@
 <?php 
-require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php"); require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php");  require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); $App = new App(); $Nav = new Nav(); $Menu = new Menu(); include($App->getProjectCommon());
+require($_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/updates-common.php");
 
-ob_start();
-?>
-<div id="midcolumn">
-	<div class="homeitem3col">
-		<h3>EMF Update Manager Site</h3>
-		<p>To install these plugins, point your Eclipse Update Manager at this site. For more on how to do this, <a href="http://www.eclipse.org/modeling/emf/docs/misc/UsingUpdateManager/UsingUpdateManager.html">click here</a>. <a href="http://www.eclipse.org/downloads/download.php?file=/technology/emft/updates/site.xml&amp;format=xml">Mirrors available</a>.
-		</p>
-		<ul>
-			<li>
-				Help
-				<ul>
-					<li>
-						Software Updates
-						<ul>
-							<li>
-								Find and Install...
-								<ul>
-									<li>
-										Search for new features to install
-										<ul>
-											<li>
-											Add Update Site...<br/>
-											* Name: <b>EMFT Update Manager Site</b><br/>
-											* URL: <b><a href="http://download.eclipse.org/technology/emft/updates/site.xml" target="_um">http://download.eclipse.org/technology/emft/updates/site.xml</a></b> (Releases)<br/>
-											(or): <b><a href="http://download.eclipse.org/technology/emft/updates/site-interim.xml" target="_um">http://download.eclipse.org/technology/emft/updates/site-interim.xml</a></b> (I, M and S Builds)
-											</li>
-										</ul>
-									</li>
-								</ul>
-							</li>
-						</ul>
-					</li>
-				</ul>
-			</li>
-		</ul>
-	</div>
-</div>
-<!-- this doesn't really fit anywhere anymore... -->
-<!-- <img alt="how to" src="http://www.eclipse.org/images/howto_banner.jpg" height="111" width="272"/> -->
-<?php
-$html = ob_get_contents();
-ob_end_clean();
+$PRS = array(
+	"MDT (EODM, OCL)" => "modeling/mdt"
+);
 
-$pageTitle = "EMFT - Update Manager";
-$pageKeywords = ""; // TODO: add something here
-$pageAuthor = "Neil Skrypuch";
+function notes()
+{
+	print "<p><i style=\"color:red\"><b>NOTE:</b> Some EMFT projects have migrated to other " .
+			"Update Manager sites, so you'll have to use the new sites for newer releases.</i></p>\n";
+}
 
-$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="/emft/includes/um.css"/>');
-
-# Generate the web page
-$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+update_manager("EMFT", "Eclipse Modeling", $PRS);
 ?>
