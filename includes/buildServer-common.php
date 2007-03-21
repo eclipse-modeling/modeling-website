0207df63446ef36315dbfceeec9978a2d0903ca4
nickb <nickb>
2007-03-21 21:00:19 +0000

    [178148] add writableBuildRoot variable

--- includes/buildServer-common.php
+++ includes/buildServer-common.php
@@ -1,4 +1,14 @@
 <?php
 	# allows server-specific path assignment, eg., for build.eclipse.org
-	$_SERVER['DOCUMENT_ROOT'] = $_SERVER["SERVER_NAME"] == "build.eclipse.org" ? "/opt/public/modeling" : $_SERVER['DOCUMENT_ROOT'];
+	if ($_SERVER["SERVER_NAME"] == "build.eclipse.org")
+	{
+		$_SERVER['DOCUMENT_ROOT'] = "/opt/public/modeling";
+		$writableBuildRoot = "/opt/public/modeling"; 
+	}
+	else
+	{
+		#$_SERVER['DOCUMENT_ROOT'] = $_SERVER['DOCUMENT_ROOT'];
+		$writableBuildRoot = "/home/www-data"; 
+		
+	}
 ?>
