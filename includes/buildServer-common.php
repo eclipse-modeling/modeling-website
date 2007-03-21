3953247d53de909c8f1755988328383e285c0046
nickb <nickb>
2007-03-21 21:26:34 +0000

    [178148] revert; variable already in _projectCommon.php

--- includes/buildServer-common.php
+++ includes/buildServer-common.php
@@ -1,14 +1,4 @@
 <?php
 	# allows server-specific path assignment, eg., for build.eclipse.org
-	if ($_SERVER["SERVER_NAME"] == "build.eclipse.org")
-	{
-		$_SERVER['DOCUMENT_ROOT'] = "/opt/public/modeling";
-		$writableBuildRoot = "/opt/public/modeling"; 
-	}
-	else
-	{
-		#$_SERVER['DOCUMENT_ROOT'] = $_SERVER['DOCUMENT_ROOT'];
-		$writableBuildRoot = "/home/www-data"; 
-		
-	}
+	$_SERVER['DOCUMENT_ROOT'] = $_SERVER["SERVER_NAME"] == "build.eclipse.org" ? "/opt/public/modeling" : $_SERVER['DOCUMENT_ROOT'];
 ?>
