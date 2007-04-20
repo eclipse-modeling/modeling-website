6f9f45247bdfb6137e55ba2e63f56740e558730f
nickb <nickb>
2007-04-20 20:33:14 +0000

    [181212]

---
+++ m2t/javadoc/index.php
@@ -0,0 +1,11 @@
+<?php
+$projDetails = array(
+	"/modeling/m2t",
+	"/modeling/m2t/downloads",
+	"/modeling",
+	"M2T"
+);
+
+require_once ("../../includes/buildServer-common.php");
+require($_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/javadoc-common.php");
+?>
---
+++ m2t/updates/index.php
@@ -0,0 +1,15 @@
+<?php 
+require_once ("../../includes/buildServer-common.php");
+require($_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/updates-common.php");
+
+$PRS = array(
+	"EMFT (JET, JET Editor)" => "technology/emft"
+);
+
+function notes()
+{
+	print "<p><i style=\"color:red\"><b>NOTE:</b> not all M2T projects have migrated to the M2T Update Manager site yet, so you'll have to use the older sites until that time.</i></p>\n";
+}
+
+update_manager("M2T", "Eclipse Modeling", $PRS);
+?>
