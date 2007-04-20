0037c167ec4908f5021c35f03817cf090164d16f
nickb <nickb>
2007-04-20 18:43:13 +0000

    [181212]

---
+++ m2t/jet/build/_common.php
@@ -0,0 +1,24 @@
+<?php
+require_once ("../../../includes/buildServer-common.php");
+
+$options = array (
+	"BaseBuilderBranch" => "M4_33",
+	
+	"BranchAndJDK" => array (
+		"emf.torolab.ibm.com=------------,------------",
+		"0.8.0=HEAD,/opt/sun-java2-5.0",
+		#"0.7.3=R0_7_maintenance,/opt/sun-java2-1.4",
+		
+		"build.eclipse.org=------------,------------",
+		"0.8.0=HEAD,/opt/public/common/ibm-java2-ppc-50",
+		#"0.7.3=R0_7_maintenance,/opt/public/stp/apps/IBMJava2-ppc-142" // bug 178681
+	),
+	
+	"Mapfile_Rule_Default" => 0, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, No Tagging=gen-false"
+
+	"EmailDefault" => "pelder@ca.ibm.com", // prefil email contact box with comma-sep'd list
+	
+	"Users" => array("pelder","pelder","pelder") /* build user, eclipse cvs user, IES cvs user */
+);
+
+?>
