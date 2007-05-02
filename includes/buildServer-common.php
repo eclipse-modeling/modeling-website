5f2dfda34bba031b7ffff21423633013763a444b
nickb <nickb>
2007-05-02 15:56:07 +0000

    define $isBuildDotEclipseServer globally

--- includes/buildServer-common.php
+++ includes/buildServer-common.php
@@ -1,4 +1,5 @@
 <?php
 	# allows server-specific path assignment, eg., for build.eclipse.org
-	$_SERVER['DOCUMENT_ROOT'] = $_SERVER["SERVER_NAME"] == "build.eclipse.org" ? "/opt/public/modeling" : $_SERVER['DOCUMENT_ROOT'];
+	$isBuildDotEclipseServer = $_SERVER["SERVER_NAME"] == "build.eclipse.org";
+	$_SERVER['DOCUMENT_ROOT'] = $isBuildDotEclipseServer ? "/opt/public/modeling" : $_SERVER['DOCUMENT_ROOT'];
 ?>
