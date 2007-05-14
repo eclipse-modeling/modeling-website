1a7c15e470799a5d5160fcfc705e07037c05867f
nickb <nickb>
2007-05-14 23:11:45 +0000

    add jcrm and search

--- emft/downloads/index.php
+++ emft/downloads/index.php
@@ -24,6 +24,22 @@ $dls = array(
 			"Examples" => "examples",
 			"Automated Tests" => "automated-tests"
 		)
+	),
+	"/jcrm" => array(
+		"JCR Management" => array(
+			"SDK (Runtime, Source)" => "SDK",
+			"Runtime" => "runtime",
+			"Examples" => "examples",
+			"Automated Tests" => "automated-tests"
+		)
+	),
+	"/search" => array(
+		"Search" => array(
+			"SDK (Runtime, Source)" => "SDK",
+			"Runtime" => "runtime",
+			"Examples" => "examples",
+			"Automated Tests" => "automated-tests"
+		)
 	)
 );
 
@@ -50,7 +66,8 @@ $pageAuthor = "Neil Skrypuch, Nick Boldt";
 # Generate the web page
 $App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="/modeling/includes/downloads.css"/>' . "\n");
 $App->AddExtraHtmlHeader('<script src="/modeling/includes/downloads.js" type="text/javascript"></script>' . "\n"); //ie doesn't understand self closing script tags, and won't even try to render the page if you use one
-$App->AddExtraHtmlHeader('<link type="application/rss+xml" rel="alternate" title="EMFT '.$trans[$projct].' Build Feed" href="http://www.eclipse.org/downloads/download.php?file=/'.$PR.'/feeds/builds-'.$projct.'.xml"/>' . "\n");
+$App->AddExtraHtmlHeader('<link type="application/rss+xml" rel="alternate" title="EMFT '.$trans[$projct].' Build Feed" href="http://www.eclipse.org/downloads/download.php?file=/technology/emft/feeds/builds-'.$projct.'.xml"/>' . "\n");
+//$App->AddExtraHtmlHeader('<link type="application/rss+xml" rel="alternate" title="EMFT '.$trans[$projct].' Build Feed" href="http://www.eclipse.org/downloads/download.php?file=/'.$PR.'/feeds/builds-'.$projct.'.xml"/>' . "\n");
 $App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
 
 ?>
