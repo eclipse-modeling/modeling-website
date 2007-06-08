b0389bb4dc2bb7e1d0c96bdf5e1b416a8314d7da
nickb <nickb>
2007-06-08 20:01:23 +0000

    [190525] fix echoed variables

--- includes/changeset-common.php
+++ includes/changeset-common.php
@@ -84,15 +84,17 @@ function changeset($bugid, $html = false)
 		$out .= "# $dirVar; path alias for $row[0]\n";
 		$out .= "$dirVar=\"".$row[0]."\";\n\n";
 
-		$mid .= "rm -i $dirVar/changeset_$bugid.patch\n";
+		$mid .= "rm -i \$$dirVar/changeset_$bugid.patch;\n";
 		
 		$end .= "if [[ \$applyPatch -eq 1 ]]; then\n";
-		$end .= "  pushd $dirVar && patch -p0 <changeset_$bugid.patch; popd;\n";
+		$end .= "  pushd \$$dirVar && patch -p0 <changeset_$bugid.patch; popd;\n";
 		$end .= "fi\n";
 		$dirVar = null;
 	}
 
-	$out .= "cd \$workspace\n";
+	$out .= "#### CONFIGURATION DONE ####\n\n";
+	
+	$out .= "cd \$workspace;\n";
 	$out .= "\n# remove any existing versions of this patch\n".$mid."\n";
 
 	$result = wmysql_query("SELECT `cvsname`, `revision` FROM `cvsfiles` NATURAL JOIN `commits` NATURAL LEFT JOIN `bugs` WHERE `bugid` = $bugid GROUP BY `fid`, `revision`, `bugid` ORDER BY `date` DESC");
@@ -105,21 +107,21 @@ function changeset($bugid, $html = false)
 			if (preg_match("/^1\.1$/", $row[1]))
 			{
 				$out .= "if [[ \$applyPatch -eq 1 ]]; then\n";
-				$out .= "  pushd $dirVar && cvs up -r1.1 $m[2]; popd;\n";
+				$out .= "  pushd \$$dirVar && cvs up -r1.1 $m[2]; popd;\n";
 				$out .= "fi\n";
 				$note .= "if [[ \$applyPatch -eq 1 ]]; then\n";
-				$note .= "  echo '[NOTE] $dirVar/$m[2] was added in this changeset. File has been checked out locally but is not in the patch.\n";
+				$note .= "  echo '[NOTE] \$$dirVar/$m[2] was added in this changeset. File has been checked out locally but is not in the patch.\n";
 				$note .= "else\n";
-				$note .= "  echo '[NOTE] $dirVar/$m[2] was added in this changeset. You will have to check it out manually:'\n";
-				$note .= "  echo '  pushd $dirVar && cvs up -r1.1 $m[2]; popd;'\n";
+				$note .= "  echo '[NOTE] \$$dirVar/$m[2] was added in this changeset. You will have to check it out manually:'\n";
+				$note .= "  echo '  pushd \$$dirVar && cvs up -r1.1 $m[2]; popd;'\n";
 				$note .= "fi\n";
 			}
 			else
 			{
 				$out .= "if [[ \$pluginsInWorkspace -eq 1 ]]; then\n";
-				$out .= "  pushd \$dirVar && cvs diff -u -r" . cvsminus($row[1]) . " -r$row[1] " . cleanPath($m[2]) . " >>changeset_$bugid.patch; popd; echo;\n";
+				$out .= "  pushd \$$dirVar && cvs diff -u -r" . cvsminus($row[1]) . " -r$row[1] " . cleanPath($m[2]) . " >>changeset_$bugid.patch; popd; echo;\n";
 				$out .= "else\n";
-				$out .= "  pushd \$dirVar && cvs diff -u -r" . cvsminus($row[1]) . " -r$row[1] $m[2] >>changeset_$bugid.patch; popd; echo;\n";
+				$out .= "  pushd \$$dirVar && cvs diff -u -r" . cvsminus($row[1]) . " -r$row[1] $m[2] >>changeset_$bugid.patch; popd; echo;\n";
 				$out .= "fi\n";
 			}
 		}
@@ -138,6 +140,6 @@ function dir2var($in)
 
 function cleanPath($in)
 {
-	return preg_replace("#plugins/|tests/|features/|examples/|doc/#","",$in);
+	return preg_replace("#plugins/|tests/|features/|examples/|doc/|modeling/#","",$in);
 }
 ?>
