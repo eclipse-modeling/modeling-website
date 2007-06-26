e7367fdcbeeab43c68568bf980f8581fcdcb62d6
nickb <nickb>
2007-06-26 19:03:46 +0000

    rename log file for better sorting in dir

--- build/promo-common.php
+++ build/promo-common.php
@@ -252,7 +252,7 @@ else
 	// echo "got: cvsbranch: $cvsbranch, ID: $ID, BR: $BR<br/>";
 
 	$logdir = "/home/www-data/promo_logs/";
-	$logfile = "promo_log_" . ($_POST["build_Close_Bugz_Only"] != "" ? 'bugzonly_' : '') . $BR . "." . $ID . "_" . date("YmdHis") . ".txt";
+	$logfile = "promo_log_" . $BR . "." . $ID . "_" . date("YmdHis") . ($_POST["build_Close_Bugz_Only"] != "" ? '_bugzonly' : '') . ".txt";
 
 	if (!$previewOnly)
 	{
