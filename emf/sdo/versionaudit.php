372c137af6620ab8ac26354218a68da9f668f07d
nickb <nickb>
2007-07-13 00:04:10 +0000

    [192773] no need to trap project= value for emf/sdo

---
+++ emf/sdo/versionaudit.php
@@ -0,0 +1,11 @@
+<?php
+
+$dirs = array(
+	"HEAD" => "/opt/public/modeling/searchcvs/cvssrc/org.eclipse.emf.ecore.sdo",
+	"R2_2_maintenance" => "/opt/public/modeling/searchcvs/cvssrc_branches/org.eclipse.emf.ecore.sdo-R2_2_maintenance",
+	"R2_1_maintenance" => "/opt/public/modeling/searchcvs/cvssrc_branches/org.eclipse.emf.ecore.sdo-R2_1_maintenance",
+	"R2_0_maintenance" => "/opt/public/modeling/searchcvs/cvssrc_branches/org.eclipse.emf.ecore.sdo-R2_0_maintenance"
+);
+
+include($_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/versionaudit-common.php");
+?>
