3c04c59dd45f788f305867745c1e9f81168f8273
nickb <nickb>
2007-07-14 21:51:13 +0000

    [196412] move into new paths

--- emf/emf/versionaudit.php
+++ emf/emf/versionaudit.php
@@ -2,10 +2,10 @@
 require_once ("../../includes/buildServer-common.php");
 
 $dirs = array(
-	"HEAD" => "/opt/public/modeling/searchcvs/cvssrc/org.eclipse.emf",
-	"R2_2_maintenance" => "/opt/public/modeling/searchcvs/cvssrc_branches/org.eclipse.emf-R2_2_maintenance",
-	"R2_1_maintenance" => "/opt/public/modeling/searchcvs/cvssrc_branches/org.eclipse.emf-R2_1_maintenance",
-	"R2_0_maintenance" => "/opt/public/modeling/searchcvs/cvssrc_branches/org.eclipse.emf-R2_0_maintenance"
+	"HEAD" => "/opt/public/modeling/searchcvs/cvssrc/emf-org.eclipse.emf",
+	"R2_2_maintenance" => "/opt/public/modeling/searchcvs/cvssrc_branches/emf-org.eclipse.emf-R2_2_maintenance",
+	"R2_1_maintenance" => "/opt/public/modeling/searchcvs/cvssrc_branches/emf-org.eclipse.emf-R2_1_maintenance",
+	"R2_0_maintenance" => "/opt/public/modeling/searchcvs/cvssrc_branches/emf-org.eclipse.emf-R2_0_maintenance"
 );
 
 include_once($_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/versionaudit-common.php");
