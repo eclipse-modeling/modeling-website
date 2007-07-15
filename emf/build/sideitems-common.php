a6365b12f741b5cf70a870e73c73a4abe30d5894
nickb <nickb>
2007-07-15 08:24:55 +0000

    [192773] link to versionaudit & updateSearchCVS; also, download stats

--- emf/build/sideitems-common.php
+++ emf/build/sideitems-common.php
@@ -15,15 +15,23 @@ function sidebar()
 			<li>QTV: <a href="/modeling/emf/<?php print $projct && $projct != "emf" ? $projct : "query"; ?>/build/">Build</a>, 
 					 <a href="/modeling/emf/<?php print $projct && $projct != "emf" ? $projct : "query"; ?>/build/promo.php">Promote</a></li>
 		</ul>
+		<hr noshade="noshade" size="1" width="90%"/>
+		<ul>
+			<li>Update: <a href="http://build.eclipse.org/modeling/build/updateSearchCVS.php">Search CVS Data</a></li>
+			<li>Audit: 
+					 <a href="http://build.eclipse.org/modeling/emf/emf/versionaudit.php">EMF</a>, 
+					 <a href="http://build.eclipse.org/modeling/mdt/xsd/versionaudit.php">XSD</a>, 
+					 <a href="http://build.eclipse.org/modeling/emf/sdo/versionaudit.php">SDO</a></li>
+		</ul>
 	</div>
 
 	<div class="sideitem">
 		<h6>Info</h6>
 		<ul>
 			<li><a href="https://bugs.eclipse.org/bugs/buglist.cgi?product=<?php print $projct == "emf" ? "EMF,MDT&amp;component=Core,Doc,Edit,Mapping,Releng,SDO,Edit,Tools,Website,XML%2FXMI,XSD" : "EMF,EMFT"; ?>&amp;bug_status=ASSIGNED">Assigned Bugs</a></li>
-			<li><a href="http://www.eclipse.org/modeling/mdt/searchcvs.php?q=branch%3A+HEAD+days%3A+7">Development This Week</a></li>
-			<li><a href="http://www.eclipse.org/modeling/mdt/searchcvs.php?q=branch%3A+R+days%3A+7">Maintenance This Week</a></li>
-			<!-- <li><a href="http://emf.torolab.ibm.com/<?php print $PR; ?>/downloads/downloads.php">Download Stats</a></li> -->
+			<li><a href="http://www.eclipse.org/modeling/emf/searchcvs.php?q=branch%3A+HEAD+days%3A+7">Development This Week</a></li>
+			<li><a href="http://www.eclipse.org/modeling/emf/searchcvs.php?q=branch%3A+R+days%3A+7">Maintenance This Week</a></li>
+			<li><a href="http://www.eclipse.org/modeling/emf/downloads/stats.php">Download Stats</a></li>
 		</ul>
 	</div>
 <?php	if ($isEMFserver && $projct == "emf") { ?>
