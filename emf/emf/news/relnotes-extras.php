487152ca1a8e729d2dd54dc91cb2cba0461b1fae
nickb <nickb>
2007-09-10 22:37:34 +0000

    [194263] show .x links on right nav

--- emf/emf/news/relnotes-extras.php
+++ emf/emf/news/relnotes-extras.php
@@ -7,7 +7,10 @@ function sideitems()
 		<p>
 			<ul>
 				<li><a href="http://www.eclipse.org/modeling/emf/news/relnotes.php?project=emf&amp;version=HEAD">Latest</a></li>
-				<li><a href="http://www.eclipse.org/modeling/emf/emf/news/relnotes2.0.php#emf">EMF 2.0</a></li>
+				<li><a href="http://www.eclipse.org/modeling/emf/news/relnotes.php?project=emf&amp;version=2.3.x">EMF 2.3.x</a></li>
+				<li><a href="http://www.eclipse.org/modeling/emf/news/relnotes.php?project=emf&amp;version=2.2.x">EMF 2.2.x</a></li>
+				<li><a href="http://www.eclipse.org/modeling/emf/news/relnotes.php?project=emf&amp;version=2.1.x">EMF 2.1.x</a></li>
+				<li><a href="http://www.eclipse.org/modeling/emf/news/relnotes.php?project=emf&amp;version=2.0.x">EMF 2.0.1+</a>, <a href="http://www.eclipse.org/modeling/emf/emf/news/relnotes2.0.php#emf">2.0.0</a></li>
 				<li><a href="http://www.eclipse.org/modeling/emf/emf/news/relnotes1.x.php#emf">EMF 1.x</a></li>
 			</ul>
 		</p>
