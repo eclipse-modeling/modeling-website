928e6a33f3ad2e0ffdca302d5ba8361697eebc93
nickb <nickb>
2007-09-25 21:56:29 +0000

    [204600]

---
+++ emft/mwe/build/_common.php
@@ -0,0 +1,22 @@
+<?php
+require_once ("../../../includes/buildServer-common.php");
+
+$options = array (
+	"BaseBuilderBranch" => "v20070614",
+	
+	"BranchAndJDK" => array (
+		"emft.eclipse.org=------------,------------",
+		"0.7.0=HEAD,/opt/sun-java2-5.0",
+		
+		"build.eclipse.org=------------,------------",
+		"0.7.0=HEAD,/opt/public/common/ibm-java2-ppc-50"
+	),
+	
+	"Mapfile_Rule_Default" => 0, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, No Tagging=gen-false"
+
+	"EmailDefault" => "b.kolb@kolbware.de", // prefil email contact box with comma-sep'd list
+	
+	"Users" => array("bkolb","bkolb",null) /* build user, eclipse cvs user, IES cvs user */
+);
+
+?>
