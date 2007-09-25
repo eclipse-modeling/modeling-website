cd0ed961ab3046133f5fb9497062a395be0cf4b5
nickb <nickb>
2007-09-25 15:45:39 +0000

    add R2_3_maintenance

--- emf/sdo/versionaudit.php
+++ emf/sdo/versionaudit.php
@@ -3,6 +3,7 @@ require_once ("../../includes/buildServer-common.php");
 
 $dirs = array(
 	"HEAD" => "/opt/public/modeling/searchcvs/cvssrc/emf-org.eclipse.emf.ecore.sdo",
+	"R2_3_maintenance" => "/opt/public/modeling/searchcvs/cvssrc_branches/emf-org.eclipse.emf.ecore.sdo-R2_3_maintenance",
 	"R2_2_maintenance" => "/opt/public/modeling/searchcvs/cvssrc_branches/emf-org.eclipse.emf.ecore.sdo-R2_2_maintenance",
 	"R2_1_maintenance" => "/opt/public/modeling/searchcvs/cvssrc_branches/emf-org.eclipse.emf.ecore.sdo-R2_1_maintenance",
 	"R2_0_maintenance" => "/opt/public/modeling/searchcvs/cvssrc_branches/emf-org.eclipse.emf.ecore.sdo-R2_0_maintenance"
