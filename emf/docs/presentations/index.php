6bbf3086c02244fa5fc2d992fbe71a5e6eedbf1e
nickb <nickb>
2007-10-04 22:55:23 +0000

    fix for missing header.php & formatting tweaks

--- emf/docs/presentations/index.php
+++ emf/docs/presentations/index.php
@@ -1,25 +1,17 @@
 <?php 
-require_once($_SERVER['DOCUMENT_ROOT'] ."/emf/includes/header.php"); 
-
-if (!$isWWWserver) 
-{ 
-	header("Location: http://www.eclipse.org/modeling/emf/docs/");
-	exit;
-}
-
 require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php"); require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php");  require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); $App = new App(); $Nav = new Nav(); $Menu = new Menu(); include($App->getProjectCommon());
 ob_start();
 
 $dirName = str_replace("/index.php","",$_SERVER["SCRIPT_NAME"]);
 $dirName = explode("/",$dirName); $dirName = $dirName[sizeof($dirName)-1]; 
-$files = loadDirSimple($_SERVER['DOCUMENT_ROOT'] . "/emf/docs/presentations/" . ($dirName!='presentations'?$dirName:""), ".*", $dirName!='presentations'?"f":"d");
+$files = loadDirSimple(".", ".*", $dirName!='presentations'?"f":"d");
 print '<div id="midcolumn">
 
 <div class="homeitem3col">
-<h3>Presentation Materials '.($dirName!='presentations'?'From '.$dirName:'').'</h3>
+<h3>Presentation Materials'.($dirName!='presentations'?' - ' . ucfirst($dirName):'').'</h3>
 <ul>
 ';
-if (sizeof($files)>2) { 
+if (sizeof($files)>0) { 
 	rsort($files);
 	foreach ($files as $file) { 
 		if ($file != "CVS" && $file !="index.php")
