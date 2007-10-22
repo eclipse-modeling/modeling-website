ee0c94b4954d244e4662fc5a0559ecf647b3d12a
nickb <nickb>
2007-10-22 04:55:56 +0000

    typo

--- emf/emf/build/_common.php
+++ emf/emf/build/_common.php
@@ -6,17 +6,17 @@ $options = array (
 	
 	"BranchAndJDK" => array (
 	"emf.torolab.ibm.com=------------,------------",
-        "2.4.0=HEAD,/opt/sun-java2-5.0",
+	"2.4.0=HEAD,/opt/sun-java2-5.0",
 	"2.3.2=R2_3_maintenance,/opt/sun-java2-5.0",
-        "2.2.5=R2_2_maintenance,/opt/sun-java2-1.4",
-        "2.1.4=R2_1_maintenance,/opt/sun-java2-1.4",
+	"2.2.5=R2_2_maintenance,/opt/sun-java2-1.4",
+	"2.1.4=R2_1_maintenance,/opt/sun-java2-1.4",
 	"2.0.7=R2_0_maintenance,/opt/sun-java2-1.4",
 		
 	"build.eclipse.org=------------,------------",
-        "2.4.0=HEAD,/opt/public/common/ibm-java2-ppc-50",
-        "2.3.2=R2_3_maintenance,/opt/public/common/ibm-java2-ppc-50",
-        "2.2.5=R2_2_maintenance,/opt/public/stp/apps/IBMJava2-ppc-142",
-        "2.1.4=R2_1_maintenance,/opt/public/stp/apps/IBMJava2-ppc-142"
+	"2.4.0=HEAD,/opt/public/common/ibm-java2-ppc-50",
+	"2.3.2=R2_3_maintenance,/opt/public/common/ibm-java2-ppc-50",
+	"2.2.5=R2_2_maintenance,/opt/public/stp/apps/IBMJava2-ppc-142",
+	"2.1.4=R2_1_maintenance,/opt/public/stp/apps/IBMJava2-ppc-142",
 	"2.0.7=R2_0_maintenance,/opt/public/stp/apps/IBMJava2-ppc-142" // 178681
 	),
 	
