cc2ddcaa0a7fd08498faa7b89f649c8d8950ffd8
nickb <nickb>
2007-12-13 20:30:55 +0000

    [212926] missing vars

--- m2m/_projectCommon.php
+++ m2m/_projectCommon.php
@@ -1,6 +1,7 @@
 <?php
 
 $PR = "modeling/m2m";
+$projectName = "M2M";
 
 $isBuildServer = (preg_match("/^(emft|build)\.eclipse\.org$/", $_SERVER["SERVER_NAME"]));
 $isBuildDotEclipseServer = $_SERVER["SERVER_NAME"] == "build.eclipse.org";
@@ -10,8 +11,62 @@ $debug = (isset ($_GET["debug"]) && preg_match("/^\d+$/", $_GET["debug"]) ? $_GE
 $writableRoot = ($isBuildServer ? $_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/" : "/home/data/httpd/writable/www.eclipse.org/");
 $writableBuildRoot = $isBuildDotEclipseServer ? "/opt/public/modeling" : "/home/www-data";
 
+$rooturl = "http://" . $_SERVER["HTTP_HOST"] . "/$PR";
+$downurl = ($isBuildServer ? "" : "http://www.eclipse.org");
+$bugurl = "https://bugs.eclipse.org";
+
+if (isset ($_GET["skin"]) && preg_match("/^(Blue|EclipseStandard|Industrial|Lazarus|Miasma|Modern|OldStyle|Phoenix|PhoenixTest|PlainText)$/", $_GET["skin"], $regs))
+{
+	$theme = $regs[1];
+}
+else
+{
+	$theme = "Phoenix";
+}
+
+/* projects/components in cvs */
+/* "proj" => "cvsname" */
+$cvsprojs = array (); /* should always be empty */
+
+/* sub-projects/components in cvs for projects/components above (if any) */
+/* "cvsname" => array("shortname" => "cvsname") */
+$cvscoms = array(
+	"org.eclipse.m2m" => array (
+		"atl" => "org.eclipse.atl",
+		/* add more here */
+	)
+);
+
+$projects = array(
+	"ATL" => "atl",
+);
+
+
+$bugcoms = array_flip($projects);
+$bugcoms = preg_replace("/ /", "%20", $bugcoms);
+
+$extraprojects = array(); //components with only downloads, no info yet, "prettyname" => "directory"
+$nodownloads = array("atl"); //components with only information, no downloads, or no builds available yet, "projectkey"
+$nonewsgroup = array("atl"); //components without newsgroup
+$nomailinglist = array("atl"); //components without mailinglist
+$incubating = $projects; // ALL components are incubating
+$nomenclature = "Component"; //are we dealing with "components" or "projects"?
+
 include_once $_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/scripts.php";
+
+$regs = null;
+$proj = (isset($_GET["project"]) && preg_match("/^(" . join("|", $projects) . ")$/", $_GET["project"], $regs) ? $regs[1] : getProjectFromPath($PR));
+$projct= preg_replace("#^/#", "", $proj);
+
+$buildtypes = array(
+	"R" => "Release",
+	"S" => "Stable",
+	"I" => "Integration",
+	"M" => "Maintenance",
+	"N" => "Nightly"
+);
 
 $Nav->addNavSeparator("Project Home", 	"/m2m/");
 
+unset ($bugcoms);
 ?>
