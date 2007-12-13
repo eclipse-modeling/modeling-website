aa5a39fb7eb89e2a7fc77d76b14818e0e1ceb1eb
nickb <nickb>
2007-12-13 20:11:25 +0000

    [212926] m2m atl component builds require web UI

---
+++ m2m/atl/build/_common.php
@@ -0,0 +1,22 @@
+<?php
+require_once ("../../../includes/buildServer-common.php");
+
+$options = array (
+	"BaseBuilderBranch" => "v20071108",
+	
+	"BranchAndJDK" => array (
+		"emft.eclipse.org=------------,------------",
+		"0.7.0=HEAD,/opt/sun-java2-5.0",
+		
+		"build.eclipse.org=------------,------------",
+        "0.7.0=HEAD,/opt/sun-java2-5.0",
+	),
+	
+	"Mapfile_Rule_Default" => 0, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, No Tagging=gen-false"
+
+	"EmailDefault" => "wpiers@obeo.fr", // prefil email contact box with comma-sep'd list
+	
+	"Users" => array("wpiers","wpiers",null) /* build user, eclipse cvs user, IES cvs user */
+);
+
+?>
