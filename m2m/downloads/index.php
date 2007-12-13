ebc4a28cf0eee4d9be283c6ce5fb4636b0b1d8e3
nickb <nickb>
2007-12-13 21:06:19 +0000

    [212926]

---
+++ m2m/downloads/index.php
@@ -0,0 +1,58 @@
+<?php
+require_once ("../../includes/buildServer-common.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php"); require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php");  require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); $App = new App(); $Nav = new Nav(); $Menu = new Menu(); include($App->getProjectCommon());
+
+/* temporary redirect for emft projects, except on build servers if downloads folder exists */
+if (isset($_GET["project"]) && isset($emft_redirects) && is_array($emft_redirects) && in_array($_GET["project"],$emft_redirects))
+{
+	header("Location: http://www.eclipse.org/emft/downloads/?project=" . $_GET["project"]);
+	exit;
+}
+ob_start();
+
+/* config */
+
+/* zips that are allowed to be absent from the downloads page (eg., new ones added mid-stream) */
+$extraZips = array(
+);
+	
+/* $project => sections/Project Name => (prettyname => filename) */
+/* only required if using something other than the default 4; otherwise will be generated */
+$dls = array(
+	/*"/newProj" => array(
+		"Project Name" => array( # same as value in _projectCommon.php's $projects array
+			"SDK (Runtime, Source)" => "SDK",
+			"Runtime" => "runtime",
+			"Examples" => "examples",
+			"Automated Tests" => "automated-tests"
+		)
+	),*/
+);
+
+/* list of valid file prefixes for projects who have been renamed; keys have leading / to match $proj */
+/* only required if using something other than the default; otherwise will be generated */
+$filePre = array(
+	/* "/newproj" => array("m2m-newproj"), */
+);
+
+/* define showNotes(), $oldrels, doLanguagePacks() in extras-$proj.php (or just extras.php for flat projects) if necessary, downloads-common.php will include them */
+/* end config */
+
+if ($isBuildServer) { include_once $_SERVER["DOCUMENT_ROOT"] . "/modeling/m2m/build/sideitems-common.php"; }
+require_once($_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/downloads-common.php");
+
+$html = ob_get_contents();
+ob_end_clean();
+
+$trans = array_flip($projects);
+$pageTitle = "Eclipse Modeling - M2M - $trans[$projct] - Downloads";
+$pageKeywords = ""; // TODO: add something here
+$pageAuthor = "";
+
+# Generate the web page
+$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="/modeling/includes/downloads.css"/>' . "\n");
+$App->AddExtraHtmlHeader('<script src="/modeling/includes/downloads.js" type="text/javascript"></script>' . "\n"); //ie doesn't understand self closing script tags, and won't even try to render the page if you use one
+$App->AddExtraHtmlHeader('<link type="application/rss+xml" rel="alternate" title="M2M '.$trans[$projct].' Build Feed" href="http://www.eclipse.org/downloads/download.php?file=/'.$PR.'/feeds/builds-'.$projct.'.xml"/>' . "\n");
+$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+
+?>
