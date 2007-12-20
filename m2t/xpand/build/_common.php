3c7e6230362fff539c14fe7d51d0da9752aa67ce
bkolb <bkolb>
2007-12-20 22:47:49 +0000

    initial

---
+++ m2t/xpand/build/_common.php
@@ -0,0 +1,22 @@
+<?php
+require_once ("../../../includes/buildServer-common.php");
+
+$options = array (
+	"BaseBuilderBranch" => "v20071108",
+	
+	"BranchAndJDK" => array (
+		"emf.torolab.ibm.com=------------,------------",
+        "0.7.0=HEAD,/opt/sun-java2-5.0",
+		
+		"build.eclipse.org=------------,------------",
+        "0.7.0=HEAD,/opt/sun-java2-5.0",
+	),
+	
+	"Mapfile_Rule_Default" => 0, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, No Tagging=gen-false"
+
+	"EmailDefault" => "b.kolb@kolbware.de", // prefil email contact box with comma-sep'd list
+	
+	"Users" => array("bkolb","bkolb","bkolb") /* build user, eclipse cvs user, IES cvs user */
+);
+
+?>
