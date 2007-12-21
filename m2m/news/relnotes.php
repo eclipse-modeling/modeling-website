77a00deaf5a8d6ed80d993d3dd17d215770f7b53
nickb <nickb>
2007-12-21 00:34:27 +0000

    add $streams for atl

--- m2m/news/relnotes.php
+++ m2m/news/relnotes.php
@@ -3,5 +3,10 @@ require_once ("../../includes/buildServer-common.php");
 
 $pageTitle = "Eclipse Modeling - M2M - Release Notes";
 
+$streams = array(
+	"atl" => array(
+		"2.0.x" => "HEAD",
+	),
+
 require ($_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/relnotes-common.php");
 ?>
