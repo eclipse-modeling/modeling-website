c1e75f7d7821eddc29a3100d83bb1b01a612874a
nickb <nickb>
2007-12-21 00:11:59 +0000

    add news

---
+++ m2m/news/relnotes.php
@@ -0,0 +1,7 @@
+<?php
+require_once ("../../includes/buildServer-common.php");
+
+$pageTitle = "Eclipse Modeling - M2M - Release Notes";
+
+require ($_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/relnotes-common.php");
+?>
