5e5b24db3c8addd7256a54c5c88e441048757852
nickb <nickb>
2007-12-22 23:46:51 +0000

    *** empty log message ***

--- build/checkBuildStatus.php
+++ build/checkBuildStatus.php
@@ -62,7 +62,7 @@ if (is_readable("/home/www-data/build/modeling/" . $data["top"] . "/" . $data["p
 }
 else
 {
-	print "Status\tUNKNOWN\t(" . $data["top"] . "/" . $data["project"] . "/downloads/drops/" . $data["version"] . "/" . $data["buildID"] . ")\n";
+	print "Status\tUNKNOWN\t(" . $data["top"] . "/" . $data["project"] . "/downloads/drops/" . $data["version"] . "/" . $data["buildID"] . ")\n\n";
 }
 
 /**********************************************************************************************************************************/
