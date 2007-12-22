63a0aa9f2818bf98a69b775e93564681d7132c91
nickb <nickb>
2007-12-22 23:53:38 +0000

    default assignee

--- emf/emf/build/_common.php
+++ emf/emf/build/_common.php
@@ -3,7 +3,7 @@ require_once ("../../../includes/buildServer-common.php");
 
 $options = array (
 	"BaseBuilderBranch" => "v20071108",
-	
+
 	"BranchAndJDK" => array (
 	"emf.torolab.ibm.com=------------,------------",
 	"2.4.0=HEAD,/opt/sun-java2-5.0",
@@ -11,7 +11,7 @@ $options = array (
 	"2.2.5=R2_2_maintenance,/opt/sun-java2-1.4",
 	"2.1.4=R2_1_maintenance,/opt/sun-java2-1.4",
 	"2.0.7=R2_0_maintenance,/opt/sun-java2-1.4",
-		
+
 	"build.eclipse.org=------------,------------",
 	"2.4.0=HEAD,/opt/public/common/ibm-java2-ppc-50",
 	"2.3.2=R2_3_maintenance,/opt/public/common/ibm-java2-ppc-50",
@@ -19,11 +19,11 @@ $options = array (
 	"2.1.4=R2_1_maintenance,/opt/public/stp/apps/IBMJava2-ppc-142",
 	"2.0.7=R2_0_maintenance,/opt/public/stp/apps/IBMJava2-ppc-142" // 178681
 	),
-	
+
 	"Mapfile_Rule_Default" => 1, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, No Tagging=gen-false"
 
-	"EmailDefault" => "", // prefil email contact box with comma-sep'd list
-	
+	"EmailDefault" => "nickboldt@gmail.com", // prefil email contact box with comma-sep'd list
+
 	"Users" => array("nickb","nickb","nboldt") /* build user, eclipse cvs user, IES cvs user */
 );
 
