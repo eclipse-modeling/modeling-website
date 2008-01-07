1934c88b97029e643929241538a06df027108726
jcote <jcote>
2008-01-07 02:13:45 +0000

    *** empty log message ***

---
+++ emft/temporality/build/_common.php
@@ -0,0 +1,24 @@
+<?php
+require_once ("../../../includes/buildServer-common.php");
+
+$options = array (
+	"BaseBuilderBranch" => "v20071108",
+	
+	"BranchAndJDK" => array (
+		"emft.eclipse.org=------------,------------",
+		"0.7.0=HEAD,/opt/sun-java2-5.0",
+		#"0.7.1=R0_7_maintenance,/opt/sun-java2-5.0",
+		
+		"build.eclipse.org=------------,------------",
+		"0.7.0=HEAD,/opt/public/common/ibm-java2-ppc-50"
+		#"0.7.1=R0_7_maintenance,/opt/public/common/ibm-java2-ppc-50"
+	),
+	
+	"Mapfile_Rule_Default" => 0, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, No Tagging=gen-false"
+
+	"EmailDefault" => "jccote@gmail.com", // prefil email contact box with comma-sep'd list
+	
+	"Users" => array("jcote","jcote",null) /* build user, eclipse cvs user, IES cvs user */
+);
+
+?>
