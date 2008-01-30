e4e580bf1a1d014170e3d1197bba86be0338472f
nickb <nickb>
2008-01-30 22:50:00 +0000

    update

--- m2t/jet/build/_common.php
+++ m2t/jet/build/_common.php
@@ -2,24 +2,24 @@
 require_once ("../../../includes/buildServer-common.php");
 
 $options = array (
-	"BaseBuilderBranch" => "v20071108",
-	
+	"BaseBuilderBranch" => "M4_34",
+
 	"BranchAndJDK" => array (
 		"emf.torolab.ibm.com=------------,------------",
         "0.9.0=HEAD,/opt/sun-java2-5.0",
 		"0.8.2=R0_8_maintenance,/opt/sun-java2-5.0",
 		"0.7.3=R0_7_maintenance,/opt/sun-java2-5.0",
-		
+
 		"build.eclipse.org=------------,------------",
         "0.9.0=HEAD,/opt/sun-java2-5.0",
         "0.8.2=R0_8_maintenance,/opt/sun-java2-5.0",
         "0.7.3=R0_7_maintenance,/opt/sun-java2-5.0",
 	),
-	
+
 	"Mapfile_Rule_Default" => 0, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, No Tagging=gen-false"
 
 	"EmailDefault" => "pelder@ca.ibm.com", // prefil email contact box with comma-sep'd list
-	
+
 	"Users" => array("pelder","pelder","pelder") /* build user, eclipse cvs user, IES cvs user */
 );
 
