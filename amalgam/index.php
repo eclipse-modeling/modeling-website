8081d9fd9400ed054c56b8d5f689f7cb01f71527
rgronback <rgronback>
2008-02-12 14:49:29 +0000

    Initial page content.

--- amalgam/index.php
+++ amalgam/index.php
@@ -2,12 +2,12 @@
 
 	#*****************************************************************************
 	#
-	# template.php
+	# index.php
 	#
-	# Author: 		Denis Roy
-	# Date:			2005-06-16
+	# Author: 		Richard Gronback
+	# Date:			2008-02-12
 	#
-	# Description: Type your page comments here - these are not sent to the browser
+	# Description: Amalgam project introductory page
 	#
 	#
 	#****************************************************************************
@@ -15,8 +15,8 @@
 	#
 	# Begin: page-specific settings.  Change these. 
 	$pageTitle 		= "Amalgam";
-	$pageKeywords	= "Type, page, keywords, here";
-	$pageAuthor		= "Type your name here";
+	$pageKeywords	= "modeling,packaging";
+	$pageAuthor		= "Richard Gronback";
 	
 	# Add page-specific Nav bars here
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
@@ -31,8 +31,50 @@
 	$html = <<<EOHTML
 
 	<div id="midcolumn">
-	  This project was just provisioned, so there is nothing to see here.
+		<table width="100%">
+		<tr><td>&nbsp;</td></tr>
+			<tr>
+				<td align="top">
+				The Amalgamation project will provide improved packaging, integration, and usability of Modeling project components. See
+				the project <a href="http://www.eclipse.org/proposals/amalgamation/">proposal</a> for more detail until this website becomes fully operational.
+				</td>
+			</tr>
+		</table><hr/>
+		
+		<div class="homeitem">
+			<h3>Quick Links</h3>
+				<ul class="midlist">
+					<li><a href="http://www.eclipse.org/proposals/amalgamation/">Project proposal</a></li>
+		</div>
+		<div class="homeitem">
+			<h3>Events</h3>
+			<ul class="midlist">
+				<li>Interested in creating a DSL using the Modeling project?  Come see this <a href="http://www.eclipsecon.org/2008/index.php?page=sub/&id=51">talk</a> at <a href="http://www.eclipsecon.org/2008/">EclipseCon 2008</a></li>
+			</ul>
+		</div>
+		<br/>
+		
+		<hr class="clearer" />
+		
 	</div>
+	
+	<div id="rightcolumn">
+		<br />
+		<div class="sideitem">
+			<h6>Getting started</h6>
+			<ul>				
+				<li>Coming</li>
+			</ul>
+		</div>
+		
+		<div class="sideitem">
+			<h6>What's New</h6>
+			<ul> 
+			    <li>January 30, 2008 - Amalgamation project <a href="http://www.eclipse.org/proposals/amalgamation/">created</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
 
 EOHTML;
 
