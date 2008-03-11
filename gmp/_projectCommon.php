219f20ef8aa6d8b0c0aa4aba31d239bce53a0eb7
nickb <nickb>
2008-03-11 17:30:13 +0000

    add missing variables

--- gmp/_projectCommon.php
+++ gmp/_projectCommon.php
@@ -1,14 +1,37 @@
 <?php
 
-$PR = "modeling/gmf";
+$PR = "modeling/m2t";
+$projectName = "GMF";
 
 $isBuildServer = (preg_match("/^(emft|build)\.eclipse\.org$/", $_SERVER["SERVER_NAME"]));
 $isBuildDotEclipseServer = $_SERVER["SERVER_NAME"] == "build.eclipse.org";
 $isWWWserver = (preg_match("/^(?:www.|)eclipse.org$/", $_SERVER["SERVER_NAME"]));
 $isEclipseCluster = (preg_match("/^(?:www.||download.|download1.|build.)eclipse.org$/", $_SERVER["SERVER_NAME"]));
+$debug = (isset ($_GET["debug"]) && preg_match("/^\d+$/", $_GET["debug"]) ? $_GET["debug"] : -1);
+$writableRoot = ($isBuildServer ? $_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/" : "/home/data/httpd/writable/www.eclipse.org/");
 $writableBuildRoot = $isBuildDotEclipseServer ? "/opt/public/modeling" : "/home/www-data";
 
+$cvscoms = array (
+	"org.eclipse.gmf" => array (
+		"gmf" => "org.eclipse.gmf",
+	)
+);
+
+$projects = array (
+	"GMF" => "gmf",
+);
 include_once $_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/scripts.php";
+$regs = null;
+$proj = (isset($_GET["project"]) && preg_match("/^(" . join("|", $projects) . ")$/", $_GET["project"], $regs) ? $regs[1] : getProjectFromPath($PR));
+$projct= preg_replace("#^/#", "", $proj);
+
+$buildtypes = array(
+	"R" => "Release",
+	"S" => "Stable",
+	"I" => "Integration",
+	"M" => "Maintenance",
+	"N" => "Nightly"
+);
 
 $Nav->addNavSeparator("Project Home", 	"/gmf/");
 
