1c3cbf68dbee412bda491a20cb9ab887f9b0a7db
nickb <nickb>
2008-03-14 19:26:04 +0000

    add missing $streams array

--- m2t/news/relnotes.php
+++ m2t/news/relnotes.php
@@ -4,4 +4,14 @@ require_once ("../../includes/buildServer-common.php");
 $pageTitle = "Eclipse Modeling - M2T - Release Notes";
 
 require ($_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/relnotes-common.php");
+$streams = array(
+	"jet" => array(
+		"0.9.0" => "HEAD",
+		"0.8.0" => "R0_8_maintenance",
+		"0.7.0" => "R0_7_maintenance",
+	),
+	"xpand" => array(
+		"0.7.0" => "HEAD",
+	),
+);
 ?>
