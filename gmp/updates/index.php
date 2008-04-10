5274e31c8afc249372842eb003706f04b167de00
nickb <nickb>
2008-04-10 03:56:41 +0000

    [151607] add sidebar w/ old sites, note about old sites, and remove incubation

--- gmp/updates/index.php
+++ gmp/updates/index.php
@@ -4,13 +4,15 @@ if ($_SERVER["SERVER_NAME"] != "www.eclipse.org") {
 }
 require_once ("../../includes/buildServer-common.php");
 require($_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/updates-common.php");
+require($_SERVER["DOCUMENT_ROOT"] . "/modeling/gmf/build/sideitems-common.php");
 
-$PRS = array(
-);
+$PRS = array();
 
 function notes()
 {
+	print "<p><i style=\"color:red\"><b>NOTE:</b> not all GMF updates have migrated to the new Update Manager site yet, " .
+			"so you'll have to use the older sites until that time. See links at right.</i></p>\n";
 }
 
-update_manager("GMF", "Eclipse Modeling", $PRS, true);
+update_manager("GMF", "Eclipse Modeling", $PRS, false);
 ?>
