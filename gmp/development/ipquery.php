8268af156fa7c28e0ba260da850e5cf105d73748
nickb <nickb>
2008-04-23 15:17:18 +0000

    [228027] extract database query from gmf-specific case into common code

--- gmp/development/ipquery.php
+++ gmp/development/ipquery.php
@@ -1,95 +1,76 @@
-<html>
-	<head></head>
-	<body>
-	
-Committers (Section 1)<br/>
-<br/>
-ahunter<br/>
-atikhomirov<br/>
-bblajer<br/>
-crevells<br/>
-mfeldman<br/>
-rgronback<br/>
-sshaw<br/>
-vramaswamy<br/>
-ldamus<br/>
-mmostafa<br/>
-dstadnik<br/>
-ashatalin<br/>
-rdvorak<br/>
-<br/>
-Developers (Section 2)<br/>
-<br/>
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php"); require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); $App = new App(); $Nav = new Nav(); $Menu = new Menu(); include($App->getProjectCommon());
+ob_start();
+
+$committers = array(
+	"ahunter",
+	"ashatalin",
+	"atikhomirov",
+	"bblajer",
+	"crevells",
+	"dstadnik",
+	"ldamus",
+	"mfeldman",
+	"mmostafa",
+	"rdvorak",
+	"rgronback",
+	"sshaw",
+	"vramaswamy",
+);
+?>
+<div id="midcolumn">
+	<div class="homeitem3col">
+		<h3>Committers (Section 1)</h3>
+		<ul>
+<?php foreach ($committers as $committer) 
+{
+	print "<li><a href=\"/$PR/searchcvs.php?q=author:$committer\"$committer</a></li>\n";
+} ?>
+		</ul>
+	</div>
+	<div class="homeitem3col">
+		<h3>Developers (Section 2)</h3>
 		<b>component, bug #, contributor, size, description</b><br/>
-		<?php
-			#
-			# Scripts for retrieving IP log information.
-			#
-		
-			# Load up the classfile
-			require_once "/home/data/httpd/eclipse-php-classes/system/dbconnection_bugs_ro.class.php";
-			
-			# Connect to database
-			$dbc 	= new DBConnectionBugs();
-			$dbh 	= $dbc->connect();
-								
-			# NOTE: product_id = 29 is GMF, bug_status = 5 is RESOLVED, resolution = 2 is FIXED, 'contributed' keyword id = 22	
-								
-			$sql_info = "SELECT 
-								attachments.filename,
-								attachments.description,
-								attachments.ispatch,
-								attachments.bug_id,
-								attachments.submitter_id,
-								LENGTH(attach_data.thedata) AS size,
-								bugs.short_desc,
-								components.name,
-								profiles.login_name
-						FROM 
-								attachments, attach_data, bugs, components, keywords, profiles 
-						WHERE
-								attachments.ispatch = 1 AND 
-								attachments.isobsolete = 0 AND
-								attachments.bug_id = bugs.bug_id AND 
-								attachments.attach_id = attach_data.id AND 
-								components.id = bugs.component_id AND 
-								bugs.bug_id = keywords.bug_id AND 
-								keywords.keywordid = 22 AND 
-								profiles.userid = attachments.submitter_id AND 
-								bugs.product_id = 29
-						ORDER BY profiles.login_name";
-														
-			
-			$rs = mysql_query($sql_info, $dbh);
-			
-			if(mysql_errno($dbh) > 0) {
-				echo "There was an error processing this request: " . $sql_info . " : ";
-				
-				# For debugging purposes - don't display this stuff in a production page.
-				echo mysql_error($dbh);
-				
-				# Mysql disconnects automatically, but I like my disconnects to be explicit.
-				$dbc->disconnect();
-				exit;
-			}
-			
-			while($myrow = mysql_fetch_assoc($rs)) {
-				echo $myrow['name'] . "," . "<a href=\"https://bugs.eclipse.org/bugs/show_bug.cgi?id=" . $myrow['bug_id'] . "\">" . $myrow['bug_id'] . "</a>" . "," . $myrow['login_name'] . "," . $myrow['size'] . "," . str_replace(",", " ", $myrow['short_desc']) . " (" . str_replace(",", " ", $myrow['description']) . ")<br/>";
-			}
-			
-			$dbc->disconnect();
-		
-			$rs 		= null;
-			$dbh 		= null;
-			$dbc 		= null;
-		?>
+<?php
+	$product_id = 29; # GMF 
+	require_once ($_SERVER['DOCUMENT_ROOT'] . "/modeling/includes/ipquery-common.php"); 
+?>
+
 <br/>	
 xPand template engine (org.eclipse.gmf.xpand, org.eclipse.gmf.xpand.editor), originally developed by Sven Efftinge for oAW component in GMT project, was refactored for application in GMF by Artem Tikhomirov.
-<br/>
-Third Party Software (Section 3)<br/>
-<br/>
-org.apache.batik_1.6,cvsroot/modeling/org.eclipse.gmf/plugins/org.apache.batik,Apache License Version 2.0 January 2004,unmodified entire package<br/>
-org.apache.xerces_2.8,maintained in Orbit,Apache License Version 2.0 January 2004,unmodified entire package<br/>
-LPG-V1.1 java runtime from http://sourceforge.net/projects/lpg,EPL v1.0<br/>
-	</body>
-</html>
+
+	</div>
+	<div class="homeitem3col">
+		<h3>Third Party Software (Section 3)</h3>
+		<ul>
+			<li>org.apache.batik_1.6,cvsroot/modeling/org.eclipse.gmf/plugins/org.apache.batik,Apache License Version 2.0 January 2004,unmodified entire package</li>
+			<li>org.apache.xerces_2.8,maintained in Orbit,Apache License Version 2.0 January 2004,unmodified entire package</li>
+			<li>LPG-V1.1 java runtime from http://sourceforge.net/projects/lpg,EPL v1.0</li>
+		</ul>
+	</div>
+</div>
+
+<div id="rightcolumn">
+	<div class="sideitem">
+		<h6>Search CVS By Author</h6>
+		<ul>
+<?php foreach ($committers as $committer) 
+{
+	print "<li><a href=\"/$PR/searchcvs.php?q=author:$committer\"$committer</a></li>\n";
+} ?>
+		</ul>
+	</div>
+</div>
+			
+
+<?php
+$html = ob_get_contents();
+ob_end_clean();
+
+$pageTitle = "Graphical Modeling Framework";
+$pageKeywords = "eclipse,project,graphical,modeling,model-driven";
+$pageAuthor = "Richard C. Gronback, Nick Boldt";
+
+$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="/modeling/includes/index.css"/>' . "\n");
+$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
