1c0eb2eda3eee58497fdeb2ac4898abe570e4392
mtaal <mtaal>
2008-04-23 15:02:27 +0000

    Updated

--- emft/downloads/extras-teneo.php
+++ emft/downloads/extras-teneo.php
@@ -5,11 +5,11 @@ function doRequirements()
 <div class="homeitem3col">
 	<h3>Build Compatibility</h3>
 		<p style="padding-left:10px">Teneo has two different current builds: the I-build (the build id starts with an I) and the 
-		S-build (the build id starts with an S). The I- and S-build are compatible with different versions of EMF/Eclipse.</p>
-		<p><b>I-build (0.8.0):</b> this build is for Eclipse 3.3/EMF 2.3</p>
-		<p><b>S-build:</b> this build is for Eclipse 3.4/EMF 2.4</p>
+		S-build (the build id starts with an S). The I- and S-build are compatible with different versions of EMF/Eclipse:</p>
+		<p style="padding-left:10px"><b>I-build (0.8.0):</b> this build is for Eclipse 3.3/EMF 2.3</p>
+		<p style="padding-left:10px"><b>S-build:</b> this build is for Eclipse 3.4/EMF 2.4</p>
 		<br/>
-	<p>The I-build 0.7.5 build is for Eclipse 3.2 and EMF 2.2. However this build/branch is not actively maintained.</p>
+	<p style="padding-left:10px">The I-build 0.7.5 build is for Eclipse 3.2 and EMF 2.2. However this build/branch is not actively maintained.</p>
 </div>
 <?php
 }
