80ed76a39e9d8155bcae6734b854e09245b63bb7
cdamus <cdamus>
2008-05-04 20:27:04 +0000

    Use latest OCL I-build, if available.

--- emf/query/build/_common.php
+++ emf/query/build/_common.php
@@ -25,7 +25,7 @@ $options = array (
 	 * matching dependencies for running a build. See also releng-common/tools/scripts/start_cron.sh
 	 * and /home/www-data/build/requests/dependencies.urls.txt 
 	 * */ 	
-	"regex" => "I200.*/eclipse-SDK-|[SR]-.*200.*/eclipse-SDK-|[ISR]200.*/emf-sdo-xsd-SDK-|[S]200.*mdt-ocl-.*SDK-",
+	"regex" => "I200.*/eclipse-SDK-|[SR]-.*200.*/eclipse-SDK-|[ISR]200.*/emf-sdo-xsd-SDK-|[ISR]200.*mdt-ocl-.*SDK-",
 
 	"Mapfile_Rule_Default" => 0, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, No Tagging=gen-false"
 
