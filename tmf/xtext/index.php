58a127c710171ee10bc8b283a7a3c5ca414fd6f9
sefftinge <sefftinge>
2008-05-09 20:57:16 +0000

    *** empty log message ***

--- tmf/xtext/index.php
+++ tmf/xtext/index.php
@@ -20,9 +20,10 @@
 	
 	# Add page-specific Nav bars here
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
-	$Nav->addNavSeparator("Downloads", 	"downloads.php");
-	$Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
-	$Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
+	$Nav->addCustomNav("Projectplan", "projectplan.php");
+	$Nav->addNavSeparator("Xtext (from oAW 4.3)", 	"http://www.openarchitectureware.org");
+	$Nav->addCustomNav("Download", "http://www.eclipse.org/gmt/oaw/download/");
+	$Nav->addCustomNav("Documentation", "http://www.eclipse.org/gmt/oaw/doc/4.2/html/contents/xtext_reference.html");
 
 	# End: page-specific settings
 	#
@@ -31,7 +32,9 @@
 	$html = <<<EOHTML
 
 	<div id="midcolumn">
-	  The Xtext project was just provisioned, so there is nothing to see here.
+	  <p>Xtext is framework/tool for the development of external textual DSLs. Just Describe your very own DSL using Xtext’s simple EBNF grammar language. The generator shipped with Xtext creates a parser, an AST-meta model (implemented in EMF) as well as a full-featured Eclipse Text Editor from that.</p>
+	  <p>The Framework integrates with technology from Eclipse Modeling such as EMF, GMF, M2T and parts of EMFT. Development with Xtext is optimized for short turnarounds, so that adding new features to an existing DSL can be done in a second.</p> 
+	  <p>Language development has never been so easy.</p>
 	</div>
 
 EOHTML;
