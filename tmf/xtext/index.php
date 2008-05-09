4803f93b66da96d1efda0b8bcaca9b315187b841
sefftinge <sefftinge>
2008-05-09 20:23:32 +0000

    testing

--- tmf/xtext/index.php
+++ tmf/xtext/index.php
@@ -15,14 +15,14 @@
 	#
 	# Begin: page-specific settings.  Change these. 
 	$pageTitle 		= "Xtext";
-	$pageKeywords	= "Type, page, keywords, here";
-	$pageAuthor		= "Type your name here";
+	$pageKeywords	= "DSLs, oAW, Xtext, Eclipse, MDD, MDSD";
+	$pageAuthor		= "Sven Efftinge";
 	
 	# Add page-specific Nav bars here
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
-	# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
-	# $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
-	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
+	$Nav->addNavSeparator("Downloads", 	"downloads.php");
+	$Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
+	$Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
 
 	# End: page-specific settings
 	#
