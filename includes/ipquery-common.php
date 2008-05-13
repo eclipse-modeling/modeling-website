9f87037dbd4f59e13ced3b97d74678273ed01305
nickb <nickb>
2008-05-13 20:46:49 +0000

    [231040] emf and mdt pages redux

--- includes/ipquery-common.php
+++ includes/ipquery-common.php
@@ -1,9 +1,8 @@
-<?php
-#
-# Script for retrieving IP log information.
-#
+<?php # Script for retrieving IP log information.
 
-# Load up the classfile
+$isFormatted = !isset($_GET["unformatted"]);
+
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php"); require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); $App = new App(); $Nav = new Nav(); $Menu = new Menu(); include($App->getProjectCommon());
 require_once "/home/data/httpd/eclipse-php-classes/system/dbconnection_bugs_ro.class.php";
 
 function doIPQuery($product_id, $isFormatted = true)
@@ -141,5 +140,132 @@ function doProductIDQuery()
 	$rs 		= null;
 	$dbh 		= null;
 	$dbc 		= null;
+}
+
+function doIPQueryPage()
+{
+	global $isFormatted, $committers, $product_id, $theme, $PR, $Menu, $Nav; 
+	sort($committers); reset($committers);
+	
+	if (!$isFormatted)
+	{
+		header("Content-type: text/plain\n\n");
+		print "Committers (Section 1)\n";
+		foreach ($committers as $committer)
+		{
+			print $committer."\n";
+		}
+		print "\n";
+		print "Developers (Section 2)\n";
+		doIPQuery($product_id, false);
+		print "\n";
+		if (isset($extra_IP) && is_array($extra_IP) && sizeof($extra_IP) > 0)
+		{
+			print "Additional IP\n";
+			foreach ($extra_IP as $ip)
+			{
+				print "$ip\n";
+			} 
+		}
+		print "\n";
+		print "Third Party Software (Section 3)\n";
+		if (isset($third_party) && is_array($third_party) && sizeof($third_party) > 0)
+		{
+			foreach ($third_party as $tp)
+			{
+				print "$tp\n";
+			}
+		}
+		exit;
+	}
+	
+	$projct= preg_replace("#.+/#", "", $PR);
+	$projectName = $projct != "modeling" ? strtoupper($projct) : "Modeling Project";
+	
+	ob_start();
+	?>
+	<div id="midcolumn">
+		<div class="homeitem3col">
+			<a name="section1"></a><h3>Committers (Section 1)</h3>
+			<ul>
+				<li>See list at right.</li>
+			</ul>
+		</div>
+		<div class="homeitem3col">
+			<a name="section2"></a><h3>Developers (Section 2)</h3>
+			<?php doIPQuery($product_id, true); ?>
+			<p>
+	 		<?php if (isset($extra_IP) && is_array($extra_IP) && sizeof($extra_IP) > 0)
+			{
+				print "<a name=\"section2a\"></a><b>Additional IP</b>\n";
+				print "<ul>\n";
+				foreach ($extra_IP as $ip)
+				{
+					print "<li>$ip</li>\n";
+				}
+				print "</ul>\n";
+			} ?>
+		</div>
+		<div class="homeitem3col">
+			<a name="section3"></a><h3>Third Party Software (Section 3)</h3>
+			<ul>
+			<?php if (isset($third_party) && is_array($third_party) && sizeof($third_party) > 0)
+			{
+				foreach ($third_party as $tp)
+				{
+					print "<li>$tp</li>\n";
+				}
+			} 
+			else
+			{
+				print "<li>None.</li>\n"; 
+			} ?>
+			</ul>
+		</div>
+	</div>
+	
+	<div id="rightcolumn">
+		<div class="sideitem">
+			<h6>Committers (Section 1)</h6>
+			<ul>
+	<?php foreach ($committers as $committer) 
+	{
+		print "<li><a href=\"/$PR/searchcvs.php?q=author:$committer\">$committer</a></li>\n";
+	} ?>
+			</ul>
+		</div>
+		<div class="sideitem">
+			<h6>Developers (Section 2)</h6>
+			<ul>
+				<li><a href="#section2">Developers (Section 2)</a></li>
+				<?php if (isset($extra_IP) && is_array($extra_IP) && sizeof($extra_IP) > 0) {
+					print '<li><a href="#section2a">Additional IP</a></li>'."\n";
+				} ?>
+			</ul>
+		</div>
+		<div class="sideitem">
+			<h6>Third Party Software (Section 3)</h6>
+			<ul>
+				<li><a href="#section3">Third Party Software (Section 3)</a></li>
+			</ul>
+		</div>
+		<div class="sideitem">
+			<h6>Data</h6>
+			<ul>
+				<li><a href="?unformatted">View unformatted data</a></li>
+			</ul>
+		</div>
+	</div>
+	
+	<?php
+	$html = ob_get_contents();
+	ob_end_clean();
+	
+	$pageTitle= "Eclipse Modeling - " . ($projectName ? $projectName . " -" : "") . " IP Log";
+	$pageKeywords = "eclipse,project,modeling,IP";
+	$pageAuthor = "Nick Boldt";
+	
+	$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="/modeling/includes/index.css"/>' . "\n");
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
 }
 ?>
