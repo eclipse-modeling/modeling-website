798f86e38d308f27900a24a9322dcabb20f58b13
nickb <nickb>
2008-05-13 20:54:22 +0000

    [231040] better database connection test

--- includes/ipquery-common.php
+++ includes/ipquery-common.php
@@ -1,102 +1,112 @@
 <?php # Script for retrieving IP log information.
 
 $isFormatted = !isset($_GET["unformatted"]);
+$bugClass="/home/data/httpd/eclipse-php-classes/system/dbconnection_bugs_ro.class.php";
 
 require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php"); require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); $App = new App(); $Nav = new Nav(); $Menu = new Menu(); include($App->getProjectCommon());
-require_once "/home/data/httpd/eclipse-php-classes/system/dbconnection_bugs_ro.class.php";
+if (is_file("$bugClass")) require_once "$bugClass";
 
 function doIPQuery($product_id, $isFormatted = true)
 {
-	# Connect to database
-	$dbc 	= new DBConnectionBugs();
-	$dbh 	= $dbc->connect();
+	global $bugClass;
+	if (!is_file($bugClass)) 
+	{
+		print "<li><b style='color:red'>Error: could not query Bugzilla database.</b></li>";
+	}
+	else
+	{
+	
+		# Connect to database
+		$dbc 	= new DBConnectionBugs();
+		$dbh 	= $dbc->connect();
+							
+		# NOTE: bug_status = 5 is RESOLVED, resolution = 2 is FIXED, 'contributed' keyword id = 22
+		# for product_id, use:	
+		# 8, EMF
+		# 12, GMT
+		# 29, GMF
+		# 40, MDDi
+		# 42, EMFT
+		# 63, Modeling
+		# 67, MDT
+		# 71, M2M
+		# 87, M2T
+		# 105, Amalgam
+		# 106, TMF
 						
-	# NOTE: bug_status = 5 is RESOLVED, resolution = 2 is FIXED, 'contributed' keyword id = 22
-	# for product_id, use:	
-	# 8, EMF
-	# 12, GMT
-	# 29, GMF
-	# 40, MDDi
-	# 42, EMFT
-	# 63, Modeling
-	# 67, MDT
-	# 71, M2M
-	# 87, M2T
-	# 105, Amalgam
-	# 106, TMF
-					
-	$sql_info = "SELECT 
-						attachments.filename,
-						attachments.description,
-						attachments.ispatch,
-						attachments.bug_id,
-						attachments.submitter_id,
-						LENGTH(attach_data.thedata) AS size,
-						bugs.short_desc,
-						components.name,
-						profiles.login_name
-				FROM 
-						attachments, attach_data, bugs, components, keywords, profiles 
-				WHERE
-						attachments.ispatch = 1 AND 
-						attachments.isobsolete = 0 AND
-						attachments.bug_id = bugs.bug_id AND 
-						attachments.attach_id = attach_data.id AND 
-						components.id = bugs.component_id AND 
-						bugs.bug_id = keywords.bug_id AND 
-						keywords.keywordid = 22 AND 
-						profiles.userid = attachments.submitter_id AND 
-						bugs.product_id = $product_id
-				ORDER BY
-						profiles.login_name";
-												
-	
-	$rs = mysql_query($sql_info, $dbh);
-	
-	if(mysql_errno($dbh) > 0) {
-		echo "There was an error processing this request: " . $sql_info . " : ";
+		$sql_info = "SELECT 
+							attachments.filename,
+							attachments.description,
+							attachments.ispatch,
+							attachments.bug_id,
+							attachments.submitter_id,
+							LENGTH(attach_data.thedata) AS size,
+							bugs.short_desc,
+							components.name,
+							profiles.login_name
+					FROM 
+							attachments, attach_data, bugs, components, keywords, profiles 
+					WHERE
+							attachments.ispatch = 1 AND 
+							attachments.isobsolete = 0 AND
+							attachments.bug_id = bugs.bug_id AND 
+							attachments.attach_id = attach_data.id AND 
+							components.id = bugs.component_id AND 
+							bugs.bug_id = keywords.bug_id AND 
+							keywords.keywordid = 22 AND 
+							profiles.userid = attachments.submitter_id AND 
+							bugs.product_id = $product_id
+					ORDER BY
+							profiles.login_name";
+													
 		
-		# For debugging purposes - don't display this stuff in a production page.
-		echo mysql_error($dbh);
+		$rs = mysql_query($sql_info, $dbh);
 		
-		# Mysql disconnects automatically, but I like my disconnects to be explicit.
-		$dbc->disconnect();
-		exit;
-	}
-
-	if ($isFormatted)
-	{	
-		print "		<table>\n			<tr><th>Component</th><th>Bug #</th><th>Contributor</th><th>Size</th><th>Description</th></tr>\n";
-	}
-	$bgcol = "#FFFFEE";
-	while($myrow = mysql_fetch_assoc($rs)) {
+		if(mysql_errno($dbh) > 0) {
+			echo "There was an error processing this request: " . $sql_info . " : ";
+			
+			# For debugging purposes - don't display this stuff in a production page.
+			echo mysql_error($dbh);
+			
+			# Mysql disconnects automatically, but I like my disconnects to be explicit.
+			$dbc->disconnect();
+			exit;
+		}
+	
 		if ($isFormatted)
 		{	
-			$bgcol = $bgcol == "#EEEEFF" ? "#FFFFEE" : "#EEEEFF";
-			$shortname = explode("@", $myrow['login_name']); $shortname = $shortname[0];
-			print "<tr bgcolor=\"$bgcol\" align=\"top\">" .
-					"<td><small style=\"font-size:8px\">" . $myrow['name'] . "</small></td>" .
-					"<td nowrap=\"nowrap\">" . doBugLink($myrow['bug_id']) . "</td>" .
-					"<td><acronym title=\"" . $myrow['login_name'] . "\">$shortname</acronym></td>" .
-					"<td>" . $myrow['size'] . "</td>" .
-					"<td width=\"99%\"><small style=\"font-size:8px\">" . preg_replace("#(\d{5,6})#", doBugLink("$1"), str_replace(",", " ", $myrow['short_desc']) . "<br/>" . str_replace(",", " ", $myrow['description'])) . "</small></td>" .
-				  "</tr>\n";
+			print "		<table>\n			<tr><th>Component</th><th>Bug #</th><th>Contributor</th><th>Size</th><th>Description</th></tr>\n";
 		}
-		else
-		{
-			print $myrow['name'] . "," . $myrow['bug_id'] . "," . $myrow['login_name'] . "," . $myrow['size'] . "," . str_replace(",", " ", $myrow['short_desc']) . " (" . str_replace(",", " ", $myrow['description']) . ")\n";
+		$bgcol = "#FFFFEE";
+		while($myrow = mysql_fetch_assoc($rs)) {
+			if ($isFormatted)
+			{	
+				$bgcol = $bgcol == "#EEEEFF" ? "#FFFFEE" : "#EEEEFF";
+				$shortname = explode("@", $myrow['login_name']); $shortname = $shortname[0];
+				print "<tr bgcolor=\"$bgcol\" align=\"top\">" .
+						"<td><small style=\"font-size:8px\">" . $myrow['name'] . "</small></td>" .
+						"<td nowrap=\"nowrap\">" . doBugLink($myrow['bug_id']) . "</td>" .
+						"<td><acronym title=\"" . $myrow['login_name'] . "\">$shortname</acronym></td>" .
+						"<td>" . $myrow['size'] . "</td>" .
+						"<td width=\"99%\"><small style=\"font-size:8px\">" . preg_replace("#(\d{5,6})#", doBugLink("$1"), str_replace(",", " ", $myrow['short_desc']) . "<br/>" . str_replace(",", " ", $myrow['description'])) . "</small></td>" .
+					  "</tr>\n";
+			}
+			else
+			{
+				print $myrow['name'] . "," . $myrow['bug_id'] . "," . $myrow['login_name'] . "," . $myrow['size'] . "," . str_replace(",", " ", $myrow['short_desc']) . " (" . str_replace(",", " ", $myrow['description']) . ")\n";
+			}
 		}
+		if ($isFormatted)
+		{	
+			print "		</table>\n";
+		}
+		
+		$dbc->disconnect();
+		
+		$rs 		= null;
+		$dbh 		= null;
+		$dbc 		= null;
 	}
-	if ($isFormatted)
-	{	
-		print "		</table>\n";
-	}
-	
-	$dbc->disconnect();
-	
-	$rs 		= null;
-	$dbh 		= null;
-	$dbc 		= null;
 }
 
 function doBugLink($id)
@@ -144,7 +154,7 @@ function doProductIDQuery()
 
 function doIPQueryPage()
 {
-	global $isFormatted, $committers, $product_id, $theme, $PR, $Menu, $Nav; 
+	global $isFormatted, $committers, $product_id, $theme, $PR, $App, $Menu, $Nav; 
 	sort($committers); reset($committers);
 	
 	if (!$isFormatted)
