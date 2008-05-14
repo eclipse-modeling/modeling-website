08adb8273e7f6c9377d286f998aa51603c339e79
nickb <nickb>
2008-05-14 20:42:31 +0000

    [231040] emft page

---
+++ emft/project-info/ipquery.php
@@ -0,0 +1,42 @@
+<?php
+$product_id = 42; # EMFT
+$committers = array(
+	"lbigearde",
+	"jlescot", 
+	"estepper", 
+	"rbihler", 
+	"nickb",
+	"cbrun",
+	"sboehme",
+	"gcannente",
+	"jcote",
+	"seberle",
+	"pfriese",
+	"mgarcia",
+	"lgoubet",
+	"jlescot",
+	"emerks",
+	"pnehrer",
+	"pschonbac",
+	"mtaal",
+	"ttonelli",
+	"cdaly",
+	"sefftinge",
+	"bkolb",
+	"drizov",
+	"dsciamma",
+	"jkohnlein",
+	"jmusset",
+	"ssmith",
+	"kthoms",
+	"atoulme",
+	"mvoelter",
+);
+
+$extra_IP = array(
+);
+
+$third_party = array(
+);
+
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/modeling/includes/ipquery-common.php"); doIPQueryPage(); ?>
