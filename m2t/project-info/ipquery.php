a8c12e16cb1b3763c788b6b0f4987b45f75a3695
nickb <nickb>
2008-05-15 16:04:13 +0000

    [231040] M2T IP log

---
+++ m2t/project-info/ipquery.php
@@ -0,0 +1,21 @@
+<?php
+$product_id = 8; # EMF 
+$committers = array(
+	"pelder",
+	"jcheuoua",
+	"akok",
+	"fcornelis",
+	"mvoelter",
+	"bkolb",
+	"sefftinge",
+	"nboldt",
+);
+
+$extra_IP = array(
+);
+
+$third_party = array(
+	"Tomcat 3.2.4,,cvsroot/modeling/org.eclipse.m2t/org.eclipse.jet/plugins/org.eclipse.jet.core/src/org/eclipse/jet/internal/core/parser/jasper, Apache Software License 1.1,derivative work",
+);
+
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/modeling/includes/ipquery-common.php"); doIPQueryPage(); ?>
