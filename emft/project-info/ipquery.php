db2aba448ef74655fa91da57d6cdc2a58d107b27
nickb <nickb>
2008-05-28 19:40:53 +0000

    add emf.search's 3 CQs

--- emft/project-info/ipquery.php
+++ emft/project-info/ipquery.php
@@ -37,7 +37,9 @@ $extra_IP = array(
 );
 
 $third_party = array(
-
+	"Apache Commons Logging Jar 1.0.4, maintained in Orbit, Apache License 2.0, unmodified entire package, 1946, search",
+	"Apache Commons Line Interface 1.0, maintained in Orbit, Apache License 2.0, unmodified entire package, 1947, search",
+	"Apache Commons Lang 2.1, maintained in Orbit, Apache License 2.0, unmodified entire package, 1948, search",
 );
 
 require_once ($_SERVER['DOCUMENT_ROOT'] . "/modeling/includes/ipquery-common.php"); doIPQueryPage(); ?>
