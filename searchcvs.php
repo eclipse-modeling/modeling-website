891eddd5dc86f3ca807195a6ef0374165fffbaeb
nickb <nickb>
2008-05-30 19:12:28 +0000

    [234277] fix broken require

--- searchcvs.php
+++ searchcvs.php
@@ -1,4 +1,5 @@
 <?php
+require_once ("includes/buildServer-common.php");
 $theme = !isset($theme) ? "Phoenix" : $theme;
 require($_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/searchcvs-common.php");
 ?>
