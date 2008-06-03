c10ca59d5d3c1b61a621198fff5df6aa62991525
nickb <nickb>
2008-06-03 23:36:00 +0000

    [231040] add dupe entries from EMFT

--- m2t/project-info/ipquery.php
+++ m2t/project-info/ipquery.php
@@ -18,7 +18,8 @@ $extra_IP = array(
 );
 
 $third_party = array(
-	"Tomcat 3.2.4, cvsroot/modeling/org.eclipse.m2t/org.eclipse.jet/plugins/org.eclipse.jet.core/src/org/eclipse/jet/internal/core/parser/jasper, Apache Software License 1.1, derivative work, 336, jet",
+	"Tomcat 3.2.4, cvsroot/modeling/org.eclipse.m2t/org.eclipse.jet/plugins/org.eclipse.jet.core/src/org/eclipse/jet/internal/core/parser/jasper, Apache Software License 1.1, derivative work, 2335, jet",
+	"Tomcat 3.2.4, Historical Approval, Apache 2.0, moved to M2T per CQ 2335, 336, jet",
 );
 
 require_once ($_SERVER['DOCUMENT_ROOT'] . "/modeling/includes/ipquery-common.php"); 
