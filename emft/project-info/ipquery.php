557030a6a0241eeb5edf7de8db22435c03344266
nickb <nickb>
2008-06-04 20:29:54 +0000

    hide stuff that has moved to EMF or elsewhere

--- emft/project-info/ipquery.php
+++ emft/project-info/ipquery.php
@@ -64,22 +64,24 @@ $third_party = array(
 	"org.apache.commons.logging 1.0.4.v200701082340, Orbit, Apache 2.0, ?, 1526, mwe",
 	"Apache Commons Line Interface 1.0, Orbit, Apache 2.0, ?, 1527, mwe",
 
-	"Apache Commons Codec 1.3, /cvsroot/tools/org.eclipse.orbit/org.apache.commons.codec, EPL 1.0, original jar repackaged as OSGi bundle, 2339, net4j", 
-	"Apache HttpClient 3.1, /cvsroot/tools/org.eclipse.orbit/org.apache.commons.httpclient, EPL 1.0, original jar repackaged as OSGi bundle, 2340, net4j", 
-	"Apache Derby 10.1.2.1, /cvsroot/tools/org.eclipse.orbit/org.apache.derby, EPL 1.0, original jar repackaged as OSGi bundle, 2341, net4j", 
-	"JMS Spec 1.1 (Glassfish) (PB CQ1614), CDDL, , , 1769, net4j",
-
 	"Apache Commons Logging Jar 1.0.4, Orbit, Apache 2.0, unmodified entire package, 1946, search",
 	"Apache Commons Line Interface 1.0, Orbit, Apache 2.0, unmodified entire package, 1947, search",
 	"Apache Commons Lang 2.1, Orbit, Apache 2.0, unmodified entire package, 1948, search",
 	
 	"wsdl4j-1.6.2.jar 1.6.2, Orbit, , , 2300, servus",
 	
-	"jpox-1.1.7.jar 1.1.7, , Apache 2.0, , 1555, teneo",
-	"jdo2-api-2.0.jar 2.0, , Apache 2.0, not distributed; required for build, 1556, teneo",
+	/* Stuff that has moved to another project */
 	
-	"LPG Runtime and Generated OCL Parser 1, Orbit, , moved to MDT per CQ 1080, 303, ocl",
-	"Apache Tomcat 3.2.4, Historical Approval, Apache 2.0, moved to M2T per CQ 2335, 336, jet",
+	//"Apache Commons Codec 1.3, /cvsroot/tools/org.eclipse.orbit/org.apache.commons.codec, EPL 1.0, original jar repackaged as OSGi bundle, 2339, net4j", 
+	//"Apache HttpClient 3.1, /cvsroot/tools/org.eclipse.orbit/org.apache.commons.httpclient, EPL 1.0, original jar repackaged as OSGi bundle, 2340, net4j", 
+	//"Apache Derby 10.1.2.1, /cvsroot/tools/org.eclipse.orbit/org.apache.derby, EPL 1.0, original jar repackaged as OSGi bundle, 2341, net4j", 
+	//"JMS Spec 1.1 (Glassfish) (PB CQ1614), CDDL, , , 1769, net4j",
+
+	//"jpox-1.1.7.jar 1.1.7, , Apache 2.0, , 1555, teneo",
+	//"jdo2-api-2.0.jar 2.0, , Apache 2.0, not distributed; required for build, 1556, teneo",
+	
+	//"LPG Runtime and Generated OCL Parser 1, Orbit, , moved to MDT per CQ 1080, 303, ocl",
+	//"Apache Tomcat 3.2.4, Historical Approval, Apache 2.0, moved to M2T per CQ 2335, 336, jet",
 );
 
 require_once ($_SERVER['DOCUMENT_ROOT'] . "/modeling/includes/ipquery-common.php"); 
