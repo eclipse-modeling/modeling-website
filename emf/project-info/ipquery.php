c1a264d6f3c0a14d410c95ae817a01795d42f270
nickb <nickb>
2008-06-11 16:24:52 +0000

    relabel geronimo as glassfish

--- emf/project-info/ipquery.php
+++ emf/project-info/ipquery.php
@@ -31,7 +31,7 @@ $third_party = array(
 
 	"SDO 1.0 Specification Interfaces, org.eclipse.emf.ecore.commonj.sdo, Royalty-free license from IBM/BEA (see about.html), unmodified source, 2336, sdo",
 	
-	"Geronimo JMS Spec 1.1 (Glassfish) (PB CQ1614), /cvsroot/modeling/org.eclipse.emf/org.eclipse.emf.net4j/examples/org.eclipse.net4j.jms.api/lib, EPL-compatible CDDL, unmodified binary, 2338, net4j",
+	"JMS Spec Version: 1.1 (PB CQ1769), /cvsroot/modeling/org.eclipse.emf/org.eclipse.emf.net4j/examples/org.eclipse.net4j.jms.api, EPL-compatible CDDL, unmodified binary, 2338, net4j",
 	"Apache Commons Codec 1.3, /cvsroot/tools/org.eclipse.orbit/org.apache.commons.codec, EPL 1.0, original jar repackaged as OSGi bundle, 2339, net4j", 
 	"Apache HttpClient 3.1, /cvsroot/tools/org.eclipse.orbit/org.apache.commons.httpclient, EPL 1.0, original jar repackaged as OSGi bundle, 2340, net4j", 
 	"Apache Derby 10.1.2.1, /cvsroot/tools/org.eclipse.orbit/org.apache.derby, EPL 1.0, original jar repackaged as OSGi bundle, 2341, net4j", 
