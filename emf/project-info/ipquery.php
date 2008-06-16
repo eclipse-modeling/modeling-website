8916ed9a0899e82de970b387581443de528a66d6
nickb <nickb>
2008-06-16 16:00:38 +0000

    add more used-but-not-distro'd entries, commented out

--- emf/project-info/ipquery.php
+++ emf/project-info/ipquery.php
@@ -36,9 +36,27 @@ $third_party = array(
 	"Apache HttpClient 3.1, /cvsroot/tools/org.eclipse.orbit/org.apache.commons.httpclient, EPL 1.0, original jar repackaged as OSGi bundle, 2340, net4j", 
 	"Apache Derby 10.1.2.1, /cvsroot/tools/org.eclipse.orbit/org.apache.derby, EPL 1.0, original jar repackaged as OSGi bundle, 2341, net4j", 
 
+	# may need these according to https://bugs.eclipse.org/bugs/show_bug.cgi?id=227333#c26
+	#"hsqldb.jar, org.eclipse.net4j.db.hsqldb, , not distributed; required for build, , net4j",
+	#"mysql-connector-java-5.1.5-bin.jar, org.eclipse.net4j.db.mysql, , not distributed; required for build, , net4j",
+
+	# may need these according to https://bugs.eclipse.org/bugs/show_bug.cgi?id=227333#c26
+	#"asm.jar, org.eclipse.emf.cdo.server.hibernate.libraries, , not distributed; required for build, , cdo",
+	#"cglib-2.1.3.jar, org.eclipse.emf.cdo.server.hibernate.libraries, , not distributed; required for build, , cdo",
+	#"commons-collections-2.1.1.jar, org.eclipse.emf.cdo.server.hibernate.libraries, , not distributed; required for build, , cdo",
+	#"dom4j-1.6.1.jar, org.eclipse.emf.cdo.server.hibernate.libraries, , not distributed; required for build, , cdo",
+	#"hibernate3.jar, org.eclipse.emf.cdo.server.hibernate.libraries, , not distributed; required for build, , cdo",
+	#"jta.jar, org.eclipse.emf.cdo.server.hibernate.libraries, , not distributed; required for build, , cdo",
+
+	# not yet shipped or part of the build, only ref'd in CVS
+	#"spring-2.5.4/spring-beans.jar, runtime plugins dir, , not distributed; required for build, , cdo",
+	#"spring-2.5.4/spring-context.jar, runtime plugins dir, , not distributed; required for build, , cdo",
+	#"spring-2.5.4/spring-core.jar, runtime plugins dir, , not distributed; required for build, , cdo",
+
 	# for Ganymede 2008, these two are not included in delivered code; may need to re-add for Io 2009		
-	#"jpox-1.1.7.jar 1.1.7, , Apache 2.0, not distributed; required for build, 2393, teneo",
+	#"jpox-1.1.7.jar 1.1.7, org.eclipse.emf.teneo.jpox.libraries, Apache 2.0, not distributed; required for build, 2393, teneo",
 	#"jdo2-api-2.0.jar 2.0, , Apache 2.0, not distributed; required for build, 2394, teneo", 
+
 );
 
 if (isset($_GET["emf"])){ 
