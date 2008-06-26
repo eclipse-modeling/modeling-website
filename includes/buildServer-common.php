502c24b0dc37d156b3b8daea1366e5ba9a203ca1
nickb <nickb>
2008-06-26 16:21:36 +0000

    [238626] default to cbi dir instead of modeling

--- includes/buildServer-common.php
+++ includes/buildServer-common.php
@@ -1,17 +1,28 @@
 <?php
-	# allows server-specific path assignment, eg., for build.eclipse.org
-	$isBuildDotEclipseServer = $_SERVER["SERVER_NAME"] == "build.eclipse.org";
-	if ($isBuildDotEclipseServer && isset($_GET["debug"]))
+
+# allows server-specific path assignment, eg., for build.eclipse.org
+$isBuildDotEclipseServer = $_SERVER["SERVER_NAME"] == "build.eclipse.org";
+if ($isBuildDotEclipseServer)
+{
+	if (isset ($_GET["debug"]))
 	{
-		ini_set('display_errors', 1); ini_set('error_reporting', E_ALL);
+		ini_set('display_errors', 1);
+		ini_set('error_reporting', E_ALL);
 	}
-	$_SERVER['DOCUMENT_ROOT'] = $isBuildDotEclipseServer ? "/opt/public/modeling" : $_SERVER['DOCUMENT_ROOT'];
-	if (!is_dir ($_SERVER['DOCUMENT_ROOT']))
+	
+	$docRootOverrides = array (
+		"/opt/public/cbi",
+		"/opt/public/modeling/public_html",
+		"/opt/public/modeling",
+		
+	);
+	foreach ($docRootOverrides as $dr)
 	{
-		$_SERVER['DOCUMENT_ROOT'] = $isBuildDotEclipseServer ? "/opt/public/modeling/public_html" : $_SERVER['DOCUMENT_ROOT'];
+		if (is_dir($dr))
+		{
+			$_SERVER['DOCUMENT_ROOT'] = $dr;
+			break;
+		}
 	}
-	if (!is_dir ($_SERVER['DOCUMENT_ROOT']))
-	{
-		$_SERVER['DOCUMENT_ROOT'] = $isBuildDotEclipseServer ? "/opt/public/cbi" : $_SERVER['DOCUMENT_ROOT'];
-	}
- ?>
+}
+?>
