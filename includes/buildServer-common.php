c2358c579cded3ea02fafc3895f534d35fe1acf1
nickb <nickb>
2008-06-26 15:59:07 +0000

    [238626]

--- includes/buildServer-common.php
+++ includes/buildServer-common.php
@@ -1,12 +1,13 @@
 <?php
 	# allows server-specific path assignment, eg., for build.eclipse.org
 	$isBuildDotEclipseServer = $_SERVER["SERVER_NAME"] == "build.eclipse.org";
-	$_SERVER['DOCUMENT_ROOT'] = $isBuildDotEclipseServer ? "/opt/public/modeling" : $_SERVER['DOCUMENT_ROOT'];
-	if (!is_dir ($_SERVER['DOCUMENT_ROOT']))
-	{
-		$_SERVER['DOCUMENT_ROOT'] = $isBuildDotEclipseServer ? "/opt/public/modeling/public_html" : $_SERVER['DOCUMENT_ROOT'];
-	}
 	if ($isBuildDotEclipseServer && isset($_GET["debug"]))
 	{
 		ini_set('display_errors', 1); ini_set('error_reporting', E_ALL);
-	} ?>
+	}
+	$_SERVER['DOCUMENT_ROOT'] = $isBuildDotEclipseServer ? "/opt/public/modeling" : $_SERVER['DOCUMENT_ROOT'];
+	if (!is_dir ($_SERVER['DOCUMENT_ROOT']))
+	{
+		$_SERVER['DOCUMENT_ROOT'] = $isBuildDotEclipseServer ? "/opt/public/cbi" : $_SERVER['DOCUMENT_ROOT'];
+	}
+ ?>
