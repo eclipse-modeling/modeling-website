93c79f0da440c533141285cd7dc70390649f06c8
estepper <estepper>
2008-07-05 17:44:50 +0000

    [239677]  Adjust the build for un-nested p3rd party jars https://bugs.eclipse.org/bugs/show_bug.cgi?id=239677

--- emf/net4j/build/_common.php
+++ emf/net4j/build/_common.php
@@ -7,13 +7,11 @@ $options = array (
 	"BranchAndJDK" => array (
 		"emft.eclipse.org=------------,------------",
 		"1.0.0=HEAD,/opt/sun-java2-5.0",
-		"2.0.0=simon_pre_0_9,/opt/sun-java2-5.0",
-		"0.7.3=R0_7_maintenance,/opt/sun-java2-1.4",
+		"1.0.1=R1_0_maintenance,/opt/sun-java2-5.0",
 		
 		"build.eclipse.org=------------,------------",
 		"1.0.0=HEAD,/opt/public/common/ibm-java2-ppc-50",
-		"2.0.0=simon_pre_0_9,/opt/public/common/ibm-java2-ppc-50",
-		"0.7.3=R0_7_maintenance,/opt/public/stp/apps/IBMJava2-ppc-142" // bug 178681
+		"1.0.1=R1_0_maintenance,/opt/public/common/ibm-java2-ppc-50"
 	),
 	
 	/* define a regular expression to be used to collect the most recent 
