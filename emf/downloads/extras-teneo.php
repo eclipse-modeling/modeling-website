d410ae76b79cbbeb97cf96d6edc4819f69a3de56
mtaal <mtaal>
2008-07-12 13:19:46 +0000

    Changed text

--- emf/downloads/extras-teneo.php
+++ emf/downloads/extras-teneo.php
@@ -6,7 +6,6 @@ function doRequirements()
 	<h3>Teneo Builds/Releases</h3>
 	<p style="padding-left:10px">Teneo has recently graduated to the first 1.0 release. New maintenance builds are published frequently. 
 	<b>The maintenance builds (see bottom of the page) are the preferred builds to be used</b>.</p>
-	<p style="padding-left:10px">The I-0.7.5 build is for Eclipse 3.2 and EMF 2.2. However this build/branch is not actively maintained.</p>
 </div>
 <?php
 }
