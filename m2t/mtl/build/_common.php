ff9b125bf79c7063a38472d9d2d2fa6953f05b8f
cbrun <cbrun>
2008-07-23 14:14:21 +0000

    add build web ui for MTL

---
+++ m2t/mtl/build/_common.php
@@ -0,0 +1,22 @@
+<?php
+require_once ("../../../includes/buildServer-common.php");
+
+$options = array (
+	"BaseBuilderBranch" => "RC2_34",
+	
+	"BranchAndJDK" => array (
+		"emft.eclipse.org=------------,------------",
+		"0.7.0=HEAD,/opt/sun-java2-5.0",
+		
+		"build.eclipse.org=------------,------------",
+		"0.7.0=HEAD,/opt/sun-java2-5.0",
+	),
+	
+	"Mapfile_Rule_Default" => 0, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, No Tagging=gen-false"
+
+	"EmailDefault" => "cedric.brun@obeo.fr", // prefil email contact box with comma-sep'd list
+	
+	"Users" => array("cbrun","cbrun",null) /* build user, eclipse cvs user, IES cvs user */
+);
+
+?>
