0b43060a51fe6fc6232c731320631e45d4997731
nickb <nickb>
2008-07-24 03:05:43 +0000

    debug

--- includes/scripts.php
+++ includes/scripts.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: scripts.php,v 1.69 2008/07/20 18:05:49 nickb Exp $
+// $Id: scripts.php,v 1.70 2008/07/24 03:05:43 nickb Exp $
 
 function PWD_debug($PWD, $suf, $str)
 {
@@ -309,7 +309,17 @@ function getNews($lim, $key, $xml = "", $linkOnly=false, $dateFmtPre="", $dateFm
 
 function build_news($cvsprojs, $cvscoms, $proj, $limit = 4)
 {
-	global $projects, $PR;
+	global $projects, $PR, $debug;
+	if ($debug > 10)
+	{
+		foreach (array_keys($cvscoms) as $z)
+		{
+			foreach (array_keys($cvscoms[$z]) as $y)
+			{
+				print "\$cvscoms[$z][$y] = " . $cvscoms[$z][$y] . "<br/>";
+			}
+		}
+	}
 
 	$types = array(
 		"I" => "integration",
