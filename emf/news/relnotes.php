4386b586cfb57bdef7e40980f5baabd793478f54
nickb <nickb>
2008-09-05 20:16:24 +0000

    update branch targets

--- emf/news/relnotes.php
+++ emf/news/relnotes.php
@@ -14,31 +14,36 @@ $extras = array("doBleedingEdge");
 
 $streams = array(
 	"emf" => array(
-		"2.4.x" => "HEAD",
+		"2.5.x" => "HEAD",
+		"2.4.x" => "R2_4_maintenance",
 		"2.3.x" => "R2_3_maintenance",
 		"2.2.x" => "R2_2_maintenance",
 		"2.1.x" => "R2_1_maintenance",
 		"2.0.x" => "R2_0_maintenance"
 	),
 	"sdo" => array(
-		"2.4.x" => "HEAD",
+		"2.5.x" => "HEAD",
+		"2.4.x" => "R2_4_maintenance",
 		"2.3.x" => "R2_3_maintenance",
 		"2.2.x" => "R2_2_maintenance",
 		"2.1.x" => "R2_1_maintenance",
 		"2.0.x" => "R2_0_maintenance"
 	),
 	"query" => array(
-		"1.2.x" => "HEAD",
+		"1.3.x" => "HEAD",
+		"1.2.x" => "R1_2_maintenance",
 		"1.1.x" => "R1_1_maintenance",
 		"1.0.x" => "R1_0_maintenance"
 	),
 	"transaction" => array(
-		"1.2.x" => "HEAD",
+		"1.3.x" => "HEAD",
+		"1.2.x" => "R1_2_maintenance",
 		"1.1.x" => "R1_1_maintenance",
 		"1.0.x" => "R1_0_maintenance"
 	),
 	"validation" => array(
-		"1.2.x" => "HEAD",
+		"1.3.x" => "HEAD",
+		"1.2.x" => "R1_2_maintenance",
 		"1.1.x" => "R1_1_maintenance",
 		"1.0.x" => "R1_0_maintenance"
 	)
