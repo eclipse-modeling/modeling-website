6f7ffca0438760dfe01d9d0219fab50ed71361b1
nickb <nickb>
2008-09-15 22:04:18 +0000

    [247219] new clean white empty page

--- download.php
+++ download.php
@@ -4,27 +4,39 @@
  * this script is only here so that download pages can be tracked before 
  * bouncing on to downloads/download.php
  * 
- * See aslo includes/scripts.php#addGoogleAnalyticsTrackingCodeToHeader() and
- * http://wiki.eclipse.org/Using_Phoenix#Google_Analytics
+ * See also http://wiki.eclipse.org/Using_Phoenix#Google_Analytics
  *  
- */
-require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	
-include_once $_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/scripts.php";
-$App = new App(); addGoogleAnalyticsTrackingCodeToHeader();
+ */ 
 $html = <<<EOHTML
+<html>
+<body onload="document.location.href='/downloads/download.php?${_SERVER["QUERY_STRING"]}';"> 
 <script type="text/javascript">
-onload=redirectOnLoad;
-function redirectOnLoad ()
-{
-	document.location.href='/downloads/download.php?${_SERVER["QUERY_STRING"]}';
-}
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
 </script>
+<script type="text/javascript">
+var pageTracker = _gat._getTracker("UA-910670-2");
+pageTracker._initData();
+pageTracker._trackPageview();
+</script>
+
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+var pageTracker = _gat._getTracker("UA-2566337-8");
+pageTracker._initData();
+pageTracker._trackPageview();
+</script>
+			
 <noscript>
 <p><blockquote style="margin:30px; border:2px purple dashed"><blockquote style="margin:30px">
 It seems you have Javascript disabled. <a href='/downloads/download.php?${_SERVER["QUERY_STRING"]}'>Click here to proceed to the download page</a>.
 </blockquote></blockquote></p>
 </noscript>
-EOHTML;
-$App->generatePage("Phoenix", null, null, "Eclipse Modeling Project", "modeling, EMF, GMF, UML, UML2, MDD, MDA, model-driven, eclipse, downloads", "Eclipse Modeling Project - Downloads", $html);
 
-?>
+</body>
+</html>
+EOHTML;
+print $html; ?>
