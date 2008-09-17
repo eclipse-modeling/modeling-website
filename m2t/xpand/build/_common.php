7652e2043b5333f3c85c2a9ad8ce1db07c7c01fb
dhubner <dhubner>
2008-09-17 15:43:22 +0000

    trying to exclude orbitBundles

--- m2t/xpand/build/_common.php
+++ m2t/xpand/build/_common.php
@@ -16,7 +16,7 @@ $options = array (
 	* matching dependencies for running a build. See also releng-common/tools/scripts/start_cron.sh
 	* and /home/www-data/build/requests/dependencies.urls.txt 
 	* */
-	"regex" => "I200.*/eclipse-SDK-|[SR]-.*200.*/eclipse-SDK-|[SR]200.*/emf-sdo-xsd-SDK-|[SR]200.*/mdt-uml2-SDK-|[SR]200.*/emft-compare-SDK-|[ISR]200.*/emft-mwe-SDK-|[SR]200.*/orbit-",
+	"regex" => "I200.*/eclipse-SDK-|[SR]-.*200.*/eclipse-SDK-|[SR]200.*/emf-sdo-xsd-SDK-|[SR]200.*/mdt-uml2-SDK-|[SR]200.*/emft-compare-SDK-|[ISR]200.*/emft-mwe-SDK-|[SR]200.*/orbit-R",
 
 	"Mapfile_Rule_Default" => 0, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, No Tagging=gen-false"
 
