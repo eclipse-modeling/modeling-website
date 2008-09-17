eb29697a5256a86a7fbeb2df737f0df2bb47d453
dhubner <dhubner>
2008-09-17 14:34:46 +0000

    regex

--- m2t/xpand/build/_common.php
+++ m2t/xpand/build/_common.php
@@ -3,20 +3,30 @@ require_once ("../../../includes/buildServer-common.php");
 
 $options = array (
 	"BaseBuilderBranch" => "RC2_34",
-	
+
 	"BranchAndJDK" => array (
 		"emft.eclipse.org=------------,------------",
 		"0.7.0=HEAD,/opt/sun-java2-5.0",
-		
+
 		"build.eclipse.org=------------,------------",
 		"0.7.0=HEAD,/opt/sun-java2-5.0",
+		
 	),
-	
+	/* define a regular expression to be used to collect the most recent 
+	* matching dependencies for running a build. See also releng-common/tools/scripts/start_cron.sh
+	* and /home/www-data/build/requests/dependencies.urls.txt 
+	* */
+	"regex" => "I200.*/eclipse-SDK-|[SR]-.*200.*/eclipse-SDK-|[SR]200.*/emf-sdo-xsd-SDK-|[SR]200.*/mdt-uml2-SDK-|[SR]200.*/emft-compare-SDK-|[ISR]200.*/emft-mwe-SDK-|[SR]200.*/orbit-",
+
 	"Mapfile_Rule_Default" => 0, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, No Tagging=gen-false"
 
 	"EmailDefault" => "dennis.huebner@itemis.de", // prefil email contact box with comma-sep'd list
-	
-	"Users" => array("dhubner","dhubner",null) /* build user, eclipse cvs user, IES cvs user */
-);
 
+	"Users" => array (
+		"dhubner",
+		"dhubner",
+		dhubner
+	) /* build user, eclipse cvs user, IES cvs user */
+	
+);
 ?>
