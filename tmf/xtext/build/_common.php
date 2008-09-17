d5e8e83ed8edc9aa4b01badc336bab642a9158d6
nickb <nickb>
2008-09-17 01:32:05 +0000

    add Xpand to regex

--- tmf/xtext/build/_common.php
+++ tmf/xtext/build/_common.php
@@ -9,14 +9,14 @@ $options = array (
 		"1.0.0=HEAD,/opt/sun-java2-5.0",
 
 		"build.eclipse.org=------------,------------",
-		"2.2.0=HEAD,/opt/public/common/ibm-java2-ppc-50",
+		"1.0.0=HEAD,/opt/public/common/ibm-java2-ppc-50",
 	),
 
 	/* define a regular expression to be used to collect the most recent 
 	 * matching dependencies for running a build. See also releng-common/tools/scripts/start_cron.sh
 	 * and /home/www-data/build/requests/dependencies.urls.txt 
 	 * */ 	
-	"regex" => "I200.*/eclipse-SDK-|[SR]-.*200.*/eclipse-SDK-|[ISR]200.*/emf-sdo-xsd-SDK-|[ISR]200.*/emft-compare-SDK-",
+	"regex" => "I200.*/eclipse-SDK-|[SR]-.*200.*/eclipse-SDK-|[ISR]200.*/emf-sdo-xsd-SDK-|[ISR]200.*/emft-compare-SDK-|[ISR]200.*/m2t-xpand-",
 
 	"Mapfile_Rule_Default" => 0, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, No Tagging=gen-false"
 
