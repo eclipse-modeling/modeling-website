f098706b1be41da11fedcf62bbc64f92d600c581
dhubner <dhubner>
2008-09-24 09:15:43 +0000

    let see ISR builds

--- m2t/xpand/build/_common.php
+++ m2t/xpand/build/_common.php
@@ -16,16 +16,16 @@ $options = array (
 	* matching dependencies for running a build. See also releng-common/tools/scripts/start_cron.sh
 	* and /home/www-data/build/requests/dependencies.urls.txt 
 	* */
-	"regex" => "I200.*/eclipse-SDK-|[SR]-.*200.*/eclipse-SDK-|[SR]200.*/emf-sdo-xsd-SDK-|[SR]200.*/mdt-uml2-SDK-|[SR]200.*/emft-compare-SDK-|[ISR]200.*/emft-mwe-SDK-|[SR]200.*/orbit-R*",
+	"regex" => "[ISR]200.*/eclipse-SDK-|[SR]-.*200.*/eclipse-SDK-|[SR]200.*/emf-sdo-xsd-SDK-|[SR]200.*/mdt-uml2-SDK-|[SR]200.*/emft-compare-SDK-|[ISR]200.*/emft-mwe-SDK-|[SR]200.*/orbit-R*",
 
 	"Mapfile_Rule_Default" => 0, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, No Tagging=gen-false"
 
 	"EmailDefault" => "dennis.huebner@itemis.de", // prefil email contact box with comma-sep'd list
 
 	"Users" => array (
-		"bkolb",
-		"bkolb",
-		null
+		"dhubner",
+		"dhubner",
+		nulls
 	) /* build user, eclipse cvs user, IES cvs user */
 	
 );
