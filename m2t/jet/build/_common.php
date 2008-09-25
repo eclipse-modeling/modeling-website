b02ee0f7226f3079637864310f0b888feb6ebab5
pelder <pelder>
2008-09-25 20:39:32 +0000

    update R0_9_maintenance build ID to 0.9.2

--- m2t/jet/build/_common.php
+++ m2t/jet/build/_common.php
@@ -7,11 +7,11 @@ $options = array (
 	"BranchAndJDK" => array (
 		"emf.torolab.ibm.com=------------,------------",
         "1.0.0=HEAD,/opt/sun-java2-5.0",
-        "0.9.1=R0_9_maintenance,/opt/sun-java2-5.0",
+        "0.9.2=R0_9_maintenance,/opt/sun-java2-5.0",
 
 		"build.eclipse.org=------------,------------",
         "1.0.0=HEAD,/opt/sun-java2-5.0",
-        "0.9.1=R0_9_maintenance,/opt/sun-java2-5.0",
+        "0.9.2=R0_9_maintenance,/opt/sun-java2-5.0",
 	),
 
 	/* define a regular expression to be used to collect the most recent 
