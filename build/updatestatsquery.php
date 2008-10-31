ffb89a87f02d97c49bdb48341196e208959cff8b
nickb <nickb>
2008-10-31 00:25:29 +0000

    [252716] disabled for security reasons

--- build/updatestatsquery.php
+++ build/updatestatsquery.php
@@ -1,5 +1,7 @@
 <?php
 
+echo "Disabled."; exit;
+
 require_once ("../includes/buildServer-common.php");
 require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php"); require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); $App = new App(); $Nav = new Nav(); $Menu = new Menu(); include($App->getProjectCommon());
 
