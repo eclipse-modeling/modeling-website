0e8c3f963ce47c78c4d3c4566ae93d9daae01027
dhubner <dhubner>
2008-11-12 13:36:02 +0000

    added notes to the download site

---
+++ tmf/downloads/extras-xtext.php
@@ -0,0 +1,12 @@
+<?php
+function showNotes()
+{
+?>
+	<div class="homeitem3col">
+		<h3>Notes</h3>
+		<p>In addition you need to download and install the antlr generator from <a href="http://ftp.itemis.de/files/xtext/antlr.zip">here</a>.
+		</p>
+	</div>
+<?php
+}
+?>
