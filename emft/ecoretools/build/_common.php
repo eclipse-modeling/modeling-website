0b5a6e3c6127c8c37cecf642e414b225815c30bf
jlescot <jlescot>
2008-11-13 12:53:38 +0000

    Upgrade feature number

--- emft/ecoretools/build/_common.php
+++ emft/ecoretools/build/_common.php
@@ -6,10 +6,12 @@ $options = array (
 	
 	"BranchAndJDK" => array (
 		"emft.eclipse.org=------------,------------",
+		"0.9.0=HEAD,/opt/sun-java2-5.0",
 		"0.8.0=HEAD,/opt/sun-java2-5.0",
 		#"0.7.1=R0_7_maintenance,/opt/sun-java2-5.0",
 		
 		"build.eclipse.org=------------,------------",
+		"0.9.0=HEAD,/opt/public/common/ibm-java2-ppc-50",
 		"0.8.0=HEAD,/opt/public/common/ibm-java2-ppc-50",
 		#"0.7.1=R0_7_maintenance,/opt/public/common/ibm-java2-ppc-50"
 	),
