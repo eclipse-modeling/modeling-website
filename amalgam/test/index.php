fee52322cc7dfe6986cf439768b21faf3916be4e
rgronback <rgronback>
2008-11-15 16:25:33 +0000

    test

---
+++ amalgam/test/index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<title>Amalgam downloads</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<meta name="author" content="Eclipse Foundation, Inc." />
+	<meta name="keywords" content="amalgam, modeling, domain-specific language, uml" />
+	<link rel="stylesheet" type="text/css" href="/eclipse/eclipse.org-common/stylesheets/visual.css" media="screen" />
+	<link rel="stylesheet" type="text/css" href="/eclipse/eclipse.org-common/stylesheets/layout.css" media="screen" />
+	<link rel="stylesheet" type="text/css" href="/eclipse/eclipse.org-common/stylesheets/print.css" media="print" />
+	<script type="text/javascript">
+
+sfHover = function() {
+	var sfEls = document.getElementById("leftnav").getElementsByTagName("LI");
+	for (var i=0; i<sfEls.length; i++) {
+		sfEls[i].onmouseover=function() {
+			this.className+=" sfhover";
+		}
+		sfEls[i].onmouseout=function() {
+			this.className=this.className.replace(new RegExp(" sfhover\\b"), "");
+		}
+	}
+}
+if (window.attachEvent) window.attachEvent("onload", sfHover);
+</script>
+</head>
+<body>
+
+<div id="header">
+	<a href="http://www.eclipse.org/"><img src="/eclipse/eclipse.org-common/stylesheets/header_logo.gif" width="163" height="68" border="0" alt="Eclipse Logo" class="logo" /></a>
+	<div id="searchbar">
+		<img src="/eclipse/eclipse.org-common/stylesheets/searchbar_transition.gif" width="92" height="26" class="transition" alt="" />
+		<img src="/eclipse/eclipse.org-common/stylesheets/searchbar_header.gif" width="64" height="17" class="header" alt="Search" />
+		<form method="get" action="http://www.eclipse.org/search/search.cgi">
+			<input type="hidden" name="t" value="All" />
+			<input type="hidden" name="t" value="Doc" />
+			<input type="hidden" name="t" value="Downloads" />
+			<input type="hidden" name="t" value="Wiki" />
+			<input type="hidden" name="wf" value="574a74" />
+			<input type="text" name="q" value="" />
+			<input type="image" class="button" src="/eclipse/eclipse.org-common/stylesheets/searchbar_submit.gif" alt="Submit" onclick="this.submit();" />
+		</form>
+	</div>
+	<ul id="headernav">
+		<li class="first"><a href="http://www.eclipse.org/org/foundation/contact.php">Contact</a></li>
+		<li><a href="http://www.eclipse.org/legal/">Legal</a></li>
+	</ul>
+</div>
+<div id="topnav">
+	<ul>
+		<li><a href="http://www.eclipse.org/modeling/amalgam" target="_self">Amalgam Homepage</a></li>
+		<li class="tabstart">&#160;&#160;&#160;</li>
+		<li><a href="http://download.eclipse.org/modeling/amalgam/downloads/index.php" target="_self">Amalgam Downloads</a></li>
+		<li class="tabstartselected">&#160;&#160;&#160;</li>
+		<li><a class="tabselected">Modeler Build I20081115-1121</a></li>
+	</ul>
+</div>
+<div id="topnavsep"></div>
+
+<div id="leftcol">
+	<ul id="leftnav">
+	<li><a href="#Modeler">Modeler</a></li>
+	 
+	  </li>
+	  <li style="background-image: url(/eclipse/eclipse.org-common/stylesheets/leftnav_fade.jpg); background-repeat: repeat-x; border-style: none;">
+				<br /><br /><br /><br /><br />
+	  </li>
+	</ul>
+</div>
+
+<div id="rightcolumn">
+		</br></br></br>
+		<div class="sideitem">
+			<h3>Related Links</h3>
+				<ul class="midlist">
+				<li><a href="modeler.build">View the <b>build model</b> for the current Modeler build.</a></li>
+				<li><a href="directory.txt">View the <b>map file entries</b> for the current build.</a></li></ul>
+		</div>
+		</br></br></br>	
+</div>	
+
+&nbsp;
+<div id="midcolumn">
+		<table BORDER=0 CELLSPACING=5 CELLPADDING=2 WIDTH="100%" > 
+			<tr> 
+			  <td> 
+				<p><b><font face="Verdana" size="+3">Modeler Build: I20081115-1121</font></b><br> 
+				These downloads are provided under the <a href="http://www.eclipse.org/legal/epl/notice.html">
+				<b>Eclipse Foundation Software User Agreement</b></a>.</p>
+			  </td>
+			</tr><tr/>
+			<tr>
+				<td width="75%">
+		   			The page provides access to the various sections of this build along with details
+					relating to its results.
+				</td>
+			</tr>
+		</table>
+		<table align="left" BORDER=0 CELLSPACING=5 CELLPADDING=2 WIDTH="100%">
+			<tr>
+			  <td><div class="homeitem3col">
+				<h3><a name="Modeler">Modeler build I20081115-1121</a></h3>
+			</tr>
+
+			<tr>
+			  <td><img align="top" src="http://www.eclipse.org/modeling/images/modeling_about.png"></td><td><a href="http://www.eclipse.org/modeling/download.php?file=/modeling/amalgam/modeler/downloads/drops/I20081115-1121/modeler-incubation.macosx.carbon.x86.tar">modeler-incubation.macosx.carbon.x86.tar</a></td>
+			</tr>
+
+			<tr>
+			  <td><img align="top" src="http://www.eclipse.org/modeling/images/modeling_about.png"></td><td><a href="http://www.eclipse.org/modeling/download.php?file=/modeling/amalgam/modeler/downloads/drops/I20081115-1121/modeler-incubation.win32.win32.x86.zip">modeler-incubation.win32.win32.x86.zip</a></td>
+			</tr>
+
+			<tr>
+			  <td><img align="top" src="http://www.eclipse.org/modeling/images/modeling_about.png"></td><td><a href="http://www.eclipse.org/modeling/download.php?file=/modeling/amalgam/modeler/downloads/drops/I20081115-1121/modeler-incubation.linux.gtk.x86.tar">modeler-incubation.linux.gtk.x86.tar</a></td>
+			</tr>
+
+		</table>
+		
+			<h3>Feature Requirements</h3>
+				<p>The following features were installed into the build target for this build, though won't necessarily be found in the product:</p>
+				<ul class="midlist">
+  					<li>Feature: org.eclipse.gef, Version: 3.4.1.v20080806-67718083A56B4H2A3213573, Repository: <a href="http://download.eclipse.org/releases/ganymede/">Ganymede</a></li>
+  					<li>Feature: org.eclipse.emf, Version: 2.4.1.v200808251517, Repository: <a href="http://download.eclipse.org/releases/ganymede/">Ganymede</a></li>
+  					<li>Feature: org.eclipse.uml2, Version: 2.2.1.v200808251630, Repository: <a href="http://download.eclipse.org/releases/ganymede/">Ganymede</a></li>
+  					<li>Feature: org.eclipse.ocl.all, Version: 1.2.2.v200808291106-348d7__FvvCDBhJdOLOlRK8, Repository: <a href="http://download.eclipse.org/releases/ganymede/">Ganymede</a></li>
+  					<li>Feature: org.eclipse.emf.emfqtv.all, Version: 1.2.2.v200809171833-67A2_sOBSMOaKyGoX3EfYeddQEEz, Repository: <a href="http://download.eclipse.org/releases/ganymede/">Ganymede</a></li>
+  					<li>Feature: org.eclipse.gmf, Version: 1.1.2.v20080916-2008-7c7a7REWvhKvgc1pKGmabT8ukFgQ, Repository: <a href="http://download.eclipse.org/releases/ganymede/">Ganymede</a></li>
+  					<li>Feature: org.eclipse.stp.sca.feature.amalgamation, Version: 1.1.0.200810141327, Repository: <a href="http://download.eclipse.org/stp/sca/amalgam/">SCA repo</a></li>
+  					<li>Feature: org.eclipse.uml2.diagram, Version: 0.8.1.v200809231457, Repository: <a href="http://download.eclipse.org/modeling/mdt/updates/releases/">MDT releases</a></li>
+  					<li>Feature: org.eclipse.uml2.diagram.clazz.search, Version: 0.7.0.v200806130939, Repository: <a href="http://download.eclipse.org/modeling/emft/updates/releases/">EMFT Milestones</a></li>
+  					<li>Feature: org.eclipse.stp.bpmn.feature, Version: 1.0.0.200806121858-4--7E77c7UAEAcQc, Repository: <a href="http://download.eclipse.org/releases/ganymede/">Ganymede</a></li>
+
+				</ul>
+			<h3>Bundle Requirements</h3>
+				<p>If individual bundles are installed into the product, they are listed here:</p>
+				<ul class="midlist">
+				
+				</ul>
+	  <hr class="clearer" />
+	</div>
+</div>
+
+<p><br/></p>
+
+<div id="footer">
+	<ul id="footernav">
+		<li class="first"><a href="http://www.eclipse.org">Home</a></li>
+		<li><a href="http://www.eclipse.org/legal/privacy.php">Privacy Policy</a></li>
+		<li><a href="http://www.eclipse.org/legal/termsofuse.php">Terms of Use</a></li>
+	</ul>
+	<p>Copyright &copy; 2008 The Eclipse Foundation. All Rights Reserved</p>
+</div>
+</body>
+</html>
