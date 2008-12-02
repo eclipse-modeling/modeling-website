4ae4eed3855ebb67679acc2fe97250576f9e2097
nickb <nickb>
2008-12-02 00:43:41 +0000

    [257000] emft.eclipse and modeling.eclipse are build servers for EMF too

--- emf/_projectCommon.php
+++ emf/_projectCommon.php
@@ -5,7 +5,7 @@ $PR = "modeling/emf";
 $projectName = "EMF";
 $defaultProj = "/emf";
 
-$isEMFserver = (preg_match("/^emf(?:\.torolab\.ibm\.com)$/", $_SERVER["SERVER_NAME"]));
+$isEMFserver = (preg_match("/^emf(?:\.torolab\.ibm\.com)$/", $_SERVER["SERVER_NAME"])) || (preg_match("/^(emft|modeling)\.eclipse\.org$/", $_SERVER["SERVER_NAME"]));
 $isBuildServer = (preg_match("/^(emft|modeling|build)\.eclipse\.org$/", $_SERVER["SERVER_NAME"])) || $isEMFserver;
 $isBuildDotEclipseServer = $_SERVER["SERVER_NAME"] == "build.eclipse.org";
 $isWWWserver = (preg_match("/^(?:www.|)eclipse.org$/", $_SERVER["SERVER_NAME"]));
