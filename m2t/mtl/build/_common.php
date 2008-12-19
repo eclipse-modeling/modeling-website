e78ca43db7f0e4622d3cd830a5f0ea31259a2725
jmusset <jmusset>
2008-12-19 10:53:56 +0000

    Change MTL qualifier version (0.8.0)

--- m2t/mtl/build/_common.php
+++ m2t/mtl/build/_common.php
@@ -6,10 +6,16 @@ $options = array (
 	
 	"BranchAndJDK" => array (
 		"emft.eclipse.org=------------,------------",
-		"0.7.0=HEAD,/opt/sun-java2-5.0",
+		"0.7.0=R0_7_maintenance,/opt/sun-java2-5.0",
 		
 		"build.eclipse.org=------------,------------",
-		"0.7.0=HEAD,/opt/sun-java2-5.0",
+		"0.7.0=R0_7_maintenance,/opt/sun-java2-5.0",
+		
+		"emft.eclipse.org=------------,------------",
+		"0.8.0=HEAD,/opt/sun-java2-5.0",
+		
+		"build.eclipse.org=------------,------------",
+		"0.8.0=HEAD,/opt/sun-java2-5.0",
 	),
 	
 	"Mapfile_Rule_Default" => 0, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, No Tagging=gen-false"
