09a534dbf9fde07e65bffc5049b1533f2285d7f7
jlescot <jlescot>
2009-01-13 16:53:26 +0000

    Make build page default select the regex-defined latest dependencies

--- emft/ecoretools/build/_common.php
+++ emft/ecoretools/build/_common.php
@@ -20,7 +20,7 @@ $options = array (
 	 * matching dependencies for running a build. See also releng-common/tools/scripts/start_cron.sh
  	 * and /home/www-data/build/requests/dependencies.urls.txt
  	 * */
- 	"regex" => "I200.*/eclipse-SDK-|[SR]-.*200.*/eclipse-SDK-|[ISR]200.*/emf-xsd-SDK-|[ISR]200.*/emf-sdo-xsd-SDK-|[ISR]200.*/mdt-ocl-.*SDK-|[ISR]200.*/emf-query-SDK-|[ISR]200.*/emf-validation-SDK-|[ISR]200.*/emf-transaction-SDK-|[ISR]200.*/GEF-|[ISR]200.*/gmf-",
+ 	"regex" => "I200.*/eclipse-SDK-|[SR]-.*200.*/eclipse-SDK-|[ISR]200.*/emf-xsd-SDK-|[ISR]200.*/mdt-ocl-.*SDK-|[ISR]200.*/emf-query-SDK-|[ISR]200.*/emf-validation-SDK-|[ISR]200.*/emf-transaction-SDK-|[ISR]200.*/GEF-|[ISR]200.*/gmf-",
 
 	"Mapfile_Rule_Default" => 0, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, No Tagging=gen-false"
 
