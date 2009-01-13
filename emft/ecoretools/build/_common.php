e5deceada225259b4535a462dab9b48440068815
jlescot <jlescot>
2009-01-13 16:40:10 +0000

    Make build page default select the regex-defined latest dependencies

--- emft/ecoretools/build/_common.php
+++ emft/ecoretools/build/_common.php
@@ -2,7 +2,7 @@
 require_once ("../../../includes/buildServer-common.php");
 
 $options = array (
-	"BaseBuilderBranch" => "R35_M2",
+	"BaseBuilderBranch" => "R35_M4",
 	
 	"BranchAndJDK" => array (
 		"emft.eclipse.org=------------,------------",
@@ -16,6 +16,12 @@ $options = array (
 		#"0.7.1=R0_7_maintenance,/opt/public/common/ibm-java2-ppc-50"
 	),
 	
+	/* define a regular expression to be used to collect the most recent
+	 * matching dependencies for running a build. See also releng-common/tools/scripts/start_cron.sh
+ 	 * and /home/www-data/build/requests/dependencies.urls.txt
+ 	 * */
+ 	"regex" => "I200.*/eclipse-SDK-|[SR]-.*200.*/eclipse-SDK-|[ISR]200.*/emf-xsd-SDK-|[ISR]200.*/emf-sdo-xsd-SDK-|[ISR]200.*/mdt-ocl-.*SDK-|[ISR]200.*/emf-query-SDK-|[ISR]200.*/emf-validation-SDK-|[ISR]200.*/emf-transaction-SDK-|[ISR]200.*/GEF-|[ISR]200.*/gmf-",
+
 	"Mapfile_Rule_Default" => 0, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, No Tagging=gen-false"
 
 	"EmailDefault" => "jacques.lescot@anyware-tech.com", // prefil email contact box with comma-sep'd list
