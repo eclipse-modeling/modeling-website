7acfeb479fae5e8a77dbadd1599ae9a4acd91803
estepper <estepper>
2009-01-31 13:56:14 +0000

    *** empty log message ***

--- emf/cdo/index.php
+++ emf/cdo/index.php
@@ -3,11 +3,9 @@ $project = "cdo";
 	
 require_once ("../../includes/buildServer-common.php");
 require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php"); 
-require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 
 require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 
 
 $App = new App(); 
-$Nav = new Nav();
 $Menu = new Menu(); 
 
 include($_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/db.php");
