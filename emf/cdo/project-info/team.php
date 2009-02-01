e19fdebb01a869366ed281a79d8419a9e6f649b5
estepper <estepper>
2009-02-01 13:06:50 +0000

    *** empty log message ***

--- emf/cdo/project-info/team.php
+++ emf/cdo/project-info/team.php
@@ -18,21 +18,25 @@ print '<div id="midcolumn">';
 
 print '<h1>Meet The ' . $projectName . 'Team</h1>';
 
-$result = wmysql_query("SELECT Name, Role, Company, Location, Website, PhotoURL, CommitterID FROM developers WHERE Role LIKE '%$comp%' ORDER BY SUBSTRING_INDEX(Name,' ',-1)");
+$result = wmysql_query("SELECT CommitterID, PhotoURL, Name, Company, Location, Role, Website, EMail FROM developers WHERE Role LIKE '%$comp%' ORDER BY SUBSTRING_INDEX(Name,' ',-1)");
 if ($result && mysql_num_rows($result) > 0)
 {
 	print '<p><table border="0" width="100%">' . "\n";
 	while ($row = mysql_fetch_row($result))
 	{
 		print '<tr><td width="25%" height="130" align="center" valign="top">' .
-		($row[5] && (preg_match("#https+://#", $row[5]) || is_file($_SERVER['DOCUMENT_ROOT'] . $row[5])) ?
-				'<img border="0" src="' . $row[5] . '" style="" height="120"/>' : '<img border="0" src="/modeling/images/team/eclipseface.png"/>') .
+		($row[1] && (preg_match("#https+://#", $row[1]) || is_file($_SERVER['DOCUMENT_ROOT'] . $row[1])) ?
+				'<img border="0" src="' . $row[1] . '" style="" height="120"/>' : '<img border="0" src="/modeling/images/team/eclipseface.png"/>') .
 				'</td><td align="left" valign="top">' . 
-		($row[4] ? '<a href="' . $row[4] . '">' . $row[0] . '</a>' : $row[0]) .	'<br/>' .
-		($row[1] ? $row[1] . "<br/>" : "") .
 		($row[2] ? $row[2] . "<br/>" : "") .
 		($row[3] ? $row[3] . "<br/>" : "") .
-		($row[6] ? '<a href="' . $_SERVER["PHP_SELF"] . "?committerid=" .  $row[6] .'">' . $row[6] . "</a><br/>" : "") .
+		($row[4] ? $row[4] . "<br/>" : "") .
+		($row[5] ? "<br/>" . $row[5] : "") .
+		'</td><td>' . "\n";
+		($row[0] ? $row[0] . "<br/>" : "") .
+		($row[6] ? '<a href="' . $row[6] . '"><img border="0" src="/modeling/emf/cdo/images/website.gif"/></a>&nbsp;" : "") .
+		($row[7] ? '<a href="' . $row[7] . '"><img border="0" src="/modeling/emf/cdo/images/email.gif"/></a>&nbsp;" : "") .
+		($row[0] ? '<a href="' . $_SERVER["PHP_SELF"] . "?committerid=" .$row[0] . '"><img border="0" src="/modeling/emf/cdo/images/statistics.gif"/></a>&nbsp;" : "") .
 			'</td></tr>' . "\n";
 	}
 
