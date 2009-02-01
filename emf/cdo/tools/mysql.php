3505b32da549a0a913344158c2cb0fd0bc140c09
estepper <estepper>
2009-02-01 11:54:33 +0000

    *** empty log message ***

--- emf/cdo/tools/mysql.php
+++ emf/cdo/tools/mysql.php
@@ -54,7 +54,7 @@ else
 			$row = mysql_fetch_row($result);
 			$count = $row[0];
 			
-			print '<h1><a href="mysql.php?table=' . $table[0] . '">' . $table[0] . '</a> (' . $count . ')</h1>' . "\n";
+			print '<h1><a href="' . $_SERVER["PHP_SELF"] . '?table=' . $table[0] . '">' . $table[0] . '</a> (' . $count . ')</h1>' . "\n";
 			$fields = wmysql_query("DESCRIBE " . $table[0] . ";");
 			if ($fields && mysql_num_rows($fields) > 0)
 			{
