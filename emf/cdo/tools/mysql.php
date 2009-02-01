9e11136eba9e25074e747bacb0869bd9949e0a62
estepper <estepper>
2009-02-01 10:26:41 +0000

    *** empty log message ***

--- emf/cdo/tools/mysql.php
+++ emf/cdo/tools/mysql.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once ("../../includes/buildServer-common.php");
+require_once ("../../../includes/buildServer-common.php");
 require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");
 require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php");
 require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php");
