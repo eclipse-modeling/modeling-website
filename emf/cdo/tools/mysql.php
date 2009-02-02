8052f3bf72434ea41ac6c1296b00f8867f54a418
estepper <estepper>
2009-02-02 06:56:19 +0000

    *** empty log message ***

--- emf/cdo/tools/mysql.php
+++ emf/cdo/tools/mysql.php
@@ -16,19 +16,20 @@ ob_start();
 print '<div id="midcolumn">';
 ########################################################################
 
+function getParameter($name, $defvalue)
+{
+	if (isset($_GET[$name])) return $_GET[$name];
+	return $defvalue;
+}
+
 function query($sql)
 {
-	$dbname = "modeling";
-	if (isset($_GET["dbname"]))
-	{
-		$dbname = $_GET["dbname"];
-	}
-	
+	$dbname = getParameter("dbname", "modeling");
 	if ($dbname == "modeling")
 	{
 		return wmysql_query($sql);
 	}
-	
+
 	global $App;
 	return $App->sql($sql, $dbname);
 }
@@ -37,18 +38,27 @@ if (isset($_GET["table"]))
 {
 	$pageTitle = $_GET["table"];
 	print '<h1>' . $_GET["table"] . '</h1>' . "\n";
+
 	$result = query("SELECT * FROM " . $_GET["table"] . ";");
 	if ($result && mysql_num_rows($result) > 0)
 	{
+		$page = getParameter("page", 0);
+		$records = getParameter("records", 100);
+
 		print '<table border="1"><tr>' ."\n";
+		print "<th>&nbsp;</th>\n";
 		for ($index = 0; $index < mysql_num_fields($result); $index++) {
 			print "<th>" . mysql_field_name($result, $index) . "</th>\n";
 		}
 
 		print "</tr>\n";
-		while ($row = mysql_fetch_row($result))
+		$i = 0;
+		$offset = $page * $records;
+		mysql_data_seek($result, $offset);
+		while ($i++ < $records && $row = mysql_fetch_row($result))
 		{
 			print "<tr>\n";
+			print "<td>" . $offset++ . "</td>\n";
 			for ($index = 0; $index < mysql_num_fields($result); $index++) {
 				print "<td>" . $row[$index] . "</td>\n";
 			}
@@ -56,7 +66,7 @@ if (isset($_GET["table"]))
 			print "</tr>\n";
 		}
 
-		print "</table><br>\n";
+		print "</table><br/>\n";
 	}
 }
 else
@@ -70,7 +80,7 @@ else
 			$result = query("SELECT COUNT(*) FROM " . $table[0] . ";");
 			$row = mysql_fetch_row($result);
 			$count = $row[0];
-			
+
 			print '<h1><a href="' . $_SERVER["PHP_SELF"] . '?table=' . $table[0] . '">' . $table[0] . '</a> (' . $count . ')</h1>' . "\n";
 			$fields = query("DESCRIBE " . $table[0] . ";");
 			if ($fields && mysql_num_rows($fields) > 0)
