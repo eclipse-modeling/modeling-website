baff4ae0550d8bdfe23088541e1966a374fe7e68
estepper <estepper>
2009-02-02 08:00:13 +0000

    *** empty log message ***

--- emf/cdo/test/include.php
+++ emf/cdo/test/include.php
@@ -1,4 +1,6 @@
 <?php
 
-print $_SERVER["PHP_SELF"] . "<br/>\n";
+print '$_SERVER["PHP_SELF"]: ' . $_SERVER["PHP_SELF"] . "<br/>\n";
+print '__FILE__: ' . __FILE__ . "<br/>\n";
+
 ?>
--- emf/cdo/test/index.php
+++ emf/cdo/test/index.php
@@ -18,7 +18,9 @@ print '<div id="midcolumn">';
 
 $pageTitle = 'Test Page';
 print "<h1>" . $pageTitle . "</h1>\n";
-print $_SERVER["PHP_SELF"] . "<br/>\n";
+print '$_SERVER["PHP_SELF"]: ' . $_SERVER["PHP_SELF"] . "<br/>\n";
+print '__FILE__: ' . __FILE__ . "<br/>\n";
+print "<br/>\n";
 require_once("include.php"); 
 
 ########################################################################
