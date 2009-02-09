842a5718afd64104185d255874066f9a1df0e667
mtaal <mtaal>
2009-02-09 08:39:09 +0000

    Removed text as it is not required anymore

--- emf/downloads/extras-teneo.php
+++ emf/downloads/extras-teneo.php
@@ -10,8 +10,7 @@ function doRequirements()
 ?>
 <div class="homeitem3col">
 	<h3>Teneo Builds/Releases</h3>
-	<p style="padding-left:10px">Teneo has recently graduated to the first 1.0 release. New maintenance builds are published frequently. 
-	<b>The maintenance builds (see bottom of the page) are the preferred builds to be used</b>.</p>
+	<p style="padding-left:10px"></b>.</p>
 </div>
 <?php
 }
