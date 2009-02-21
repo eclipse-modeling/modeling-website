b32bb1e12df04cc756eb764f5f59d57af772e4ee
nickb <nickb>
2009-02-21 22:41:10 +0000

    [265302] use explicit version matches for emf 2.5, oqtv 1.3

--- emf/query/build/_common.php
+++ emf/query/build/_common.php
@@ -28,7 +28,10 @@ $options = array (
 	 * matching dependencies for running a build. See also releng-common/tools/scripts/start_cron.sh
 	 * and /home/www-data/build/requests/dependencies.urls.txt 
 	 * */ 	
-	"regex" => "I200.*/eclipse-SDK-|[SR]-.*200.*/eclipse-SDK-|[ISR]200.*/emf-xsd-SDK-|[ISR]200.*/emf-sdo-xsd-SDK-|[ISR]200.*mdt-ocl-.*SDK-",
+	"regex" => "I200.*/eclipse-SDK-|[SR]-.*200.*/eclipse-SDK-|" .
+			"2\.5\..+/[ISR]200.*/emf-xsd-SDK-|" .
+			"3\.0\..+/[ISR]200.*/mdt-uml2-SDK|" .
+			"1\.3\..+/[ISR]200.*mdt-ocl-.*SDK-",
 
 	"Mapfile_Rule_Default" => 0, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, No Tagging=gen-false"
 
