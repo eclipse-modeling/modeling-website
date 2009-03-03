80f26fd29f0d3730b96490119db2b1b4b5bb82e3
dhubner <dhubner>
2009-03-03 08:04:55 +0000

    allow eclipse SDK 3.5 only

--- emft/mwe/build/_common.php
+++ emft/mwe/build/_common.php
@@ -11,7 +11,7 @@ $options = array (
 		"build.eclipse.org=------------,------------",
 		"0.7.0=HEAD,/opt/public/common/ibm-java2-ppc-50"
 	),
-	"regex" => "[ISR]200.*/eclipse-SDK-|[SR]-.*200.*/eclipse-SDK-|[SR]200.*/emf-runtime-|[SR]200.*/orbit-R*",
+	"regex" => "[SR]-3.5.*200.*/eclipse-SDK-|[NMISR]200.*/emf-runtime-|[SR]200.*/orbit-R*",
 	
 	"Mapfile_Rule_Default" => 0, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, No Tagging=gen-false"
 
