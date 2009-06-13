20eec8a1e614f42b9aaecacd62771704a72eb6de
sefftinge <sefftinge>
2009-06-13 15:01:07 +0000

    updated URL

--- tmf/downloads/extras-xtext.php
+++ tmf/downloads/extras-xtext.php
@@ -4,8 +4,8 @@ function showNotes()
 ?>
 	<div class="homeitem3col">
 		<h3>Notes</h3>
-		<p>In addition you need to install the antlr generator.</p>
-		<p>Update site: <a href="http://xtext.itemis.com/updates/milestones/site.xml">http://xtext.itemis.com/updates/milestones/site.xml</a>
+		<p>In addition you need to install the antlr generator from itemis.com.</p>
+		<p>Update site: <a href="http://download.itemis.com/updates/milestones/site.xml">http://download.itemis.com/updates/milestones/site.xml</a>
 		</p>
 	</div>
 <?php
