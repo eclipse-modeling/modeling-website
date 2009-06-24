0bc70f8968631c5fad3bd09251cd9b932319d007
dhubner <dhubner>
2009-06-24 12:37:22 +0000

    changed download url

--- tmf/downloads/extras-xtext.php
+++ tmf/downloads/extras-xtext.php
@@ -5,7 +5,7 @@ function showNotes()
 	<div class="homeitem3col">
 		<h3>Notes</h3>
 		<p>In addition you need to install the antlr generator from itemis.com.</p>
-		<p>Update site: <a href="http://download.itemis.com/updates/milestones/site.xml">http://download.itemis.com/updates/milestones/site.xml</a>
+		<p>Update site: <a href="http://download.itemis.com/updates/">http://download.itemis.com/updates/</a>
 		</p>
 	</div>
 <?php
