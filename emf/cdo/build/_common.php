0808b5492e158f9c8398fa8585b2de5cd5f36e67
estepper <estepper>
2009-06-27 07:29:59 +0000

    Adjust versions to 3.0.0 in development stream

--- emf/cdo/build/_common.php
+++ emf/cdo/build/_common.php
@@ -6,16 +6,28 @@ $options = array (
 
 	"BranchAndJDK" => array (
 		"emft.eclipse.org=------------,------------",
-		"2.0.0=HEAD,/opt/sun-java2-5.0",
-		"1.0.7=R1_0_maintenance,/opt/sun-java2-5.0",
-		"1.0.8=R1_0_maintenance,/opt/sun-java2-5.0",
-		"1.0.9=R1_0_maintenance,/opt/sun-java2-5.0",
+		"3.0.0=HEAD,/opt/sun-java2-5.0",
+		"2.0.1=R2_0_maintenance,/opt/sun-java2-5.0",
+		"2.0.2=R2_0_maintenance,/opt/sun-java2-5.0",
+		"2.0.3=R2_0_maintenance,/opt/sun-java2-5.0",
+		"2.0.4=R2_0_maintenance,/opt/sun-java2-5.0",
+		"2.0.5=R2_0_maintenance,/opt/sun-java2-5.0",
+		"2.0.6=R2_0_maintenance,/opt/sun-java2-5.0",
+		"2.0.7=R2_0_maintenance,/opt/sun-java2-5.0",
+		"2.0.8=R2_0_maintenance,/opt/sun-java2-5.0",
+		"2.0.9=R2_0_maintenance,/opt/sun-java2-5.0",
 
 		"modeling.eclipse.org=------------,------------",
-		"2.0.0=HEAD,/opt/sun-java2-5.0",
-		"1.0.7=R1_0_maintenance,/opt/sun-java2-5.0",
-		"1.0.8=R1_0_maintenance,/opt/sun-java2-5.0",
-		"1.0.9=R1_0_maintenance,/opt/sun-java2-5.0",
+		"3.0.0=HEAD,/opt/sun-java2-5.0",
+		"2.0.1=R2_0_maintenance,/opt/sun-java2-5.0",
+		"2.0.2=R2_0_maintenance,/opt/sun-java2-5.0",
+		"2.0.3=R2_0_maintenance,/opt/sun-java2-5.0",
+		"2.0.4=R2_0_maintenance,/opt/sun-java2-5.0",
+		"2.0.5=R2_0_maintenance,/opt/sun-java2-5.0",
+		"2.0.6=R2_0_maintenance,/opt/sun-java2-5.0",
+		"2.0.7=R2_0_maintenance,/opt/sun-java2-5.0",
+		"2.0.8=R2_0_maintenance,/opt/sun-java2-5.0",
+		"2.0.9=R2_0_maintenance,/opt/sun-java2-5.0",
 	),
 
 	/* define a regular expression to be used to collect the most recent
@@ -29,7 +41,7 @@ $options = array (
 
 	"Mapfile_Rule_Default" => 0, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, No Tagging=gen-false"
 
-	"EmailDefault" => "stepper@esc-net.de,smcduff@hotmail.com,mtaal@elver.org,stefan.winkler-et@fernuni-hagen.de,vroldan@opencanarias.com,dietisheim@puzzle.ch", // prefil email contact box with comma-sep'd list
+	"EmailDefault" => "stepper@esc-net.de,smcduff@hotmail.com,mtaal@elver.org,stefan.winkler-et@fernuni-hagen.de,vroldan@opencanarias.com,dietisheim@puzzle.ch,ibrahim.sallam@objectivity.com", // prefil email contact box with comma-sep'd list
 
 	"Users" => array("estepper","estepper",null) /* build user, eclipse cvs user, IES cvs user */
 );
