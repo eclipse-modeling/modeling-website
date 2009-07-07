8a9ab55a53648dab0e2f194693a40690b7c413a2
radvorak <radvorak>
2009-07-07 18:50:50 +0000

    migrate build to modelign.eclipse.org

--- m2m/qvtoml/build/_common.php
+++ m2m/qvtoml/build/_common.php
@@ -9,6 +9,10 @@ $options = array (
 		"2.0.0=HEAD,/opt/sun-java2-5.0",
 		"2.0.1=R2_0_maintenance,/opt/sun-java2-5.0",
 
+		"modeling.eclipse.org=------------,------------",
+		"2.0.0=HEAD,/opt/sun-java2-5.0",
+		"2.0.1=R2_0_maintenance,/opt/sun-java2-5.0",
+		
 		"build.eclipse.org=------------,------------",
 		"2.0.0=HEAD,/opt/public/common/ibm-java2-ppc-50",
 		"2.0.1=R2_0_maintenance,/opt/public/common/ibm-java2-ppc-50"
