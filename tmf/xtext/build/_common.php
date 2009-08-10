55312fdb177538bb8670e3f9b02b97fe4828650b
sefftinge <sefftinge>
2009-08-10 12:45:20 +0000

    updated version of Xpand and Xtext from 0.7.1 to 0.7.2

--- tmf/xtext/build/_common.php
+++ tmf/xtext/build/_common.php
@@ -6,13 +6,13 @@ $options = array (
 
 	"BranchAndJDK" => array (
 		"emft.eclipse.org=------------,------------",
-		"0.7.1=HEAD,/opt/sun-java2-5.0",
+		"0.7.2=HEAD,/opt/sun-java2-5.0",
 
 		"modeling.eclipse.org=------------,------------",
-        "0.7.1=HEAD,/opt/sun-java2-5.0",
+        "0.7.2=HEAD,/opt/sun-java2-5.0",
         
 		"build.eclipse.org=------------,------------",
-		"0.7.1=HEAD,/opt/public/common/ibm-java2-ppc-50",
+		"0.7.2=HEAD,/opt/public/common/ibm-java2-ppc-50",
 
 	),
 
