697dc753c05037a63345c8c9f4274534dd6616fd
ahunter <ahunter>
2009-08-12 20:06:28 +0000

    [286340] gmf-head ahunter 090912 R2_2_maintenance builds are 2.2.1

--- gmp/gmf/build/_common.php
+++ gmp/gmf/build/_common.php
@@ -7,11 +7,11 @@ $options = array (
 	"BranchAndJDK" => array (
 		"emf.torolab.ibm.com=------------,------------",
         "2.3.0=HEAD,/opt/sun-java2-5.0",
-        "2.2.x=R2_2_maintenance,/opt/sun-java2-5.0",
+        "2.2.1=R2_2_maintenance,/opt/sun-java2-5.0",
         
         "modeling.eclipse.org=------------,------------",
         "2.3.0=HEAD,/opt/sun-java2-6.0_64",
-        "2.2.x=R2_2_maintenance,/opt/sun-java2-6.0_64",
+        "2.2.1=R2_2_maintenance,/opt/sun-java2-6.0_64",
 	),
 
 	/* define a regular expression to be used to collect the most recent 
