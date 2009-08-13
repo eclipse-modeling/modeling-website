3c932814050ebccf0b8cee363f44c54d7c5d492f
bgruschko <bgruschko>
2009-08-13 08:09:26 +0000

    revert

--- emf/validation/build/_common.php
+++ emf/validation/build/_common.php
@@ -6,22 +6,19 @@ $options = array (
 	
 	"BranchAndJDK" => array (
 		"emf.torolab.ibm.com=------------,------------",
-        "1.4.0=HEAD,/opt/sun-java2-5.0",
-        "1.3.1=R1_3_maintenance,/opt/sun-java2-5.0",
+        "1.3.0=HEAD,/opt/sun-java2-5.0",
         "1.2.1=R1_2_maintenance,/opt/sun-java2-5.0",
 		"1.1.1=R1_1_maintenance,/opt/sun-java2-5.0",
 		#"1.0.3=R1_0_maintenance,/opt/sun-java2-1.4",
 		
 		"emft.eclipse.org=------------,------------",
-        "1.4.0=HEAD,/opt/sun-java2-5.0",
-        "1.3.1=R1_3_maintenance,/opt/sun-java2-5.0",
+        "1.3.0=HEAD,/opt/sun-java2-5.0",
         "1.2.1=R1_2_maintenance,/opt/sun-java2-5.0",
 		"1.1.1=R1_1_maintenance,/opt/sun-java2-5.0",
 		#"1.0.3=R1_0_maintenance,/opt/sun-java2-1.4",
 		
 		"build.eclipse.org=------------,------------",
-        "1.4.0=HEAD,/opt/sun-java2-5.0",
-        "1.3.1=R1_3_maintenance,/opt/sun-java2-5.0",
+        "1.3.0=HEAD,/opt/public/common/ibm-java2-ppc-50",
         "1.2.1=R1_2_maintenance,/opt/public/common/ibm-java2-ppc-50",
 		"1.1.1=R1_1_maintenance,/opt/public/common/ibm-java2-ppc-50",
 		#"1.0.3=R1_0_maintenance,/opt/public/stp/apps/IBMJava2-ppc-142" // bug 178681
@@ -37,7 +34,7 @@ $options = array (
 	
 	"Mapfile_Rule_Default" => 0, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, No Tagging=gen-false"
 
-	"EmailDefault" => "boris.gruschko@sap.com", // prefil email contact box with comma-sep'd list
+	"EmailDefault" => "nickboldt@gmail.com", // prefil email contact box with comma-sep'd list
 	
     "Users" => array("nickb","nickb",NULL) /* build user, eclipse cvs user, IES cvs user */
 );
