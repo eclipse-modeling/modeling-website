a9ef84b88aaeb21cf827eeb6bf7cc584f9ada432
bgruschko <bgruschko>
2009-08-13 08:36:09 +0000

    added modeling build server

--- emf/validation/build/_common.php
+++ emf/validation/build/_common.php
@@ -11,6 +11,12 @@ $options = array (
 		"1.1.1=R1_1_maintenance,/opt/sun-java2-5.0",
 		#"1.0.3=R1_0_maintenance,/opt/sun-java2-1.4",
 		
+		"modeling.eclipse.org=------------,------------",
+        "1.3.0=HEAD,/opt/sun-java2-5.0",
+        "1.2.1=R1_2_maintenance,/opt/sun-java2-5.0",
+		"1.1.1=R1_1_maintenance,/opt/sun-java2-5.0",
+		#"1.0.3=R1_0_maintenance,/opt/sun-java2-1.4",
+		
 		"emft.eclipse.org=------------,------------",
         "1.3.0=HEAD,/opt/sun-java2-5.0",
         "1.2.1=R1_2_maintenance,/opt/sun-java2-5.0",
@@ -34,7 +40,7 @@ $options = array (
 	
 	"Mapfile_Rule_Default" => 0, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, No Tagging=gen-false"
 
-	"EmailDefault" => "nickboldt@gmail.com", // prefil email contact box with comma-sep'd list
+	"EmailDefault" => "boris.gruschko@sap.com", // prefil email contact box with comma-sep'd list
 	
     "Users" => array("nickb","nickb",NULL) /* build user, eclipse cvs user, IES cvs user */
 );
