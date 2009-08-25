a6d345c7c946c7571c9eaca4668c6b94ca906d5f
wpiers <wpiers>
2009-08-25 14:32:44 +0000

    updated build page

--- m2m/atl/build/_common.php
+++ m2m/atl/build/_common.php
@@ -10,6 +10,11 @@ $options = array (
 		"2.0.2=R2_0_maintenance,/opt/sun-java2-5.0",
 		"2.0.1=R2_0_maintenance,/opt/sun-java2-5.0",
 		
+		"modeling.eclipse.org=------------,------------",
+		"3.0.0=HEAD,/opt/sun-java2-5.0",
+		"2.0.2=R2_0_maintenance,/opt/sun-java2-5.0",
+		"2.0.1=R2_0_maintenance,/opt/sun-java2-5.0",
+		
 		"build.eclipse.org=------------,------------",
 		"3.0.0=HEAD,/opt/public/common/ibm-java2-ppc-50",
 		"2.0.2=R2_0_maintenance,/opt/public/common/ibm-java2-ppc-50",
