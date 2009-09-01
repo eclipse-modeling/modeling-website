50ba55352069c7a4d757bda2c010b606d945d9c1
cbrun <cbrun>
2009-09-01 13:54:25 +0000

    change 1.0.0 to 1.0.1

--- emf/compare/build/_common.php
+++ emf/compare/build/_common.php
@@ -6,7 +6,7 @@ $options = array (
 	
 	"BranchAndJDK" => array (
 		"emft.eclipse.org=------------,------------",
-		"1.0.0=HEAD,/opt/sun-java2-5.0",
+		"1.0.1=HEAD,/opt/sun-java2-5.0",
 		"0.8.1=R0_8_maintenance,/opt/sun-java2-5.0",
 		"0.7.0=HEAD,/opt/sun-java2-5.0",
 		"0.7.1=R0_7_maintenance,/opt/sun-java2-5.0",
