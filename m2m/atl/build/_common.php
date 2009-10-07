7e5f71934a145827505108f3cdad8bed1178cc15
wpiers <wpiers>
2009-10-07 12:36:56 +0000

    updated version number

--- m2m/atl/build/_common.php
+++ m2m/atl/build/_common.php
@@ -7,17 +7,17 @@ $options = array (
 	"BranchAndJDK" => array (
 		"emft.eclipse.org=------------,------------",
 		"3.1.0=HEAD,/opt/sun-java2-5.0",
-		"3.0.1=R3_0_maintenance,/opt/sun-java2-5.0",
+		"3.0.2=R3_0_maintenance,/opt/sun-java2-5.0",
 		"2.0.3=R2_0_maintenance,/opt/sun-java2-5.0",
 		
 		"modeling.eclipse.org=------------,------------",
 		"3.1.0=HEAD,/opt/sun-java2-5.0",
-		"3.0.1=R3_0_maintenance,/opt/sun-java2-5.0",
+		"3.0.2=R3_0_maintenance,/opt/sun-java2-5.0",
 		"2.0.3=R2_0_maintenance,/opt/sun-java2-5.0",
 		
 		"build.eclipse.org=------------,------------",
 		"3.1.0=HEAD,/opt/public/common/ibm-java2-ppc-50",
-		"3.0.1=R3_0_maintenance,/opt/public/common/ibm-java2-ppc-50",
+		"3.0.2=R3_0_maintenance,/opt/public/common/ibm-java2-ppc-50",
 		"2.0.3=R2_0_maintenance,/opt/public/common/ibm-java2-ppc-50",
 	),
 	
