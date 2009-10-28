f77fe51dad150970bd5833764044fdcdb3d0dc97
nickb <nickb>
2009-10-28 17:02:43 +0000

    *** empty log message ***

--- emf/downloads/index.php
+++ emf/downloads/index.php
@@ -64,7 +64,7 @@ $dls = array(
 		"Model Query 2 (Incubating)" => array( # same as value in _projectCommon.php's $projects array
 			"<acronym title=\"Click to download archived All-In-One p2 Repo Update Site\"><img alt=\"Click to download archived All-In-One p2 Repo Update Site\" src=\"/modeling/images/dl-icon-update-zip.gif\"/> <b style=\"color:green\">All-In-One Update Site</b></acronym>" => "Update",
 			"SDK (Runtime, Source)" => "SDK",
-			"Automated Tests (Hibernate and EclipseLink)" => "Automated-Tests"
+			"Automated Tests" => "Automated-Tests"
 		)
 	),
 	"/teneo" => array(
