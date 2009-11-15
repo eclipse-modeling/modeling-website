ae6403a5ad2365e9dffbafb50ec7a012beb03d6f
atikhomirov <atikhomirov>
2009-11-15 22:44:08 +0000

    update regexp to match latest deps

--- gmp/gmf/build/_common.php
+++ gmp/gmf/build/_common.php
@@ -16,15 +16,15 @@ $options = array (
 	 * and /home/www-data/build/requests/dependencies.urls.txt 
 	 * */ 	
 	"regex" => "I200.*/eclipse-SDK-|[SR]-.*200.*/eclipse-SDK-|" .
-			"2\.5\..+/[ISR]200.*/emf-xsd-SDK-|" .
-			"3\.0\..+/[ISR]200.*/mdt-uml2-SDK|" .
-			"1\.3\..+/[ISR]200.*/mdt-ocl-.*SDK-|" .
+			"2\.[56]\..+/[ISR]200.*/emf-xsd-SDK-|" .
+			"3\.[01]\..+/[ISR]200.*/mdt-uml2-SDK|" .
+			"1\.[34]\..+/[ISR]200.*/mdt-ocl-.*SDK-|" .
 			"1\.3\..+/[ISR]200.*/emf-query-SDK-|" .
 			"1\.3\..+/[ISR]200.*/emf-validation-SDK-|" .
 			"1\.3\..+/[ISR]200.*/emf-transaction-SDK-|" .
-			"2\.0\..+/[ISR]200.*/m2m-qvtoml-SDK-|" .
+			"[23]\.0\..+/[ISR]200.*/m2m-qvtoml-SDK-|" .
 			"[ISR]200.*/GEF-|" .
-			"orbitBundles-",
+			"orbitBundles-.*\.map",
 
 	"Mapfile_Rule_Default" => 0, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, No Tagging=gen-false"
 
