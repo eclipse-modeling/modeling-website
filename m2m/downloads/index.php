e9768571f332e13757c27496f193d88a9f0780b2
radvorak <radvorak>
2009-12-08 23:08:44 +0000

    fix link to Automated-Tests

--- m2m/downloads/index.php
+++ m2m/downloads/index.php
@@ -33,7 +33,8 @@ $dls = array(
 			"SDK (Runtime, Source, Examples)" => "SDK",
 			"Runtime" => "runtime",
 			"Examples" => "examples",
-			"Automated Tests" => "automated-tests"
+			"Automated Tests" => "automated-tests", # deprecated Modeling build version
+			"Automated-Tests" => "Automated-Tests" # new Athena version
 		)
 	),
 	"/atl" => array(
