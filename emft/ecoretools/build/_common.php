70746c21f3ba79abae789e1e01a95309699ab196
jlescot <jlescot>
2009-12-23 13:18:10 +0000

    Show 0.10.0 artifacts

--- emft/ecoretools/build/_common.php
+++ emft/ecoretools/build/_common.php
@@ -6,11 +6,13 @@ $options = array (
 	
 	"BranchAndJDK" => array (
 		"emft.eclipse.org=------------,------------",
+		"0.10.0=HEAD,/opt/sun-java2-5.0",
 		"0.9.0=HEAD,/opt/sun-java2-5.0",
 		"0.8.0=HEAD,/opt/sun-java2-5.0",
 		#"0.7.1=R0_7_maintenance,/opt/sun-java2-5.0",
 		
 		"build.eclipse.org=------------,------------",
+		"0.10.0=HEAD,/opt/public/common/ibm-java2-ppc-50",
 		"0.9.0=HEAD,/opt/public/common/ibm-java2-ppc-50",
 		"0.8.0=HEAD,/opt/public/common/ibm-java2-ppc-50",
 		#"0.7.1=R0_7_maintenance,/opt/public/common/ibm-java2-ppc-50"
@@ -24,7 +26,7 @@ $options = array (
 
 	"Mapfile_Rule_Default" => 0, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, No Tagging=gen-false"
 
-	"EmailDefault" => "jacques.lescot@anyware-tech.com", // prefil email contact box with comma-sep'd list
+	"EmailDefault" => "jlescot@sierrawireless.com", // prefil email contact box with comma-sep'd list
 	
 	"Users" => array("jlescot","jlescot",null) /* build user, eclipse cvs user, IES cvs user */
 );
