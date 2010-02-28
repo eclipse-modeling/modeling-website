a70424a2085c30d474f4913d17b74a9224f01052
ckrause <ckrause>
2010-02-28 17:37:11 +0000

    added SVN url to install section ----------------------------------------------------------------------

--- emft/henshin/downloads.php
+++ emft/henshin/downloads.php
@@ -1,34 +1,32 @@
-<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+<?php
 
-	#*****************************************************************************
-	#
-	# template.php
-	#
-	# Author: 		Denis Roy
-	# Date:			2005-06-16
-	#
-	# Description: Type your page comments here - these are not sent to the browser
-	#
-	#
-	#****************************************************************************
-	
-	#
-	# Begin: page-specific settings.  Change these. 
-	$pageTitle 		= "Henshin - Downloads";
-	$pageKeywords	= "EMF, Henshin, model transformation, Downloads";
-	$pageAuthor		= "Christian Krause";
-	
-	# Add page-specific Nav bars here
-	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
-	# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
-	# $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
-	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 
 
-	# End: page-specific settings
-	#
+$App = new App();
+$Nav = new Nav();
+$Menu = new Menu();
+
+include($App->getProjectCommon());
+
+#
+# Begin: page-specific settings.  Change these. 
+$pageTitle 		= "Henshin - Downloads";
+$pageKeywords	= "EMF, Henshin, model transformation, Downloads";
+$pageAuthor		= "Christian Krause";
+	
+# Add page-specific Nav bars here
+# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
+# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
+# $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
+# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
+
+# End: page-specific settings
+#
 		
-	# Paste your HTML content between the EOHTML markers!	
-	$html = <<<EOHTML
+# Paste your HTML content between the EOHTML markers!	
+$html = <<<EOHTML
 
 <div id="maincontent">
 
@@ -36,7 +34,7 @@
 		<h1>$pageTitle</h1>
 		
 		<p>
-		<b>The first release is scheduled Februar 2010.</b>
+		<b>The first release is scheduled for February 2010.</b>
 		</p>
 
 		<!--
@@ -88,7 +86,7 @@
 
 EOHTML;
 
+# Generate the web page
+$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
 
-	# Generate the web page
-	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
 ?>
--- emft/henshin/index.php
+++ emft/henshin/index.php
@@ -1,34 +1,31 @@
-<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 
 
-	#*****************************************************************************
-	#
-	# template.php
-	#
-	# Author: 		Denis Roy
-	# Date:			2005-06-16
-	#
-	# Description: Type your page comments here - these are not sent to the browser
-	#
-	#
-	#****************************************************************************
-	
-	#
-	# Begin: page-specific settings.  Change these. 
-	$pageTitle 		= "Henshin";
-	$pageKeywords	= "EMF, Henshin, model transformation";
-	$pageAuthor		= "Christian Krause";
-	
-	# Add page-specific Nav bars here
-	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
-	# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
-	# $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
-	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
+$App = new App();
+$Nav = new Nav(); 
+$Menu = new Menu();
 
-	# End: page-specific settings
-	#
+include($App->getProjectCommon());
+
+#
+# Begin: page-specific settings.  Change these. 
+$pageTitle 		= "Henshin";
+$pageKeywords	= "EMF, Henshin, model transformation";
+$pageAuthor		= "Christian Krause";
+	
+# Add page-specific Nav bars here
+# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
+# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
+# $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
+# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
+
+# End: page-specific settings
+#
 		
-	# Paste your HTML content between the EOHTML markers!	
-	$html = <<<EOHTML
+# Paste your HTML content between the EOHTML markers!	
+$html = <<<EOHTML
 
 <div id="maincontent">
 
@@ -113,6 +110,7 @@
 
 EOHTML;
 
-	# Generate the web page
-	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+# Generate the web page
+$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+
 ?>
