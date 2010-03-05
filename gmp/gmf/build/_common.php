3675f243ed669d326ce13b6d8ffc49e0e7347254
ahunter <ahunter>
2010-03-05 23:08:46 +0000

    gmf-head ahunter 100305 Change build user to Anthony

--- gmp/gmf/build/_common.php
+++ gmp/gmf/build/_common.php
@@ -29,7 +29,8 @@ $options = array (
 	"Mapfile_Rule_Default" => 0, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, No Tagging=gen-false"
 
 	"EmailDefault" => "anthonyh@ca.ibm.com", // prefil email contact box with comma-sep'd list: can't send to gmf-releng@eclipse.org, so use actual recipients
-	"Users" => array("nickb","nickb",NULL), /* build user, eclipse cvs user, IES cvs user */
+
+	"Users" => array("ahunter","ahunter","anthonyh") /* build user, eclipse cvs user, IES cvs user */
 );
 
 ?>
