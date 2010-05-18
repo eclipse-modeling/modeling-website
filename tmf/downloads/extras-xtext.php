94d2816425401d538600f0b73465bcdb88ad4b57
sefftinge <sefftinge>
2010-05-18 21:05:46 +0000

    updated link to external website

--- tmf/downloads/extras-xtext.php
+++ tmf/downloads/extras-xtext.php
@@ -4,8 +4,7 @@ function showNotes()
 ?>
 	<div class="homeitem3col">
 		<h3>Notes</h3>
-		<p>In addition you need to install the antlr generator from itemis.com.</p>
-		<p>Update site: <a href="http://download.itemis.com/updates/">http://download.itemis.com/updates/</a>
+		<p>In addition we highly recommend to install the ANTLR generator from <a href="http://xtext.itemis.com/xtext/language=en/23947/downloads" target="new_window">itemis.com</a>.</p>
 		</p>
 	</div>
 <?php
