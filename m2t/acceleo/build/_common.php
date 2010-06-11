6544814db79b171591539cb071903cb6ad6e7787
lgoubet <lgoubet>
2010-06-11 14:28:39 +0000

    Changing build to allow 3.0 maintenance building

--- m2t/acceleo/build/_common.php
+++ m2t/acceleo/build/_common.php
@@ -7,29 +7,32 @@ $options = array (
 	"BranchAndJDK" => array (
 		"emft.eclipse.org=------------,------------",
 		"0.7.0=R0_7_maintenance,/opt/sun-java2-5.0",
-		
 		"build.eclipse.org=------------,------------",
 		"0.7.0=R0_7_maintenance,/opt/sun-java2-5.0",
 		
 		"emft.eclipse.org=------------,------------",
 		"0.8.0=R0_8_maintenance,/opt/sun-java2-5.0",
-		
 		"modeling.eclipse.org=------------,------------",
 		"0.8.1=R0_8_maintenance,/opt/sun-java2-5.0",
 		
 		"emft.eclipse.org=------------,------------",
-		"3.0.0=HEAD,/opt/sun-java2-5.0",
-		
+		"3.0.0=R3_0_0_maintenance,/opt/sun-java2-5.0",
 		"build.eclipse.org=------------,------------",
-		"3.0.0=HEAD,/opt/sun-java2-5.0",
-		
+		"3.0.0=R3_0_0_maintenance,/opt/sun-java2-5.0",
 		"modeling.eclipse.org=------------,------------",
-		"3.0.0=HEAD,/opt/sun-java2-5.0",
+		"3.0.0=R3_0_0_maintenance,/opt/sun-java2-5.0",
+		
+		"emft.eclipse.org=------------,------------",
+		"3.1.0=HEAD,/opt/sun-java2-5.0",
+		"build.eclipse.org=------------,------------",
+		"3.1.0=HEAD,/opt/sun-java2-5.0",
+		"modeling.eclipse.org=------------,------------",
+		"3.1.0=HEAD,/opt/sun-java2-5.0",
 	),
 	
 	"Mapfile_Rule_Default" => 0, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, No Tagging=gen-false"
 
-	"EmailDefault" => "cedric.brun@obeo.fr", // prefil email contact box with comma-sep'd list
+	"EmailDefault" => "laurent.goubet@obeo.fr", // prefil email contact box with comma-sep'd list
 	
 	"Users" => array("cbrun","cbrun",null) /* build user, eclipse cvs user, IES cvs user */
 );
