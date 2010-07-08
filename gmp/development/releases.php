52e1b2f8d0f75273b13a7abe8ad5fde5ada7c88d
ahunter <ahunter>
2010-07-08 22:38:33 +0000

    gmf-head ahunter 100708 GMP and GMF releases table

--- gmp/development/releases.php
+++ gmp/development/releases.php
@@ -13,13 +13,11 @@ $pageKeywords	= "developer,resources,modeling,graphical";
 $pageAuthor		= "Anthony Hunter";
 	
 # Paste your HTML content between the EOHTML markers!
-# $Id: releases.php,v 1.4 2010/07/08 22:30:52 ahunter Exp $	
 $html = <<<EOHTML
 
+<!-- $Id: releases.php,v 1.5 2010/07/08 22:38:33 ahunter Exp $ -->	
 <style type="text/css">
    h1 { font-family : Arial, Helvetica, sans-serif; font-size : 14pt; font-weight : bold; font-style : normal; }
-   h2 { font-family : Arial, Helvetica, sans-serif; font-size : 12pt; font-weight : bold; font-style : normal; }
-   h3 { font-family : Arial, Helvetica, sans-serif; font-size : 10pt; font-weight : bold; font-style : normal; }
    table { text-align:left; padding: 3px; border: 1px solid black; }
    tr.white { background-color: white; border: 1px solid black; }
    tr.top { background-color: #CCFFFF; border: 1px solid black; }
@@ -30,9 +28,6 @@ $html = <<<EOHTML
    tr.helios { background-color: #CCFFFF; border: 1px solid black; }
    tr.indigo { background-color: #CCCCFF; border: 1px solid black; }
    td { font-family : Arial, Helvetica, sans-serif; padding: 3px; font-size : 10pt; text-align: left; vertical-align: top; border: 1px solid black;}
-   p { font-family : Arial, Helvetica, sans-serif; font-size : 10pt;}
-   p.footer { font-family : Arial, Helvetica, sans-serif; font-size : 8pt; }
-   pre  { font-family : Courier, sans-serif; font-size : 10pt; color : #44cc44;}
 </style>
 
 <h1>Eclipse Graphical Modeling Project (GMP) Versions</h1>
@@ -56,7 +51,7 @@ $html = <<<EOHTML
 
 <tr class="callisto">
 <td rowspan="4">Callisto</td>
-<td>June 26, 2006</td>
+<td>06/26/06</td>
 <td>1.0.0</td>
 <td>1.0.0</td>
 <td rowspan="4">[2]</td>
@@ -72,7 +67,7 @@ $html = <<<EOHTML
 </tr>
 
 <tr class="callisto">
-<td>September 27, 2006</td>
+<td>09/27/06</td>
 <td>1.0.1</td>
 <td>1.0.1</td>
 <td rowspan="2">3.2.1</td>
@@ -85,7 +80,7 @@ $html = <<<EOHTML
 </tr>
 
 <tr class="callisto">
-<td>October 27, 2006</td>
+<td>10/27/06</td>
 <td>1.0.2</td>
 <td>1.0.2</td>
 <td rowspan="2">1.0.2</td>
@@ -95,7 +90,7 @@ $html = <<<EOHTML
 </tr>
 
 <tr class="callisto">
-<td>February 10, 2007</td>
+<td>02/10/07</td>
 <td>1.0.3</td>
 <td>1.0.3</td>
 <td>3.2.2</td>
@@ -106,7 +101,7 @@ $html = <<<EOHTML
 
 <tr class="europa">
 <td rowspan="3">Europa</td>
-<td>June 27, 2007</td>
+<td>06/27/07</td>
 <td>2.0.0</td>
 <td>1.0.100</td>
 <td rowspan="3">[2]</td>
@@ -122,7 +117,7 @@ $html = <<<EOHTML
 </tr>
 
 <tr class="europa">
-<td>September 28, 2007</td>
+<td>09/28/07</td>
 <td>2.0.1</td>
 <td>1.0.101</td>
 <td>3.3.1</td>
@@ -136,7 +131,7 @@ $html = <<<EOHTML
 </tr>
 
 <tr class="europa">
-<td>February 29, 2008</td>
+<td>02/29/08</td>
 <td>2.0.2</td>
 <td>1.0.102</td>
 <td>3.3.2</td>
@@ -148,7 +143,7 @@ $html = <<<EOHTML
 
 <tr class="ganymede">
 <td rowspan="5">Ganymede</td>
-<td>June 25, 2008</td>
+<td>06/25/08</td>
 <td>2.1.0</td>
 <td>1.1.0</td>
 <td>1.1.0</td>
@@ -164,7 +159,7 @@ $html = <<<EOHTML
 </tr>
 
 <tr class="ganymede">
-<td>August 15, 2008</td>
+<td>08/15/08</td>
 <td>2.1.1</td>
 <td>1.1.1</td>
 <td>1.1.1</td>
@@ -174,7 +169,7 @@ $html = <<<EOHTML
 </tr>
 
 <tr class="ganymede">
-<td>September 24, 2009</td>
+<td>09/24/09</td>
 <td>2.1.2</td>
 <td>1.1.2</td>
 <td rowspan="3">1.1.2</td>
@@ -187,7 +182,7 @@ $html = <<<EOHTML
 </tr>
 
 <tr class="ganymede">
-<td>February 25, 2009</td>
+<td>02/25/09</td>
 <td rowspan="2">2.1.3</td>
 <td>1.1.3</td>
 <td rowspan="2">3.4.2</td>
@@ -205,7 +200,7 @@ $html = <<<EOHTML
 
 <tr class="galileo">
 <td rowspan="4">Galileo</td>
-<td>June 24, 2009</td>
+<td>06/24/09</td>
 <td>2.2.0</td>
 <td>1.2.0</td>
 <td>1.2.0</td>
@@ -221,7 +216,7 @@ $html = <<<EOHTML
 </tr>
 
 <tr class="galileo">
-<td>September 25, 2009</td>
+<td>09/25/09</td>
 <td>2.2.1</td>
 <td>1.2.1</td>
 <td>1.2.1</td>
@@ -233,7 +228,7 @@ $html = <<<EOHTML
 </tr>
 
 <tr class="galileo">
-<td>February 26, 2010</td>
+<td>02/26/10</td>
 <td rowspan="2">2.2.2</td>
 <td>1.3.0</td>
 <td rowspan="2">1.3.0</td>
@@ -249,7 +244,7 @@ $html = <<<EOHTML
 
 <tr class="helios">
 <td rowspan="2">Helios</td>
-<td>June 23, 2010</td>
+<td>06/23/10</td>
 <td rowspan="2">2.3.0</td>
 <td>1.4.0</td>
 <td rowspan="2">1.4.0</td>
@@ -265,7 +260,7 @@ $html = <<<EOHTML
 </tr>
 
 <tr class="helios">
-<td>September 24, 2010</td>
+<td>09/24/10</td>
 <td>1.4.1</td>
 <td>3.6.1</td>
 <td>3.6.1</td>
@@ -273,7 +268,7 @@ $html = <<<EOHTML
 
 <tr class="indigo">
 <td>Indigo</td>
-<td>June 2x, 2011</td>
+<td>06/2x/11</td>
 <td></td>
 <td></td>
 <td></td>
@@ -290,10 +285,10 @@ $html = <<<EOHTML
 
 <tr class="white">
 <td colspan="14">
-<p>[1] - GMF Runtime and GMF Notation were not separate projects until Helios and the GMF Restructure.</p>
-<p>[2] - GMF Notation was not a separate feature until Ganymede.</p>
-<p>[3] - MDT OCL started depending on MDT UML2 in Europa.</p>
-<p>[4] - GMF Tooling started depending on M2M QVT in Galileo.</p>
+[1] - GMF Runtime and GMF Notation were not separate projects until Helios and the GMF Restructure.<br>
+[2] - GMF Notation was not a separate feature until Ganymede.<br>
+[3] - MDT OCL started depending on MDT UML2 in Europa.<br>
+[4] - GMF Tooling started depending on M2M QVT in Galileo.
 </td>
 </tr>
 </table>
