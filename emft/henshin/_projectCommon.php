f7d586c91706d556435e7e2f57f875ed2f0a03c3
ckrause <ckrause>
2010-08-04 16:00:01 +0000

    add publications page

--- emft/henshin/_projectCommon.php
+++ emft/henshin/_projectCommon.php
@@ -16,5 +16,6 @@ $Nav->addCustomNav("Installation", "install.php", "_self", 2);
 $Nav->addCustomNav("FAQ", "http://wiki.eclipse.org/Henshin_FAQ", "_self", 2);
 $Nav->addCustomNav("Wiki", "http://wiki.eclipse.org/Henshin", "_self", 2);
 $Nav->addCustomNav("Mailing List", "https://dev.eclipse.org/mailman/listinfo/henshin-dev", "_self", 2);
+$Nav->addCustomNav("Publications", "publications.php", "_self", 2);
 
 ?>
---
+++ emft/henshin/publications.php
@@ -0,0 +1,49 @@
+<?php 
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 
+
+$App = new App();
+$Nav = new Nav();
+$Menu = new Menu();
+
+include($App->getProjectCommon());    
+
+#
+# Begin: page-specific settings.  Change these. 
+$pageTitle 	= "Henshin - Publications";
+$pageKeywords	= "EMF, Henshin, model transformation, installation";
+$pageAuthor	= "Christian Krause";
+
+# Add page-specific Nav bars here
+# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
+# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
+# $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
+# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
+
+# End: page-specific settings
+#
+		
+# Paste your HTML content between the EOHTML markers!	
+$html = <<<EOHTML
+
+<div id="maincontent">
+<div id="midcolumn">
+<h1>$pageTitle</h1>
+
+<ul>
+<li><i>Henshin: Advanced Concepts and Tools for In-place EMF Model Transformations</i>
+To appear in the Proceedings of <a href="http://models2010.ifi.uio.no/">MoDELS'10</a>, 
+Lecture Notes in Computer Science. Springer, 2010.
+</li>
+<ul>
+
+</div>
+</div>
+
+EOHTML;
+
+# Generate the web page
+$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+
+?>
