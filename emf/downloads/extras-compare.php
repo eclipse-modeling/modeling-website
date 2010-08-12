cccf26888115005ff95b12e17cbffb8b8abdfa52
lgoubet <lgoubet>
2010-08-12 09:49:42 +0000

    Archiving old compare builds

--- emf/downloads/extras-compare.php
+++ emf/downloads/extras-compare.php
@@ -4,7 +4,7 @@
 $oldrels = array(
 	"0.8.0" => "200806180301",
 	"0.8.1" => "200809170822",
-	"0.9.0" => "http://archive.eclipse.org/modeling/emf/compare/downloads/drops/0.9.0/S200906090441/",
+	"0.9.0" => array("2009/06/09", "http://archive.eclipse.org/modeling/emf/compare/downloads/drops/0.9.0/S200906090441/"),
 	"1.0.0" => "200906220949",
 );
 
