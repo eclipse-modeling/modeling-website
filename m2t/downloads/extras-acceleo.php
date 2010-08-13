534f8d80db897d14ce76d024a978e1c7b6bbadc2
lgoubet <lgoubet>
2010-08-13 08:20:00 +0000

    Archiving old acceleo builds

---
+++ m2t/downloads/extras-acceleo.php
@@ -0,0 +1,10 @@
+<?php
+
+// archived releases
+$oldrels = array(
+	"0.8.0" => "200906220953",
+	"0.8.1" => "200909161003",
+	"0.9.0" => array("2010/02/02", "http://archive.eclipse.org/modeling/m2t/acceleo/downloads/drops/0.9.0/S201002020636/"),
+);
+
+?>
