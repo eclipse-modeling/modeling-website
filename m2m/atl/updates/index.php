18041b9ca0290792a08861fd0be9ef618c208021
wpiers <wpiers>
2010-11-09 13:55:52 +0000

    fixed page

--- m2m/atl/updates/index.php
+++ m2m/atl/updates/index.php
@@ -1,15 +1,19 @@
-<?php 
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+	
 	#
 	# Begin: page-specific settings.  Change these. 
 	$pageTitle 		= "M2M ATL Update Sites";
 	$pageKeywords	= "";
 	$pageAuthor		= "William Piers";
 	
+	# End: page-specific settings
+	#
+
 	# Paste your HTML content between the EOHTML markers!	
 	$html = <<<EOHTML
-	
-	<div id="midcolumn">
-		<h1>M2M ATL Update Sites</h1>
+
+<div id="midcolumn">
+<h1>M2M ATL Update Sites</h1>
 
 		<div class="homeitem3col">
 			<p>To install these plugins, point your Install Manager at this site.</p>
@@ -23,9 +27,9 @@
 				</ul>
 			</li>
 		</ul>
-	</div>
-	
-		
+			</div>
+</div>
+
 EOHTML;
 
 
