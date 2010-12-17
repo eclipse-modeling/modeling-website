b07b864baa861cd8d7b3551f7858504cfe96aef9
ahunter <ahunter>
2010-12-17 15:35:49 +0000

    [332859] gmp-head ahunter 101217 No Indigo builds for GMF Tooling

--- gmp/gmf-tooling/build/_common.php
+++ gmp/gmf-tooling/build/_common.php
@@ -6,11 +6,13 @@ $options = array (
 
 	"BranchAndJDK" => array (
         "localhost=------------,------------",
+        "2.4.0=HEAD,/opt/sun-java2-5.0",
         "2.3.1=R2_3_maintenance,/opt/sun-java2-5.0",
         "2.2.2=R2_2_maintenance,/opt/sun-java2-6.0_64",
         "2.1.4=R2_1_maintenance,/opt/sun-java2-5.0",
 
         "modeling.eclipse.org=------------,------------",
+        "2.4.0=HEAD,/opt/sun-java2-6.0_64",
         "2.3.1=R2_3_maintenance,/opt/sun-java2-6.0_64",
         "2.2.2=R2_2_maintenance,/opt/sun-java2-6.0_64",
         "2.1.4=R2_1_maintenance,/opt/sun-java2-5.0",
@@ -22,15 +24,15 @@ $options = array (
 	 * and /home/www-data/build/requests/dependencies.urls.txt 
 	 * */ 	
 	"regex" => "I2010.*eclipse-SDK-.*x86_64.*|S-.*2010.*/eclipse-SDK-3.6.*x86_64*|" .
-			"2\.6\..+/[ISR]20.*/emf-xsd-SDK-|" .
-			"3\.1\..+/[ISR]20.*/mdt-uml2-SDK|" .
-			"3\.0\..+/[ISR]20.*/mdt-ocl-.*SDK-|" .
-			"1\.4\..+/[ISR]20.*/emf-query-SDK-|" .
-			"1\.4\..+/[ISR]20.*/emf-validation-SDK-|" .
-			"1\.4\..+/[ISR]20.*/emf-transaction-SDK-|" .
-			"3\.0\..+/[ISR]20.*/m2m-qvtoml-SDK-|" .
-			"1\.4\..+/[ISR]20.*/gmf-runtime-sdk-|" .
-			"3\.6\..+/[ISR]20.*/GEF-SDK-|" .
+			"2\.7\..+/[ISR]20.*/emf-xsd-SDK-|" .
+			"3\.2\..+/[ISR]20.*/mdt-uml2-SDK|" .
+			"3\.1\..+/[ISR]20.*/mdt-ocl-.*SDK-|" .
+			"1\.5\..+/[ISR]20.*/emf-query-SDK-|" .
+			"1\.5\..+/[ISR]20.*/emf-validation-SDK-|" .
+			"1\.5\..+/[ISR]20.*/emf-transaction-SDK-|" .
+			"3\.1\..+/[ISR]20.*/m2m-qvtoml-SDK-|" .
+			"1\.5\..+/[ISR]20.*/gmf-runtime-sdk-|" .
+			"3\.7\..+/[ISR]20.*/GEF-SDK-|" .
 			"orbitBundles-.*\.map",
 
 	"Mapfile_Rule_Default" => 0, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, No Tagging=gen-false"
