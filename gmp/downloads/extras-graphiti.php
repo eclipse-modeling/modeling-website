5bb0464e82a3508c7a90ea6ff67c6c79bc69f5fc
ahunter <ahunter>
2011-01-17 22:33:57 +0000

    [334058] ahunter 110117 Links to Graphiti on the Eclipse GMP page

---
+++ gmp/downloads/extras-graphiti.php
@@ -0,0 +1,12 @@
+<?php
+
+function showNotes()
+{
+?>
+	<div class="homeitem3col">
+		<h3>Graphiti Downloads</h3>
+		<p><a href="http://www.eclipse.org/graphiti/downloads.php"><b>Graphiti downloads can be found HERE</b></a></p>
+	</div>
+<?php
+}
+?>
