af38d0d00d36ca6e7992d32d1c89460b43f643be
estepper <estepper>
2011-06-12 14:45:54 +0000

    *** empty log message ***

--- emf/cdo/build/promote.sh
+++ emf/cdo/build/promote.sh
@@ -8,6 +8,19 @@
 
 for job in `ls jobs`
 do
-	lastBuildNumber=`cat jobs/$job/nextBuildNumber`
-	nextBuildNumber=`cat /shared/jobs/$job/nextBuildNumber`
+	if [ -f "jobs/$job/nextBuildNumber" ]
+	then
+		lastBuildNumber=`cat "jobs/$job/nextBuildNumber"`
+	else
+		lastBuildNumber=0
+	fi
+	
+	nextBuildNumber=`cat "/shared/jobs/$job/nextBuildNumber"`
+	if [ "$nextBuildNumber" != "$lastBuildNumber" ]
+	then
+		echo "Promoting a build..."
+	else
+		echo "Nothing to promote."
+	fi
+	
 done
