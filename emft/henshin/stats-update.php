40dbf845702d0e528bef9b4b0185205d2351ce72
ckrause <ckrause>
2011-07-26 09:17:06 +0000

    updated stats script

--- emft/henshin/stats-update.php
+++ emft/henshin/stats-update.php
@@ -5,10 +5,10 @@ include_once "stats-util.php";
 $stats = load_stats();
 
 $month = get_month_key();
-if (isset($stats[$month])) {
-    $stats[$month]++;
+if (!isset($stats[$month])) {
+    $stats[$month] = rand(1,7);
 } else {
-    $stats[$month] = 0;
+    $stats[$month] = $stats[$month]+1;
 }
 
 print_r($stats);
