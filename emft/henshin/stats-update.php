3f5f2251560f4b29621d80b3e8cd4fe3a53d4158
ckrause <ckrause>
2011-07-26 09:08:48 +0000

    c

--- emft/henshin/stats-update.php
+++ emft/henshin/stats-update.php
@@ -2,8 +2,17 @@
 
 include_once "stats-util.php";
 
-$stats = get_stats();
+$stats = load_stats();
+
+$month = get_month_key();
+if (isset($stats[$month])) {
+    $stats[$month]++;
+} else {
+    $state[$month] = 0;
+}
 
 print_r($stats);
+
+save_stats($stats);
 
 ?>
