5f8779b82284974d809a1ea062db35eecf5cd42a
ckrause <ckrause>
2011-07-26 11:44:42 +0000

    x

--- emft/henshin/stats-info.php
+++ emft/henshin/stats-info.php
@@ -4,7 +4,7 @@ include_once "stats-util.php";
 $stats = load_stats('R');
 echo "Release stats:<br>\n";
 print_r($stats);
-echo "<br<br>\n";
+echo "<br><br>\n";
 
 $stats = load_stats('N');
 echo "Nightly stats:<br>\n";
