497d119e804107396bc678b12aa5250b8b99d724
nbros <nbros>
2011-09-16 12:35:15 +0000

    remove generated doc from CVS

--- emft/facet/doc/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-CODIR=nightly
-svn co http://dev.eclipse.org/svnroot/modeling/org.eclipse.emft.facet/trunk/doc $CODIR
-MAINDOC=$CODIR/org.eclipse.emf.facet.doc
-xsltproc -o $MAINDOC/toc.html $MAINDOC/toc.xsl $MAINDOC/toc.xml
-
