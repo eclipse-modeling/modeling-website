1a4186489c992165312127f5f10e6b7ded5a2674
dhubner <dhubner>
2012-01-12 12:42:17 +0000

    [xtext] prettyprint updatesites

--- tmf/updates/index.php
+++ tmf/updates/index.php
@@ -1,17 +1,22 @@
-<?php 
+<?php
 if ($_SERVER["SERVER_NAME"] != "www.eclipse.org") {
 	header("Location: http://www.eclipse.org/modeling/tmf/updates/");
 }
 require_once ("../../includes/buildServer-common.php");
-require($_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/updates-common.php");
+require($_SERVER["DOCUMENT_ROOT"] . "/modeling/tmf/updates/updates-common.php");
 
 $PRS = array(
 "TMF Xtext" => "modeling/tmf/xtext",
 );
 
+$siteXMLs = array("Releases (R)" => "releases/",
+				 "Milestones &amp; RCs (S)" => "milestones/",
+				 "Nightly Builds (N)" => "nightly/")
+
+
 function notes()
 {
 }
 
-update_manager("TMF", "Eclipse Modeling", $PRS, false);
+update_manager("TMF", "Eclipse Modeling", $PRS, false, true);
 ?>
