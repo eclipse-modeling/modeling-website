db7735ed06cf15345ab1c4008168a8591bb4cc5c
dhubner <dhubner>
2012-01-12 12:08:58 +0000

    [xtext] point update manger links to xtext p2 repositories

--- tmf/updates/index.php
+++ tmf/updates/index.php
@@ -6,11 +6,12 @@ require_once ("../../includes/buildServer-common.php");
 require($_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/updates-common.php");
 
 $PRS = array(
+"TMF Xtext" => "modeling/tmf/xtext",
 );
 
 function notes()
 {
 }
 
-update_manager("TMF", "Eclipse Modeling", $PRS, true);
+update_manager("TMF", "Eclipse Modeling", $PRS, false);
 ?>
