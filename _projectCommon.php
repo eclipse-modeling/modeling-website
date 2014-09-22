a192793f7e0cd11e8cbb7b67b933ccf675c82d99
jonas <jonas.helming@googlemail.com>
2014-09-22 11:14:18 +0200

    added Google Analytics

--- _projectCommon.php
+++ _projectCommon.php
@@ -51,6 +51,17 @@ $Nav->addCustomNav("Mailing Lists", "http://www.eclipse.org/mail/index_project.p
 
 
 $App->AddExtraHtmlHeader("<link rel=\"stylesheet\" type=\"text/css\" href=\"/modeling/includes/common.css\"/>\n");
+$App->AddExtraHtmlHeader("
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-55011482-1', 'auto');
+  ga('send', 'pageview');
+
+</script>")
 
 
 
