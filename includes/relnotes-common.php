78574dc7412d55a905c88fe4a692f95358cb1111
Christopher Guindon <chris.guindon@eclipse.org>
2016-02-08 13:57:13 -0500

    Bug 477734 - [SECURITY] Xss + SQL INJECTION
    THIS IS NOT A FIX, SOME BREAKAGE IS EXPECTED.
    
    Given the severity of this bug, we added an exit() at the top of this
    file to stop it from being executed on our servers. The owner(s) of this
    website should review every request to MYSQL, $_POST and $_GET variables
    AS SOON AS POSSIBLE.
    
    There is more scripts in this repo that are vulnerable but I did not
    modified because they are protected with the internalUseOnly() function.
    I kept these files as-is to avoid breakage to your build but this is
    still very dangerous, someone needs to review this code as soon as
    possible.
    
    SQL injection is a code injection technique, used to attack data-driven
    applications, in which malicious SQL statements are inserted into an
    entry field for execution (e.g. to dump the database contents to the
    attacker).
    
    Cross-Site Scripting (XSS) vulnerabilities are a type of computer
    security vulnerability typically found in Web applications. XSS
    vulnerabilities enable attackers to inject client-side script into Web
    pages viewed by other users.
    
    Signed-off-by: Christopher Guindon <chris.guindon@eclipse.org>

--- includes/relnotes-common.php
+++ includes/relnotes-common.php
@@ -1,4 +1,27 @@
 <?php
+
+/**
+ * Bug 477734 - [SECURITY] Xss + SQL INJECTION
+ *
+ * SQL injection is a code injection technique,
+ * used to attack data-driven applications, in which malicious
+ * SQL statements are inserted into an entry field for execution
+ * (e.g. to dump the database contents to the attacker).
+ *
+ * Cross-Site Scripting (XSS) vulnerabilities are a type of
+ * computer security vulnerability typically found in Web applications.
+ * XSS vulnerabilities enable attackers to inject client-side script
+ * into Web pages viewed by other users.
+ *
+ * Given the severity of this bug, we added an exit() at the top
+ * of this file to stop it from being executed on our servers.
+ *
+ * The owner(s) of this website should review every request to MYSQL before
+ * removing the exit() on this page.
+ *
+ */
+exit();
+
 /* REMINDER:
  When adding new projects to the database, you must insert a 0.0.0 release as a basis from which
  to compare, or you won't get anything returned from your query.
@@ -37,7 +60,7 @@ $projectAndVersion = "?project=$projct" . (isset($_GET["version"]) ? "&amp;versi
 
 /* set defaults */
 $cvscom = "";
-$tmp = array_keys($cvsprojs); 
+$tmp = array_keys($cvsprojs);
 if (sizeof($tmp) > 0)
 {
 	$proj = (isset($defaultProj)) ? preg_replace("#^/#", "", $defaultProj) : $tmp[0];
