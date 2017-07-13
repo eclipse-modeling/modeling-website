4a9a4fbde0ce5bfa57ac58295a6ac76dd37e5b1b
Jonas Helming <jhelming@eclipsesource.com>
2017-07-13 11:44:33 +0200

    added page for modeling in the web

---
+++ web.php
@@ -0,0 +1,92 @@
+<?php  																														
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	
+if (is_file($_SERVER['DOCUMENT_ROOT'] . "/projects/common/project-info.class.php")) 
+{
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/projects/common/project-info.class.php");
+}
+$App = new App(); $Nav = new Nav(); $Menu = new Menu(); 
+if (class_exists("ProjectInfo"))
+{
+	$projectInfo = new ProjectInfo("modeling");
+	$projectInfo->generate_common_nav( $Nav );
+}		
+include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+
+	#*****************************************************************************
+	#
+	# index.php
+	#
+	# Author: 		Richard Gronback (with edits by Nick Boldt)
+	# Date:			2006-05-11
+	#
+	# Description: Modeling project homepage
+	#
+	#
+	#****************************************************************************
+	
+	#
+	# Begin: page-specific settings.  Change these. 
+	$pageTitle 		= "Modeling in the web";
+	$pageKeywords	= "emf, UI, forms, modeling, UML, UML2, model-driven, web, html, javascript, json, angular, react";
+	$pageAuthor		= "Richard Gronback";
+	
+	# Paste your HTML content between the EOHTML markers!	
+	$html = <<<EOHTML
+
+
+<div id="maincontent">
+	<div id="midcolumn">
+		<h1>$pageTitle</h1>
+		<table>
+		<tr>
+		<td><p></p>
+		</td>
+		<td align="right"><img src="http://www.eclipse.org/modeling/images/modeling_pos_logo_fc_med.jpg">
+		</td>
+		</table>
+		<div class="container-fluid">
+		<div id="content">Slowly but steadily, selected modeling tools are being migrated to web technology using emerging technologies such as Atom, Eclipse Che, Monaco, Thea, or LSP. The following technologies allow you to develop modeling tools and model-based application in the web.</div>
+  </div>
+<script type="text/javascript" src="ejs_production.js"></script>
+	<script>
+(function () {
+	// Render the template using the specified data.
+    var html = new EJS({url: "projects.ejs"}).render({projects: [
+	{Title:'JSON Forms', 
+		Description:'JSON Forms is a framework to efficiently build form-based web UIs. These UIs are targeted at entering, modifying, and viewing data and are usually embedded within an application. JSON Forms eliminates the need to write HTML templates and Javascript for data binding by hand in order to create customizable forms. It does so by leveraging the capabilities of JSON and JSON schema and providing a simple and declarative way of describing forms. Forms are then rendered with a UI framework, currently one that is based on AngularJS. ',
+		URL:'http://jsonforms.io',
+		Logo:'http://jsonforms.io/images/jsonforms-logo-white.svg'
+	},
+	
+]});
+    document.getElementById("content").innerHTML = html;
+}());
+</script>
+		
+		
+		
+		
+		
+
+	</div>
+	
+	
+	<div id="rightcolumn">
+		<div class="sideitem">
+			<h6>News on Twitter</h6>
+		<a class="twitter-timeline" href="https://twitter.com/hashtag/eclipsemf" data-widget-id="503883842478809088">#eclipsemf Tweets</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+		</div>
+	</div>
+</div>
+
+
+
+EOHTML;
+
+
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
